c17d9348ed2fc11947e9ec52f6379d1c335ad29d
dev-launchers-sandbox <53383188+dev-launchers-sandbox@users.noreply.github.com>
2020-01-04 00:27:50 -0600

    Changed to relative paths

--- src/routes/LandingRoute.js
+++ src/routes/LandingRoute.js
@@ -3,9 +3,9 @@ import ReactDOM from "react-dom";
 
 //import style from "./LandingRoute.module.css";
 
-import Header from "/src/components/modules/Header";
-import LandingBody from "/src/components/modules/LandingBody";
-import Footer from "/src/components/modules/Footer";
+import Header from "../components/modules/Header";
+import LandingBody from "../components/modules/LandingBody";
+import Footer from "../components/modules/Footer";
 
 export default class LandingRoute extends React.Component {
   constructor() {
