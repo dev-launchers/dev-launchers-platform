2eab5a5bf3ee54c23c02627566b257c8daf9a17a
Kris Gano <kris@pyxld.com>
2020-01-05 00:31:19 -0600

    Re-initialized project for local building

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-//import LandingRoute from "./routes/LandingRoute.js";
-//import CreateRoute from "./routes/CreateRoute.js";
-//import LearnRoute from "./routes/LearnRoute.js";
-//import PlayRoute from "./routes/PlayRoute.js";
-//import EarnRoute from "./routes/EarnRoute.js";
-//import SupportUsRoute from "./routes/SupportUsRoute.js";
+import LandingRoute from "./routes/LandingRoute.js";
+import CreateRoute from "./routes/CreateRoute.js";
+import LearnRoute from "./routes/LearnRoute.js";
+import PlayRoute from "./routes/PlayRoute.js";
+import EarnRoute from "./routes/EarnRoute.js";
+import SupportUsRoute from "./routes/SupportUsRoute.js";
 
-//import "./styles.css";
+import "./styles.css";
 
 class App extends React.Component {
   constructor() {
@@ -26,12 +26,6 @@ class App extends React.Component {
           toastClassName="toast"
           progressClassName="toast-progress"
         />
-      </div>
-    );
-  }
-}
-/*
-
         <Router>
           <Switch>
             <Route exact path="/" component={LandingRoute} />
@@ -42,6 +36,13 @@ class App extends React.Component {
             <Route path="/support-us" component={SupportUsRoute} />
           </Switch>
         </Router>
+      </div>
+    );
+  }
+}
+/*
+
+
 
 */
 /*
