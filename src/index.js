5d146f4f1cd1d5490c0db4235a992ba2056055e9
dev-launchers-sandbox <53383188+dev-launchers-sandbox@users.noreply.github.com>
2020-01-04 20:13:56 -0600

    Trying to get CI working!!!

--- src/index.js
+++ src/index.js
@@ -17,22 +17,7 @@ import "./styles.css";
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      showModal: false
-    };
-
-    this.handleOpenModal = this.handleOpenModal.bind(this);
-    this.handleCloseModal = this.handleCloseModal.bind(this);
   }
-
-  handleOpenModal() {
-    this.setState({ showModal: true });
-  }
-
-  handleCloseModal() {
-    this.setState({ showModal: false });
-  }
-
   render() {
     return (
       <div className="App">
