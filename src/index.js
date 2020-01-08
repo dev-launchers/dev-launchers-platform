4a4c56990b23f24760479aa13a2e06bde24ba1e0
Kris Gano <kris@pyxld.com>
2020-01-07 22:39:08 -0600

    Changed from BrowserRouter to HashRouter to fix site on github pages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
