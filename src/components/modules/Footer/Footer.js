40ea2a1967e1afc317e670924121f88ee0632ed4
Kris Gano <kris@pyxld.com>
2020-02-02 22:47:47 -0600

    Refactor Footer component to remove getRandomQuote()
    List of quotes have been moved to our content layer.
    Created RandomQuote component in Footer, which fetches a list of all 
    quotes from our content layer and then displays a random, formatted 
    selection.

--- src/components/modules/Footer/Footer.js
+++ src/components/modules/Footer/Footer.js
@@ -3,24 +3,15 @@ import ReactDOM from "react-dom";
 import { Link } from "react-router-dom";
 
 import style from "./Footer.module.css";
+
+import RandomQuote from "./RandomQuote";
+
 import logoMonogramImage from "../../../images/logo-monogram.png";
 
 export default function Footer(props) {
-  const getRandomQuote = () => {
-    const quotes = [
-      "Technology, like art, is a soaring exercise of the human imagination. – Daniel Bell",
-      "Innovation is the outcome of a habit, not a random act. – Sukant Ratnakar",
-      "Any sufficiently advanced technology is indistinguishable from magic. – Arthur C. Clarke",
-      "The technology you use impresses no one. The experience you create with it is everything. – Sean Gerety",
-      "Technology is best when it brings people together. – Matt Mullenweg",
-      "Technology is nothing. What’s important is that you have a faith in people, that they’re basically good and smart, and if you give them tools, they’ll do wonderful things with them. – Steve Jobs"
-    ];
-    return quotes[parseInt(quotes.length * Math.random())];
-  };
-
   return (
     <footer className={style.FooterBlock}>
-      <div className={style.QuoteArea}>{getRandomQuote()}</div>
+      <RandomQuote />
       <Link to={"/"} className="nav-link">
         <img className={style.FooterLogo} src={logoMonogramImage} />
       </Link>
