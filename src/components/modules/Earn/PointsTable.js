95e50960ee3bdfead8596d8e6b106c528d111cfc
Zac Demi <zac.demi@gmail.com>
2020-02-10 13:39:15 -0600

    Refactored PointsTable into its own component

---
+++ src/components/modules/Earn/PointsTable.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Tabletop from "tabletop";
+
+export default function PointsTable(props) {
+  const [students, setStudents] = React.useState([]);
+
+  React.useEffect(() => {
+    Tabletop.init({
+      // https://docs.google.com/spreadsheets/d/e/2PACX-1vQfJccD-2qd8eVQ6BPIc3EbbBUcTcxIUAxNub31QrWalpfExtTccMBYORQoFqPcxt_HRDuWLT9KXwN0/pubhtml
+      // https://docs.google.com/spreadsheets/d/1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0/edit?usp=sharing
+      key: "1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0",
+      callback: googleData => {
+        console.log("google sheet data --->", googleData);
+        setStudents(googleData.students.elements);
+      },
+      simpleSheet: false
+    });
+  }, []);
+
+  return (
+    <table style={{ margin: "5%", width: "75%", textAlign: "center" }}>
+      <tbody>
+        <tr style={{ fontWeight: "bold" }}>
+          <td style={{}}>Name</td>
+          <td>Points</td>
+          <td>XP</td>
+        </tr>
+        {students
+          .filter(entry => {
+            if (entry.isActive == 1) return true;
+            return false;
+          })
+          .map(entry => {
+            return (
+              <tr>
+                <td style={{}}>{entry.name}</td>
+                <td style={{}}>{entry.points}</td>
+                <td style={{}}>{entry.xp}</td>
+              </tr>
+            );
+          })}
+      </tbody>
+    </table>
+  );
+}
--- src/routes/EarnRoute.js
+++ src/routes/EarnRoute.js
@@ -1,35 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
-
-import Tabletop from "tabletop";
-
-//import style from "./HomeRoute.module.css";
 
 import Header from "../components/modules/Header";
 import PageBody from "../components/common/PageBody";
 import Footer from "../components/modules/Footer";
 import SpendCardCollection from "../components/modules/Earn/SpendCardCollection";
 import TaskTable from "../components/modules/Earn/TaskTable";
-
-// Get content data
-let rewards = require("../content/collections/rewards.json").data;
+import PointsTable from "../components/modules/Earn/PointsTable";
 
 export default function EarnRoute() {
-  const [students, setStudents] = React.useState([]);
-
-  React.useEffect(() => {
-    Tabletop.init({
-      // https://docs.google.com/spreadsheets/d/e/2PACX-1vQfJccD-2qd8eVQ6BPIc3EbbBUcTcxIUAxNub31QrWalpfExtTccMBYORQoFqPcxt_HRDuWLT9KXwN0/pubhtml
-      // https://docs.google.com/spreadsheets/d/1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0/edit?usp=sharing
-      key: "1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0",
-      callback: googleData => {
-        console.log("google sheet data --->", googleData);
-        setStudents(googleData.students.elements);
-      },
-      simpleSheet: false
-    });
-  }, []);
-
   return (
     <div>
       <Header />
@@ -63,29 +41,7 @@ export default function EarnRoute() {
         <br />
         <hr />
         <h2>Current Points</h2>
-        <table style={{ margin: "5%", width: "75%", textAlign: "center" }}>
-          <tbody>
-            <tr style={{ fontWeight: "bold" }}>
-              <td style={{}}>Name</td>
-              <td>Points</td>
-              <td>XP</td>
-            </tr>
-            {students
-              .filter(entry => {
-                if (entry.isActive == 1) return true;
-                return false;
-              })
-              .map(entry => {
-                return (
-                  <tr>
-                    <td style={{}}>{entry.name}</td>
-                    <td style={{}}>{entry.points}</td>
-                    <td style={{}}>{entry.xp}</td>
-                  </tr>
-                );
-              })}
-          </tbody>
-        </table>
+        <PointsTable />
         <br />
         <br />
       </PageBody>
