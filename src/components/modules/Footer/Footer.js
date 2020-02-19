0a8da1eee7fa3ea312b04338f3bd3dc0798a22bc
Kris Gano <kris@pyxld.com>
2020-02-18 18:41:49 -0600

    Change 'Events' in navigation to 'Join'

--- src/components/modules/Footer/Footer.js
+++ src/components/modules/Footer/Footer.js
@@ -28,8 +28,8 @@ export default function Footer(props) {
         <Link to={"/earn"} className="nav-link">
           <div className={style.NavEntry}>EARN</div>
         </Link>
-        <Link to={"/events"} className="nav-link">
-          <div className={style.NavEntry}>EVENTS</div>
+        <Link to={"/join"} className="nav-link">
+          <div className={style.NavEntry}>JOIN</div>
         </Link>
         <Link to={"/support-us"} className="nav-link">
           <div className={style.NavEntry}>SUPPORT US</div>
