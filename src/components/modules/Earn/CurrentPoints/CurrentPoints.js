7a6ca81b0226d45f1395be72cd496785df5284fc
Zac Demi <zac.demi@gmail.com>
2020-03-10 17:38:49 -0500

    added leaderboard context to earn route

--- src/components/modules/Earn/CurrentPoints/CurrentPoints.js
+++ src/components/modules/Earn/CurrentPoints/CurrentPoints.js
@@ -1,22 +1,10 @@
 import React from "react";
-import Tabletop from "tabletop";
 import styles from "./CurrentPoints.module.css";
 
-export default function CurrentPoints() {
-  const [students, setStudents] = React.useState([]);
+import { useLeaderboardContext } from "../../../../context/LeaderboardContext.js";
 
-  React.useEffect(() => {
-    Tabletop.init({
-      // https://docs.google.com/spreadsheets/d/e/2PACX-1vQfJccD-2qd8eVQ6BPIc3EbbBUcTcxIUAxNub31QrWalpfExtTccMBYORQoFqPcxt_HRDuWLT9KXwN0/pubhtml
-      // https://docs.google.com/spreadsheets/d/1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0/edit?usp=sharing
-      key: "1ukOl5lCKF8eXiVgjLgFqDzmdudjik5H_rnws8jPFyJ0",
-      callback: googleData => {
-        console.log("google sheet data --->", googleData);
-        setStudents(googleData.students.elements);
-      },
-      simpleSheet: false
-    });
-  }, []);
+export default function CurrentPoints() {
+  const { leaderboard } = useLeaderboardContext();
 
   return (
     <div>
@@ -31,7 +19,7 @@ export default function CurrentPoints() {
             <td className={styles.tableColumnHeading}>Points</td>
             <td className={styles.tableColumnHeading}>XP</td>
           </tr>
-          {students
+          {leaderboard
             .filter(entry => {
               if (entry.isActive == 1) return true;
               return false;
