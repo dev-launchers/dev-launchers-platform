e6cade4b1c86c9560c3744b438eebd173286ae0a
Zac Demi <zac.demi@gmail.com>
2020-03-12 17:18:16 -0500

    added attachment toggle

--- src/components/common/Card/Attachments/Attachments.js
+++ src/components/common/Card/Attachments/Attachments.js
@@ -2,28 +2,35 @@ import React, { useState } from "react";
 import style from "./Attachments.module.css";
 
 export default function Attachments(props) {
+  const [showAttachment, setShowAttachment] = useState(true);
+
+  const imageClick = () => setShowAttachment(!showAttachment);
+
   return (
     <div>
       <div className={style.attachmentImgContainer}>
         <img
           src={"images/Attachment.png"}
+          onClick={() => imageClick()}
           className={style.attachmentImg}
         ></img>
       </div>
 
-      <div className={style.attachmentLinkContainer}>
-        {JSON.parse(props.data).map((item, i) => {
-          return (
-            <a
-              href={item.href}
-              className={style.attachmentLink}
-              target="_blank"
-            >
-              {item.name}
-            </a>
-          );
-        })}
-      </div>
+      {showAttachment && (
+        <div className={style.attachmentLinkContainer}>
+          {JSON.parse(props.data).map((item, i) => {
+            return (
+              <a
+                href={item.href}
+                className={style.attachmentLink}
+                target="_blank"
+              >
+                {item.name}
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
