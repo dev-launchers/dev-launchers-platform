eae1a8197df3410f4b175a75610dd44ec5b82cab
Zac Demi <zac.demi@gmail.com>
2020-03-12 16:23:18 -0500

    removed toggle for attachment icon

--- src/components/common/Card/Attachments/Attachments.js
+++ src/components/common/Card/Attachments/Attachments.js
@@ -2,31 +2,28 @@ import React, { useState } from "react";
 import style from "./Attachments.module.css";
 
 export default function Attachments(props) {
-  const [showAttachment, setShowAttachment] = useState(false);
-
-  const imageClick = () => setShowAttachment(!showAttachment);
-
-  const array = props.data.split(",");
-
   return (
     <div>
       <div className={style.attachmentImgContainer}>
         <img
           src={"images/Attachment.png"}
-          onClick={() => imageClick()}
           className={style.attachmentImg}
         ></img>
       </div>
 
-      {showAttachment && (
-        <div className={style.attachmentLinkContainer}>
-          {array.map((item, i) => (
-            <a href="http://zacdemi.com" className={style.attachmentLink}>
-              {item}
+      <div className={style.attachmentLinkContainer}>
+        {JSON.parse(props.data).map((item, i) => {
+          return (
+            <a
+              href={item.href}
+              className={style.attachmentLink}
+              target="_blank"
+            >
+              {item.name}
             </a>
-          ))}
-        </div>
-      )}
+          );
+        })}
+      </div>
     </div>
   );
 }
