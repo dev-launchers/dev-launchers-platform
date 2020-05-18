2383240131374d9ae314c5d5788bf5e2d9940034
pyxld-kris <kris@pyxld.com>
2020-05-18 14:01:05 -0500

    Added Calendar route

--- src/components/modules/App/App.js
+++ src/components/modules/App/App.js
@@ -21,63 +21,50 @@ import BlogRoute from "../../../routes/BlogRoute.js";
 import SupportUsRoute from "../../../routes/SupportUsRoute.js";
 import StoriesRoute from "../../../routes/StoriesRoute";
 import JoinRoute from "../../../routes/JoinRoute";
+import CalendarRoute from "../../../routes/CalendarRoute";
 import PixilartWrapperRoute from "../../../routes/PixilartWrapperRoute";
 import HangoutRoute from "../../../routes/HangoutRoute";
 
 import MentorSignupRoute from "../../../routes/MentorSignupRoute";
 
 function App(props) {
-  return (
-    <useAuthContext.Provider>
-      <useSheetsContext.Provider>
-        <useRepoContext.Provider>
-          <useLeaderboardContext.Provider>
-            <div className="App">
-              <ToastContainer
-                className="toast-container"
-                toastClassName="toast"
-                progressClassName="toast-progress"
-              />
-              <Router basename={process.env.PUBLIC_URL}>
-                <ScrollToTop />
-                <HamburgerMenu />
-                <Switch>
-                  <Route exact path="/" component={HomeRoute} />
-                  <Route exact path="/create" component={CreateRoute} />
-                  <Route exact path="/learn" component={LearnRoute} />
-                  <Route exact path="/play" component={PlayRoute} />
-                  <Route exact path="/earn" component={EarnRoute} />
-                  <Route exact path="/join" component={JoinRoute} />
-                  <Route exact path="/stories" component={StoriesRoute} />
-                  <Route
-                    path="/blog/:pathType/:authorId/:articleId"
-                    component={BlogRoute}
-                  />
-                  <Route exact path="/support-us" component={SupportUsRoute} />
-                  <Route
-                    exact
-                    path="/pixilart-wrapper"
-                    component={PixilartWrapperRoute}
-                  />
-                  <Route
-                    exact
-                    path="/(hangout|hangouts)/"
-                    component={HangoutRoute}
-                  />
+    return (
+        <useAuthContext.Provider>
+            <useSheetsContext.Provider>
+                <useRepoContext.Provider>
+                    <useLeaderboardContext.Provider>
+                        <div className="App">
+                            <ToastContainer
+                                className="toast-container"
+                                toastClassName="toast"
+                                progressClassName="toast-progress"
+                            />
+                            <Router basename={process.env.PUBLIC_URL}>
+                                <ScrollToTop />
+                                <HamburgerMenu />
+                                <Switch>
+                                    <Route exact path="/" component={HomeRoute} />
+                                    <Route exact path="/create" component={CreateRoute} />
+                                    <Route exact path="/learn" component={LearnRoute} />
+                                    <Route exact path="/play" component={PlayRoute} />
+                                    <Route exact path="/earn" component={EarnRoute} />
+                                    <Route exact path="/join" component={JoinRoute} />
+                                    <Route exact path="/calendar" component={CalendarRoute} />
+                                    <Route exact path="/stories" component={StoriesRoute} />
+                                    <Route path="/blog/:pathType/:authorId/:articleId" component={BlogRoute} />
+                                    <Route exact path="/support-us" component={SupportUsRoute} />
+                                    <Route exact path="/pixilart-wrapper" component={PixilartWrapperRoute} />
+                                    <Route exact path="/(hangout|hangouts)/" component={HangoutRoute} />
 
-                  <Route
-                    exact
-                    path="/mentor-signup"
-                    component={MentorSignupRoute}
-                  />
-                </Switch>
-              </Router>
-            </div>
-          </useLeaderboardContext.Provider>
-        </useRepoContext.Provider>
-      </useSheetsContext.Provider>
-    </useAuthContext.Provider>
-  );
+                                    <Route exact path="/mentor-signup" component={MentorSignupRoute} />
+                                </Switch>
+                            </Router>
+                        </div>
+                    </useLeaderboardContext.Provider>
+                </useRepoContext.Provider>
+            </useSheetsContext.Provider>
+        </useAuthContext.Provider>
+    );
 }
 
 export default App;
