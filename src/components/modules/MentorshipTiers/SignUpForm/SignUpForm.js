ce452f65a66e240eb54a7fe9b749688cf98cd406
pyxld-kris <kris@pyxld.com>
2020-08-10 23:26:08 -0500

    Style form area on members page

--- src/components/modules/MentorshipTiers/SignUpForm/SignUpForm.js
+++ src/components/modules/MentorshipTiers/SignUpForm/SignUpForm.js
@@ -3,17 +3,17 @@ import React from "react";
 import style from "./SignUpForm.module.css";
 
 export default function SignUpForm() {
-  return (
-    <div style={{ width: "100%", textAlign: "center" }}>
-      <iframe
-        className={style.signUpIframe}
-        src="https://docs.google.com/forms/d/e/1FAIpQLSeHOcc3_-CKxkGIlCRZ7myiAe3lBbQnAim5Zv5gZqvsuH-90w/viewform?embedded=true"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
-      >
-        Loading…
-      </iframe>
-    </div>
-  );
+    return (
+        <div className={style.formContainer} style={{ width: "100%", textAlign: "center" }}>
+            <iframe
+                className={style.signUpIframe}
+                src="https://docs.google.com/forms/d/e/1FAIpQLSeHOcc3_-CKxkGIlCRZ7myiAe3lBbQnAim5Zv5gZqvsuH-90w/viewform?embedded=true"
+                frameborder="0"
+                marginheight="0"
+                marginwidth="0"
+            >
+                Loading…
+            </iframe>
+        </div>
+    );
 }
