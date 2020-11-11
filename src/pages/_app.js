b3d869a36c26dea5c383b751c99526d71b683d9d
Kris Gano <kris@pyxld.com>
2020-11-11 02:52:48 -0600

    Fix debug alerts

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import "../styles/globals.css";
 import React from "react";
 
+import { useRouter } from "next/router";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -9,30 +10,53 @@ import { SheetsProvider } from "../context/SheetsContext";
 import { RepoProvider } from "../context/RepoContext";
 import { LeaderboardProvider } from "../context/LeaderboardContext";
 
-import App from "next/app";
-
-class MyApp extends App {
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <AuthProvider>
-        <SheetsProvider>
-          <RepoProvider>
-            <LeaderboardProvider>
-              <div className="App">
-                <ToastContainer
-                  className="toast-container"
-                  toastClassName="toast"
-                  progressClassName="toast-progress"
-                />
-              </div>
-              <Component {...pageProps} />
-            </LeaderboardProvider>
-          </RepoProvider>
-        </SheetsProvider>
-      </AuthProvider>
-    );
+function MyApp({ Component, pageProps }) {
+  /* <Code added to debug iphone issue using alerts> */
+  const router = useRouter();
+  if (router.query.debug === "true") {
+    alert("Entering debug mode - console errors will now display in alerts");
+    function handleError(evt) {
+      if (evt.message) {
+        // Chrome sometimes provides this
+        alert(
+          "error: " +
+            evt.message +
+            " at linenumber: " +
+            evt.lineno +
+            " of file: " +
+            evt.filename
+        );
+      } else {
+        alert(
+          "error: " +
+            evt.type +
+            " from element: " +
+            (evt.srcElement || evt.target)
+        );
+      }
+    }
+    window.addEventListener("error", handleError, true);
   }
+  /* </Code added to debug iphone issue using alerts> */
+
+  return (
+    <AuthProvider>
+      <SheetsProvider>
+        <RepoProvider>
+          <LeaderboardProvider>
+            <div className="App">
+              <ToastContainer
+                className="toast-container"
+                toastClassName="toast"
+                progressClassName="toast-progress"
+              />
+            </div>
+            <Component {...pageProps} />
+          </LeaderboardProvider>
+        </RepoProvider>
+      </SheetsProvider>
+    </AuthProvider>
+  );
 }
 
 export default MyApp;
