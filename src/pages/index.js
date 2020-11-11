75cdd22fb3a78d54c81bb92834ec0ee83d4505c9
Kris Gano <kris@pyxld.com>
2020-11-10 23:18:58 -0600

    Add debug parameter to app: ?debug=true pushes console errors to alert

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 //import style from "./HomeRoute.module.css";
 
+import { useRouter } from "next/router";
+import * as queryString from "query-string";
+
 import Header from "../components/common/Header";
 import Home from "../components/modules/Home";
 import Footer from "../components/common/Footer";
@@ -9,6 +12,34 @@ export default function Index() {
   React.useEffect(() => {
     document.title = "Home";
   }, []);
+
+  /* <Code added to debug iphone issue using alerts> */
+  const router = useRouter();
+  if (router.query.debug === "true") {
+    window.addEventListener("error", handleError, true);
+
+    function handleError(evt) {
+      if (evt.message) {
+        // Chrome sometimes provides this
+        alert(
+          "error: " +
+            evt.message +
+            " at linenumber: " +
+            evt.lineno +
+            " of file: " +
+            evt.filename
+        );
+      } else {
+        alert(
+          "error: " +
+            evt.type +
+            " from element: " +
+            (evt.srcElement || evt.target)
+        );
+      }
+    }
+  }
+  /* </Code added to debug iphone issue using alerts> */
 
   return (
     <div>
