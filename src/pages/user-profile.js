96dbac6b0d458c30906907e1a31485c6f759b7cb
han1ya <emailhaniya@gmailcom>
2021-02-25 19:52:33 -0600

    Create user profile page

---
+++ src/pages/user-profile.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Header from "../components/common/Header"
+import UserProfile from "../components/modules/UserProfile"
+import Footer from "../components/common/Footer"
+
+export default function UserProfile(){
+
+  return(
+    <div>
+    <Header />
+    <UserProfile />
+    <Footer />
+    </div>
+  )
+}
