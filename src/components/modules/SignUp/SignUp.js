6cd9e19bb9706790a8a1091194f1cc8b1fbd6100
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-03-16 11:52:50 +0300

    [SignUp] sending username to the api

--- src/components/modules/SignUp/SignUp.js
+++ src/components/modules/SignUp/SignUp.js
@@ -1,20 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import PageBody from "../../common/PageBody";
 import Link from "next/link";
+import axios from "axios";
 import DiscordImage from "../../../images/signup/discord.png";
 import { FormWrapper, DiscordAuthWrapper } from "./StyledSignUp";
 
 export default function SignUp() {
+  const [username, setUsername] = useState("");
+  const handleUsernameChange = e => {
+    setUsername(e.target.value);
+  };
+  const updateUser = async e => {
+    e.preventDefault();
+    try {
+      const currentUser = await axios(
+        "https://api-staging.devlaunchers.com/users/current",
+        {
+          withCredentials: true
+        }
+      );
+      await axios.put(
+        `https://api-staging.devlaunchers.com/users/${currentUser.data.id}`,
+        { username: username },
+        {
+          withCredentials: true
+        }
+      );
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
     <div>
       <PageBody>
         <br />
         <h1>Sign up</h1>
         <FormWrapper>
-          <form>
+          <form onSubmit={updateUser}>
             <br />
             <label>Username</label>
-            <input type="text" />
+            <input
+              onChange={handleUsernameChange}
+              value={username}
+              type="text"
+            />
             <input type="submit" value="Sign up" />
           </form>
         </FormWrapper>
