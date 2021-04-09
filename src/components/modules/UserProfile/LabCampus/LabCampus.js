451513e0acaf9341e56d407904f8373edbf0f412
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-04-09 03:56:03 +0300

    fix bug of room elements disappear in the gird

--- src/components/modules/UserProfile/LabCampus/LabCampus.js
+++ src/components/modules/UserProfile/LabCampus/LabCampus.js
@@ -15,65 +15,74 @@ import { ConvertCentralTime } from "../../../../utils/TimeZoneConverter";
 import { DateTime, Interval } from "luxon";
 const LabCampus = props => {
   const [currentTime, setCurrentTime] = useState(DateTime.now());
+
   const handleProgression = eventInterval => {
     let remainingLabMintues =
       Interval.fromDateTimes(currentTime, eventInterval.end).count("minute") -
       1;
-    setInterval(() => {
+
+    const timeUpdater = setInterval(() => {
       setCurrentTime(DateTime.now());
       remainingLabMintues =
         Interval.fromDateTimes(currentTime, eventInterval.end).count("minute") -
         1;
     }, 60000);
-    return (
-      <Progress
-        max="100"
-        value={
-          -1 *
-          Math.ceil(
-            remainingLabMintues / (eventInterval.count("minute") - 1) / 0.01 -
-              100
-          )
-        }
-      />
-    );
+
+    const percentage =
+      -1 *
+      Math.ceil(
+        remainingLabMintues / (eventInterval.count("minute") - 1) / 0.01 - 100
+      );
+
+    if (percentage == 100) clearInterval(timeUpdater);
+
+    return <Progress max="100" value={percentage} />;
   };
 
   return (
     <Wrapper>
       <strong>all times are based off {currentTime.zoneName}</strong>
       <TimeLineContainer>
-        {events.map(
-          (
-            {
-              title,
-              startWeekDay,
-              startHour,
-              startMinute,
-              endWeekDay,
-              endHour,
-              endMinute,
-              location
-            },
-            i
-          ) => {
-            const eventStart = ConvertCentralTime(
-              startWeekDay,
-              startHour,
-              startMinute
-            );
-            const eventEnd = ConvertCentralTime(endWeekDay, endHour, endMinute);
-            return (
-              <TimeStamp key={i}>
-                {title} <br />
-                {eventStart.toFormat("t")}
-                {" - "}
-                {eventEnd.toFormat("t")} <br />
-                {location} Room
-              </TimeStamp>
-            );
-          }
-        )}
+        {events
+          .filter(
+            events =>
+              events.startWeekDay == currentTime.setZone("UTC-5").weekday
+          )
+          .map(
+            (
+              {
+                title,
+                startWeekDay,
+                startHour,
+                startMinute,
+                endWeekDay,
+                endHour,
+                endMinute,
+                location
+              },
+              i
+            ) => {
+              const eventStart = ConvertCentralTime(
+                startWeekDay,
+                startHour,
+                startMinute
+              );
+              const eventEnd = ConvertCentralTime(
+                endWeekDay,
+                endHour,
+                endMinute
+              );
+              return (
+                <TimeStamp key={i}>
+                  {title} <br />
+                  {eventStart.toFormat("t")}
+                  {" - "}
+                  {eventEnd.toFormat("t")} <br />
+                  {location} Room
+                </TimeStamp>
+              );
+            }
+          )}
       </TimeLineContainer>
 
       <LabsContainer>
@@ -85,58 +94,73 @@ const LabCampus = props => {
           "Landing Zone"
         ].map((labTitle, i) => {
           const gridArea = labTitle.split(" ")[0];
+          let currentEvent = events.filter(
+            event =>
+              event.location == labTitle &&
+              event.startWeekDay == currentTime.setZone("UTC-5").weekday
+          );
 
-          return (
+          return currentEvent.length == 0 ? (
             <Lab key={i} style={{ gridArea }}>
-              {events
-                .filter(event => event.location == labTitle)
-                .map((filteredEvent, i) => {
-                  const {
-                    startWeekDay,
-                    startHour,
-                    startMinute,
-                    endWeekDay,
-                    endHour,
-                    endMinute
-                  } = filteredEvent;
-                  const eventStart = ConvertCentralTime(
-                    startWeekDay,
-                    startHour,
-                    startMinute
-                  );
-                  const eventEnd = ConvertCentralTime(
-                    endWeekDay,
-                    endHour,
-                    endMinute
-                  );
-                  const eventInterval = Interval.fromDateTimes(
-                    eventStart,
-                    eventEnd
-                  );
-                  const isLabActive =
-                    !eventInterval.isAfter(currentTime) &&
-                    !eventInterval.isBefore(currentTime);
-                  // console.log(isLabActive);
-                  // console.log(eventStart);
-                  return (
-                    <React.Fragment key={i}>
-                      <Time>{eventStart.toFormat("t")}</Time>
-                      <Title>{labTitle}</Title>
-                      <AlignedDropdown
-                        toggleBtnText="Join"
-                        dropdownItems={
-                          <>
-                            <a href="#">Primary</a>
-                            <a href="#">Room 1</a>
-                            <a href="#">Room 2</a>
-                          </>
-                        }
-                      />
-                      {isLabActive && handleProgression(eventInterval)}
-                    </React.Fragment>
-                  );
-                })}
+              <Time>nothing for today!</Time>
+              <Title>{labTitle}</Title>
+              <AlignedDropdown
+                toggleBtnText="Join"
+                dropdownItems={
+                  <>
+                    <a href="#">Primary</a>
+                    <a href="#">Room 1</a>
+                    <a href="#">Room 2</a>
+                  </>
+                }
+              />
             </Lab>
+          ) : (
+            currentEvent.map(
+              ({
+                startWeekDay,
+                startHour,
+                startMinute,
+                endWeekDay,
+                endHour,
+                endMinute
+              }) => {
+                const eventStart = ConvertCentralTime(
+                  startWeekDay,
+                  startHour,
+                  startMinute
+                );
+                const eventEnd = ConvertCentralTime(
+                  endWeekDay,
+                  endHour,
+                  endMinute
+                );
+                const eventInterval = Interval.fromDateTimes(
+                  eventStart,
+                  eventEnd
+                );
+                const isLabActive =
+                  !eventInterval.isAfter(currentTime) &&
+                  !eventInterval.isBefore(currentTime);
+                return (
+                  <Lab key={i} style={{ gridArea }}>
+                    <Time>{eventStart.toFormat("t")}</Time>
+                    <Title>{labTitle}</Title>
+                    <AlignedDropdown
+                      toggleBtnText="Join"
+                      dropdownItems={
+                        <>
+                          <a href="#">Primary</a>
+                          <a href="#">Room 1</a>
+                          <a href="#">Room 2</a>
+                        </>
+                      }
+                    />
+                    {isLabActive && handleProgression(eventInterval)}
+                  </Lab>
+                );
+              }
+            )
           );
         })}
       </LabsContainer>
