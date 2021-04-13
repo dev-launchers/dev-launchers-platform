43b2269dbbe71f86aa9d8fcc658e2387df6285e1
han1ya <emailhaniya@gmailcom>
2021-04-13 16:59:20 -0500

    render event names to page

--- src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
+++ src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 
 export default function WeeksGlance() {
+  const [eventList, setEventList] = useState([]);
+  const [entryTimes, setEntryTimes] = useState([]);
   let componentDidMount = () => {
-    alert(">:((((((");
     axios
       .get(
         "https://api.calendly.com/scheduled_events?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2FHFHGJK4NNLTU5PXT",
@@ -15,11 +16,28 @@ export default function WeeksGlance() {
       )
       .then(response => {
         console.log(response);
+        let tempEventList = [];
+        let tempEventTimes = [];
+        response.data.collection.forEach(function(entry) {
+          tempEventList.push(entry.name);
+        });
+        setEventList(tempEventList);
+        response.data.collection.forEach(function(entry) {
+          tempEventTimes.push(entry.name);
+        });
+        tempEventTimes.push(entry.name);
       })
       .catch(err => {
         console.error(err);
       });
   };
   React.useEffect(componentDidMount, []);
-  return <div />;
+  return (
+    <div>
+      {eventList.map((entry, index) => {
+        return entry;
+      })}
+      ;
+    </div>
+  );
 }
