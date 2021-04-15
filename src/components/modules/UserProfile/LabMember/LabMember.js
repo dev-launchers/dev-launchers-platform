5720ccc8c4d0823c9529ff10651da885f8fa31ba
Nathan D <nathandemars@devlaunchers.com>
2021-04-15 16:18:07 -0500

    update-lab-member-component
    Add form to component

--- src/components/modules/UserProfile/LabMember/LabMember.js
+++ src/components/modules/UserProfile/LabMember/LabMember.js
@@ -40,6 +40,42 @@ export default function LabMember() {
     });
   };
 
+  const openTimeCapsuleForm = () => {
+    setModalContent(
+      <div>
+        <form style={{ textAlign: "center" }}>
+          <h4>What did you do today?</h4>
+          <textarea id="todayaction" name="todayaction" rows="10" cols="75">
+            Your answer
+          </textarea>
+          <br />
+          <h4>Is there anything new that you learned today?</h4>
+          <textarea id="todaylearn" name="todaylearn" rows="10" cols="75">
+            Your answer
+          </textarea>
+          <br />
+          <h4>How useful was the session?</h4>
+          <input type="radio" id="vuseful" name="usefulness" value="vuseful" />
+          <label for="vuseful">Very useful</label>
+          <br />
+          <input type="radio" id="useful" name="usefulness" value="useful" />
+          <label for="useful">Useful</label>
+          <br />
+          <input
+            type="radio"
+            id="nvuseful"
+            name="usefulness"
+            value="nvuseful"
+          />
+          <label for="nvuseful">Not very useful</label>
+          <br />
+          <input type="submit" />
+        </form>
+      </div>
+    );
+    openModal();
+  };
+
   return (
     <div>
       <Modal
@@ -51,19 +87,28 @@ export default function LabMember() {
       <Portal>
         <Title>Lab - Members</Title>
         <br />
+
         <BeginnerAndIntermediateLabSignUp
           onClick={openBeginnerAndIntermediateLabSignUpModal}
         >
           Sign - up for Beginner/Intermediate Labs
         </BeginnerAndIntermediateLabSignUp>
+
         <AdvancedAndIntermediateLabSignUp
           onClick={openAdvancedAndIntermediateLabSignUpModal}
         >
           Sign - up for Advanced/Intermediate Labs
         </AdvancedAndIntermediateLabSignUp>
+
         <OneOnOneSignUp onClick={openOneOnOneSignUpModal}>
           Sign - Up for One-on-One's
         </OneOnOneSignUp>
+
+        <SubmitTimeCapsule onClick={openTimeCapsuleForm}>
+          Submit Time Capsules
+        </SubmitTimeCapsule>
+
+        <ViewTimeCapsule>View Time Capsules</ViewTimeCapsule>
       </Portal>
     </div>
   );
