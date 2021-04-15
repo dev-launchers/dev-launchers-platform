9421fe6a0d1a0d766995471035b32edd286fc652
Kris Gano <kris@pyxld.com>
2021-04-14 23:25:13 -0500

    fix(pyxld_kris): Fix emulated environment variable context

--- src/context/EnvironmentVariablesContext.js
+++ src/context/EnvironmentVariablesContext.js
@@ -1,12 +1,16 @@
 import React from "react";
 import constate from "constate"; // State Context Object Creator
+import { useRouter } from "next/router";
 
 // Built from this article: https://www.sitepoint.com/replace-redux-react-hooks-context-api/
 
 // Step 1: Create a custom hook that contains your state and actions
 function useEnvironmentVariables() {
+  const router = useRouter();
+
   // Production values by default
   const [envData, setEnvData] = React.useState({
+    envType: "PROD",
     API_URL: "https://api.devlaunchers.com",
     GOOGLE_AUTH_URL: "https://api.devlaunchers.com/auth/google",
     DISCORD_AUTH_URL:
@@ -14,20 +18,22 @@ function useEnvironmentVariables() {
   });
 
   React.useEffect(() => {
-    let env = envData;
-    if (typeof window !== "undefined") {
-      const currentUrl = window.location.href;
-      if (currentUrl.indexOf("staging") != -1) {
-        // In staging
-        env.API_URL = "https://api-staging.devlaunchers.com";
-        env.GOOGLE_AUTH_URL =
-          "https://api-staging.devlaunchers.com/auth/google";
-        env.DISCORD_AUTH_URL =
-          "https://discord.com/api/oauth2/authorize?client_id=815294711983112194&redirect_uri=https%3A%2F%2Fapi-staging.devlaunchers.com%2Fusers%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify";
+    setTimeout(() => {
+      if (typeof window !== "undefined") {
+        const currentUrl = window.location.href;
+        if (currentUrl.indexOf("staging") != -1) {
+          // In staging
+          let env = {
+            envType: "STAGING",
+            API_URL: "https://api-staging.devlaunchers.com",
+            GOOGLE_AUTH_URL: "https://api-staging.devlaunchers.com/auth/google",
+            DISCORD_AUTH_URL:
+              "https://discord.com/api/oauth2/authorize?client_id=815294711983112194&redirect_uri=https%3A%2F%2Fapi-staging.devlaunchers.com%2Fusers%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify"
+          };
+          setEnvData(env);
+        }
       }
-    }
-
-    setEnvData(env);
+    }, 1000);
   }, []);
 
   return { envData };
