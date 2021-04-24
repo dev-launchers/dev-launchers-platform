e57ad1e6e88d55bab5b4c01fb3e9e91f9a1c741f
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-04-24 07:09:38 +0300

    fix(Enjoy2Live): refactor SupportUs to styled

--- src/components/modules/SupportUs/StyledSupportUs.js
+++ src/components/modules/SupportUs/StyledSupportUs.js
@@ -1,42 +1,44 @@
-.contentArea {
+import styled from "styled-components";
+
+export const ContentArea = styled.div`
   display: flex;
   margin-bottom: 4rem;
-}
+`;
 
-.studentImageWrapper {
+export const StudentImageWrapper = styled.div`
   width: 40%;
   display: flex;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.studentImage {
+export const StudentImage = styled.img`
   width: 90%;
-}
+`;
 
-.ctaWrapper {
+export const CtaWrapper = styled.div`
   width: 60%;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-}
+`;
 
-.ctaText {
+export const CtaText = styled.div`
   width: 70%;
   margin: auto;
-}
+`;
 
-.ctaButtonWrapper {
+export const CtaButtonWrapper = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 3rem;
-}
+`;
 
-.statusNote {
+export const StatusNote = styled.div`
   width: 50%;
   text-align: center;
   font-size: 1rem;
   color: black;
   margin-top: 1%;
-}
+`;
