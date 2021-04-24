d193693d3a5b4dce0ff8e645cdf80570f6f084d8
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-04-24 07:36:58 +0300

    fix(Enjoy2Live): refactor SocialMedia to styled

--- src/components/modules/SocialMedia/SocialMedia.js
+++ src/components/modules/SocialMedia/SocialMedia.js
@@ -4,112 +4,95 @@ import Link from "next/link";
 import logoMonogramImage from "../../../images/logo-monogram.png";
 import chessPiece from "../../../images/check-it/chess_display_large.jpg";
 
-import style from "./SocialMedia.module.css";
+import {
+  Side,
+  Main,
+  Row,
+  List,
+  NewTabLink,
+  LogoImage,
+  ChessImageWrapper,
+  ChessImage
+} from "./StyledSocialMedia.js";
 
 export default function SocialMedia() {
   return (
     <div>
       <PageBody>
         <br />
-        <div class={style.row}>
-          <div class={style.side}>
+        <Row>
+          <Side>
             <h2>check it</h2>
-            <div className={style.chessImageWrapper}>
-              <img
-                className={style.chessImage}
-                src={chessPiece}
-                alt="chess-piece"
-              />
-            </div>
-          </div>
-          <div class={style.main}>
+            <ChessImageWrapper>
+              <ChessImage src={chessPiece} />
+            </ChessImageWrapper>
+          </Side>
+          <Main>
             <p>
               <b>Join</b> the Discord chats and Twitch streams to interact with
               the Devlaunchers community.
             </p>
-            <ul class={style.list}>
+            <List>
               <li>
-                <a
-                  href="https://discord.io/devlaunchers/"
-                  target="_blank"
-                  class={style.link}
-                >
+                <NewTabLink href="https://discord.io/devlaunchers/">
                   <i class="fab fa-discord fa-2x" />
                   &nbsp;Discord
-                </a>
+                </NewTabLink>
               </li>
               <li>
-                <a
-                  href="https://www.twitch.tv/devlaunchers"
-                  target="_blank"
-                  class={style.link}
-                >
+                <NewTabLink href="https://www.twitch.tv/devlaunchers">
                   <i class="fab fa-twitch fa-2x" />
                   &nbsp;Twitch
-                </a>
+                </NewTabLink>
               </li>
-            </ul>
+            </List>
             <br />
             <p>
               <b>Collaborate</b> with fellow members on a variety of projects.
             </p>
-            <ul class={style.list}>
+            <List>
               <li>
-                <Link href={"/hangout"} className={style.link}>
+                <Link href={"/hangout"} passHref>
                   <a>
-                    <div className={style.LogoImageHolder}>
-                      <img
-                        src={logoMonogramImage}
-                        className={style.LogoImage}
-                      />
+                    <div>
+                      <LogoImage src={logoMonogramImage} />
                       &nbsp;Hangout Link
                     </div>
                   </a>
                 </Link>
               </li>
               <li>
-                <Link href={"/play"} className={style.link}>
+                <Link href={"/play"} passHref>
                   <a>
-                    <div className={style.LogoImageHolder}>
-                      <img
-                        src={logoMonogramImage}
-                        className={style.LogoImage}
-                      />
+                    <div>
+                      <LogoImage src={logoMonogramImage} />
                       &nbsp;Project Showcase
                     </div>
                   </a>
                 </Link>
               </li>
-            </ul>
+            </List>
 
             <br />
             <p>
               <b>Follow</b> devlauncher adventures on social media.
             </p>
-            <ul class={style.list}>
+            <List>
               <li>
-                <a
-                  href="https://www.instagram.com/devlaunchers/"
-                  target="_blank"
-                  class={style.link}
-                >
+                <NewTabLink href="https://www.instagram.com/devlaunchers/">
                   <i class="fab fa-instagram fa-2x" />
                   &nbsp;Instagram
-                </a>
+                </NewTabLink>
               </li>
               <li>
-                <a
-                  href="https://www.linkedin.com/company/devlaunchers/"
-                  target="_blank"
-                  class={style.link}
-                >
+                <NewTabLink href="https://www.linkedin.com/company/devlaunchers/">
                   <i class="fab fa-linkedin fa-2x" />
                   &nbsp;LinkedIn
-                </a>
+                </NewTabLink>
               </li>
-            </ul>
-          </div>
-        </div>
+            </List>
+          </Main>
+        </Row>
 
         <br />
       </PageBody>
