0d98d190218c1b9236b58407a1c69e174b13e198
han1ya <emailhaniya@gmailcom>
2021-04-24 13:29:44 -0500

    render only events in the next 7 days

--- src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
+++ src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
@@ -1,14 +1,19 @@
 import React, { useState } from "react";
 import { Wrapper, CalendarContainer, WeekCalendar } from "./StyledWeeksGlance";
 import axios from "axios";
+import { DateTime } from "luxon";
 
 export default function WeeksGlance() {
   const [eventList, setEventList] = useState([]);
-  const [entryTimes, setEntryTimes] = useState([]);
+  const [eventTimes, setEventTimes] = useState([]);
+
+  const todaysDate = DateTime.now().setZone("UTC-5");
+  const oneWeekFromNow = todaysDate.plus({ days: 7 });
+
   let componentDidMount = () => {
     axios
       .get(
-        "https://api.calendly.com/scheduled_events?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2FHFHGJK4NNLTU5PXT&min_start_time=2021-04-20T01%3A09%3A05.678Z",
+        `https://api.calendly.com/scheduled_events?user=https%3A%2F%2Fapi.calendly.com%2Fusers%2FHFHGJK4NNLTU5PXT&status=active&sort=start_time%3Aasc&min_start_time=${todaysDate.toISO()}&max_start_time=${oneWeekFromNow.toISO()}`,
         {
           headers: {
             authorization: `Bearer RD_DszQ_ThsIrK2JD6pCKkl0KcoKBbdrwTYM8iYxN3c`
@@ -26,13 +31,15 @@ export default function WeeksGlance() {
         response.data.collection.forEach(function(entry) {
           tempEventTimes.push(entry.start_time);
         });
-        setEntryTimes(tempEventTimes);
+        setEventTimes(tempEventTimes);
       })
       .catch(err => {
         console.error(err);
       });
   };
   React.useEffect(componentDidMount, []);
+
+  //each array should hold a list of events
   let monday = ["Monday"];
   let tuesday = ["Tuesday"];
   let wednesday = ["Wednesday"];
@@ -41,6 +48,7 @@ export default function WeeksGlance() {
   let saturday = ["Saturday"];
   let sunday = ["Sunday"];
 
+  //each index
   let weekday = [];
   weekday[0] = sunday;
   weekday[1] = monday;
@@ -49,23 +57,21 @@ export default function WeeksGlance() {
   weekday[4] = thursday;
   weekday[5] = friday;
   weekday[6] = saturday;
+
   return (
     <Wrapper>
       {eventList.map((entry, index) => {
         return entry;
       })}
-      {entryTimes.map((entry, index) => {
-        var n = weekday[new Date(entry).getUTCDay()];
-        n.append(entry);
-        return entry;
+      {eventTimes.map((entry, index) => {
+        return (
+          " " +
+          `Day ${DateTime.fromJSDate(new Date(entry)).toFormat(
+            "cccc"
+          )} ${DateTime.fromJSDate(new Date(entry)).toFormat("t")}` +
+          ", "
+        );
       })}
-      ;<CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
-      <CalendarContainer></CalendarContainer>
     </Wrapper>
   );
 }
