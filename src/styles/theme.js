14e3300eb61b2d089ad20e6e52d55b134c8078b5
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-04-28 19:20:16 +0300

    feature(Enjoy2Live): added theme provider

---
+++ src/styles/theme.js
@@ -0,0 +1,21 @@
+const theme = {
+  colors: {
+    dark: "#1c1c1c",
+    amber: "#ff7f0e",
+    chrome: "#ffab00",
+    steelBlue: "#3a7ca5",
+    skyBlue: "#81c3d7",
+    light: "#f0edee"
+  },
+  fonts: {
+    headline: "Abel",
+    normal: "Nunito Sans"
+  },
+  fontSizes: {
+    small: "1em",
+    medium: "2em",
+    large: "3em"
+  }
+};
+
+export default theme;
