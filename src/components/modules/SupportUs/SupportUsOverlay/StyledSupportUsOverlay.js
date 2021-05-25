b7310574ca5e5292e17c314538e75eabf706fb1a
Nathan D <nathandemars@devlaunchers.com>
2021-05-25 00:16:23 -0500

    Redesign Support Us Page - Create new Overlay/Header - Create new Call To Action

---
+++ src/components/modules/SupportUs/SupportUsOverlay/StyledSupportUsOverlay.js
@@ -0,0 +1,71 @@
+import styled from "styled-components";
+import theme from "../../../../styles/theme.js";
+
+export const Wrapper = styled.div`
+  position: relative;
+  background-color: ${theme.colors.NEUTRAL_2};
+  width: 100%;
+  height: 30vw;
+  border-bottom: 0.5rem solid ${theme.colors.NEUTRAL_1};
+
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  justify-content: space-between;
+
+  @media (orientation: portrait) {
+    height: 80vw;
+  }
+`;
+
+export const RainbowedMessageArea = styled.div`
+  width: 70%;
+  height: 50%;
+  background-color: ${theme.colors.NEUTRAL_1};
+  flex-direction: column;
+  margin-top: 7.25%;
+`;
+
+export const MessageArea = styled.div`
+  background-color: ${theme.colors.NEUTRAL_1};
+  height: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 5%;
+`;
+
+export const Tagline = styled.div`
+  width: 80%;
+  /* font-weight: bold; */
+  font-size: 3rem;
+  color: white;
+  font-family: ${theme.fonts.headline}, sans-serif;
+  line-height: 1.1em;
+
+  @media (orientation: portrait) {
+    width: 80%;
+  }
+`;
+
+export const ImageArea = styled.div`
+  position: relative;
+  width: 30%;
+  height: 100%;
+  background-color: ${theme.colors.ACCENT_3};
+  overflow: hidden;
+
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+`;
+
+export const Image = styled.img`
+  height: 100%;
+  filter: drop-shadow(-0.075rem 0rem 0 ${theme.colors.NEUTRAL_2})
+    drop-shadow(0rem -0.075rem 0 ${theme.colors.NEUTRAL_2})
+    drop-shadow(0.15rem 0rem 0 ${theme.colors.NEUTRAL_2}) grayscale(100%);
+  transform: scale(2.25);
+  position: relative;
+  top: 20%;
+`;
