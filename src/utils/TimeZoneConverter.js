4c281f99ad976fdc0aa529dd71e5abdb6b54d19a
AngeredZeus <zeus336bc@live.com>
2021-06-09 22:11:30 -0400

    ProgramDescriptionArea now converts the lab times to user's local time Added some bold styling Reworded the How It Works section

--- src/utils/TimeZoneConverter.js
+++ src/utils/TimeZoneConverter.js
@@ -8,3 +8,10 @@ export const ConvertCentralTime = (weekday = 6, hour, minute = 0) => {
     zone: "UTC-5"
   }).setZone("local");
 };
+
+export const ConvertCentralTimeString = (weekday = 0, hour = 0, minute = 0) => {
+  const centralDate = ConvertCentralTime(weekday,hour,minute);
+  const localDate = centralDate.toLocaleString({ weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric'});
+  const localTz = centralDate.toFormat("ZZZZ");
+  return `${localDate} ${localTz}`;
+}
