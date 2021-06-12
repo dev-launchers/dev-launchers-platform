ee1c1e62c1bc222a51b95fdd48b9bf3c4943edd5
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-06-12 10:29:58 +0300

    make cards bigger

--- src/components/modules/Projects/StyledProjects.js
+++ src/components/modules/Projects/StyledProjects.js
@@ -12,11 +12,11 @@ export const Layout = styled.div`
 export const ProjectContainer = styled.div`
   border-top: 1px solid rgba(0, 0, 0, 0.1);
   box-shadow: 0 4px 4px 0 rgb(0 0 0 / 20%);
-  width: 70%;
+  width: 100%;
   height: 100%;
   max-width: 530px;
 
   @media (orientation: portrait) {
-    width: 50%;
+    width: 90vw;
   }
 `;
