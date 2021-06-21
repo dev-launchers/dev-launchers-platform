d811a79b38c7e38a326b9c55f4f3748206e87395
Kris Gano <kris@pyxld.com>
2021-06-21 12:29:35 -0500

    Project pages touchups

--- src/components/modules/Projects/Projects.js
+++ src/components/modules/Projects/Projects.js
@@ -10,35 +10,51 @@ const Projects = () => {
   const projects = useProjectsDataContext();
 
   return (
-    <Layout>
-      {projects.map((project, i) => (
-        <ProjectContainer>
-          <Card
-            isLinkingInside
-            style={{ margin: 0, width: "100%", height: "100%" }}
-            cardData={{
-              id: project.id,
-              title: project.title,
-              secondaryText: "Commitment level: " + project.commitmentLevel,
-              tags: project.keywords.map(({ keyword }) => keyword),
-              description: project.catchPhrase,
-              href: project.slug,
-              imageSrc: env().STRAPI_URL + project.heroImage.url,
-              actions: (
-                <>
-                  <Link href={`projects/${project.slug}` || ""} passHref>
-                    <a>LEARN MORE</a>
-                  </Link>
-                  <Link href="support-us" passHref>
-                    <a>DONATE</a>
-                  </Link>
-                </>
-              )
-            }}
-          />
-        </ProjectContainer>
-      ))}
-    </Layout>
+    <div
+      style={{
+        width: "90%",
+        marginLeft: "auto",
+        marginRight: "auto",
+        marginTop: "3rem"
+      }}
+    >
+      <h1>Projects you can join!</h1>
+      <div>
+        Create, discover, and join open-source software projects! We help
+        members to contribute meaningfully and gain industry-ready experience
+        along the way. Build epic products, tools, and games used by real people
+        while building valuable skills and meeting awesome people!
+      </div>
+      <Layout>
+        {projects.map((project, i) => (
+          <ProjectContainer>
+            <Card
+              isLinkingInside
+              style={{ margin: 0, width: "100%", height: "100%" }}
+              cardData={{
+                id: project.id,
+                title: project.title,
+                secondaryText: "Commitment level: " + project.commitmentLevel,
+                tags: project.keywords.map(({ keyword }) => keyword),
+                description: project.catchPhrase,
+                href: project.slug,
+                imageSrc: env().STRAPI_URL + project.heroImage.url,
+                actions: (
+                  <>
+                    <Link href={`projects/${project.slug}` || ""} passHref>
+                      <a>LEARN MORE</a>
+                    </Link>
+                    <Link href="support-us" passHref>
+                      <a>DONATE</a>
+                    </Link>
+                  </>
+                )
+              }}
+            />
+          </ProjectContainer>
+        ))}
+      </Layout>
+    </div>
   );
 };
 
