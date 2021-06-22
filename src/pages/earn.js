f781a619eaa54f7262652206d30a092e1a4fe135
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-06-22 19:56:54 +0300

    import head from next.js

--- src/pages/earn.js
+++ src/pages/earn.js
@@ -3,6 +3,7 @@ import React from "react";
 import Header from "../components/common/Header";
 import Earn from "../components/modules/Earn";
 import Footer from "../components/common/Footer";
+import Head from "next/head";
 
 export default function EarnRoute() {
   return (
