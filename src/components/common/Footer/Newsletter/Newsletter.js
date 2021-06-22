b89f02ef1050589fedcd569832f40c5298d34143
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-06-22 22:40:06 +0300

    fix(Enjoy2Live): connect newsletter to api

--- src/components/common/Footer/Newsletter/Newsletter.js
+++ src/components/common/Footer/Newsletter/Newsletter.js
@@ -8,22 +8,40 @@ import {
 } from "./StyledNewsletter";
 import validateEmail from "../../../../utils/ValidateEmail";
 import { withTheme } from "styled-components";
+import axios from "axios";
+import { env } from "../../../../utils/EnvironmentVariables";
 
 const NewsLetter = (props) => {
   const [email, setEmail] = useState("");
   const [isCorrect, setIsCorrect] = useState(false);
   const [check, setCheck] = useState(false);
   const [secret, setSecret] = useState(false);
+  const [isEmailAlreadyUsed, setIsEmailAlreadyUsed] = useState(false);
 
-  function EmailValidation() {
+  const EmailValidation = (email) => {
     if (email == "password") {
       setSecret(true);
     } else if (!validateEmail(email)) {
       setCheck(true);
     } else {
-      setIsCorrect(true);
+      const res = axios
+        .post(env().STRAPI_URL + "/newsletters", {
+          email: email,
+        })
+        .then(({ data }) => {
+          setIsCorrect(true);
+          console.log(data);
+        })
+        .catch((err) => {
+          setIsEmailAlreadyUsed(true);
+          console.error(err);
+        });
     }
-  }
+  };
+
+  const sendEmail = () => {
+    EmailValidation(email);
+  };
 
   if (secret) {
     return (
@@ -33,7 +51,7 @@ const NewsLetter = (props) => {
             fontSize: "7rem",
             marginTop: "10px",
             marginBottom: "20px",
-            textAlign: "center"
+            textAlign: "center",
           }}
         >
           →{" "}
@@ -64,7 +82,7 @@ const NewsLetter = (props) => {
               fontSize: "3rem",
               marginTop: "0px",
               marginBottom: "12px",
-              textAlign: "center"
+              textAlign: "center",
             }}
           >
             <span
@@ -103,12 +121,67 @@ const NewsLetter = (props) => {
                 cursor: "pointer",
               }}
               size={1}
-              onClick={EmailValidation}
+              onClick={sendEmail}
             >
               <button id="submitButton">Submit</button>
             </Col>
           </Row>
           <ErrorMessage>Please enter a valid email!</ErrorMessage>
+        </div>
+      );
+    } else if (isEmailAlreadyUsed) {
+      return (
+        <div className="form">
+          <h2
+            style={{
+              fontSize: "3rem",
+              marginTop: "0px",
+              marginBottom: "12px",
+              textAlign: "center",
+            }}
+          >
+            <span
+              style={{
+                fontSize: "3.25rem",
+              }}
+            >
+              ✉
+            </span>
+            &nbsp; Sign up to our newsletter!&nbsp;
+          </h2>
+          <Row>
+            <Col
+              style={{
+                backgroundColor: props.theme.colors.ACCENT_2,
+                fontWeight: "bold",
+                borderBottomLeftRadius: "25px",
+                marginLeft: "-5px",
+                marginRight: "5px",
+                outline: "3px solid red",
+              }}
+              size={3}
+            >
+              <input
+                id="userEmail"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Enter your email here!"
+              />
+            </Col>
+            <Col
+              style={{
+                fontWeight: "bold",
+                borderBottomRightRadius: "25px",
+                cursor: "pointer",
+              }}
+              size={1}
+              onClick={sendEmail}
+            >
+              <button id="submitButton">Submit</button>
+            </Col>
+          </Row>
+          <ErrorMessage>You already used this email before</ErrorMessage>
         </div>
       );
     } else {
@@ -119,7 +192,7 @@ const NewsLetter = (props) => {
               fontSize: "3rem",
               marginTop: "0px",
               marginBottom: "12px",
-              textAlign: "center"
+              textAlign: "center",
             }}
           >
             <span
@@ -155,7 +228,7 @@ const NewsLetter = (props) => {
                 cursor: "pointer",
               }}
               size={1}
-              onClick={EmailValidation}
+              onClick={sendEmail}
             >
               <button id="submitButton">Submit</button>
             </Col>
