6a39ff6692050f10e667b6e89a590fc3deab8c80
AngeredZeus <zeus336bc@live.com>
2021-06-26 23:01:51 -0400

    Fix displayed date if the event has already occured

--- src/utils/TimeZoneConverter.js
+++ src/utils/TimeZoneConverter.js
@@ -9,8 +9,11 @@ export const ConvertCentralTime = (weekday = 6, hour, minute = 0) => {
   }).setZone("local");
 };
 
-export const ConvertCentralTimeString = (weekday = 0, hour = 0, minute = 0) => {
-  const centralDate = ConvertCentralTime(weekday,hour,minute);
+export const ConvertCentralTimeString = (weekday = 0, hour = 0, minute = 0, repeat = {weeks: 1}) => {
+  let centralDate = ConvertCentralTime(weekday,hour,minute);
+  if (DateTime.now() > centralDate){
+    centralDate = centralDate.plus(repeat)
+  }
   const localDate = centralDate.toLocaleString({ weekday: 'long', month: 'long', day: 'numeric', hour: 'numeric'});
   const localTz = centralDate.toFormat("ZZZZ");
   return `${localDate} ${localTz}`;
