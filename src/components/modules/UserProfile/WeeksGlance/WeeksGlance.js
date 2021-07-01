b23c517f32265039829a82024bd85a1e1b0da78c
han1ya <emailhaniya@gmailcom>
2021-06-30 23:22:28 -0500

    in process of refactoring calendar to common component

--- src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
+++ src/components/modules/UserProfile/WeeksGlance/WeeksGlance.js
@@ -3,7 +3,7 @@ import { Wrapper, Event, Day, WeekdayTitle } from "./StyledWeeksGlance";
 import axios from "axios";
 import { DateTime } from "luxon";
 
-let makeDateCompatible = isoDate => {
+let makeDateCompatible = (isoDate) => {
   let arrStr = isoDate.split(".");
   return `${arrStr[0]}${arrStr[1].replace("+", "-").substr(3)}`;
 };
@@ -14,58 +14,11 @@ export default function WeeksGlance() {
   let current = DateTime.now();
   let max = current.plus({ days: 7 });
 
-  let makeRequest = () => {
-    axios
-      .get(
-        `https://www.googleapis.com/calendar/v3/calendars/c_pu1dj74902v1ablvm1i0s22hi4%40group.calendar.google.com/events?timeMax=${makeDateCompatible(
-          max.toISO()
-        )}&timeMin=${makeDateCompatible(
-          current.toISO()
-        )}&singleEvents=true&key=AIzaSyCgXZRjXOwT6DilHJyjj5B3svz6cETj_MI`
-      )
-      .then(response => {
-        console.log(response);
-        let tempEventList = [];
-        response.data.items.forEach(function(entry) {
-          console.log(entry.summary);
-          let time = DateTime.fromISO(entry.start.dateTime, {
-            zone: entry.start.timeZone
-          }).setZone();
+  let calendarDataURL = "https://www.googleapis.com/calendar/v3/calendars/c_pu1dj74902v1ablvm1i0s22hi4%40group.calendar.google.com/events?timeMax=  
+  makeDateCompatible(max.toISO())&timeMin=makeDateCompatible(current.toISO())
+  &singleEvents=true&key=AIzaSyCgXZRjXOwT6DilHJyjj5B3svz6cETj_MI";
 
-          tempEventList.push({
-            name: entry.summary,
-            time: time.toFormat("t"),
-            weekday: time.weekday
-          });
-        });
-
-        setEventList(tempEventList);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-  };
-
-  React.useEffect(makeRequest, []);
-
-  let weekdays = ["MON", "TUES", "WED", "THURS", "FRI", "SAT", "SUN"];
-  let dates = [];
-  const createDate = () => {
-    let todayInt = current.weekday;
-    console.log(todayInt);
-
-    for (let i = 1; i < todayInt; i++) {
-      dates.push(current.minus({ days: todayInt - i }).day);
-    }
-
-    dates.push(current.day);
-
-    for (let i = todayInt + 1; i < 8; i++) {
-      dates.push(current.plus({ days: 1 }).day);
-    }
-    return dates;
-  };
-  let dateslist = createDate();
+  URL = {calendarDataURL};
 
   return (
     <Wrapper>
