1b871902800c06b811818d23d1dfd0396afd2a0e
G-Saravanan <gautham.saravanan50@k12.leanderisd.org>
2021-07-20 17:18:36 -0500

    fix(gvsaravanan): Fix background images

--- src/components/common/FeaturedProjects/FeaturedProjects.js
+++ src/components/common/FeaturedProjects/FeaturedProjects.js
@@ -1,13 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import Link from "next/link";
 import { withTheme } from "styled-components";
 import { useProjectsDataContext } from "../../../context/ProjectsContext";
-import {
-  FeaturedBar,
-  ColorBlock,
-  ProjectArea,
-  ProjectCard
-} from "./StyledFeaturedProjects";
+import { FeaturedBar, ColorBlock, ProjectArea } from "./StyledFeaturedProjects";
 import Card from "../../common/Card";
 import { env } from "../../../utils/EnvironmentVariables";
 
@@ -17,9 +12,7 @@ const FeaturedProjects = props => {
 
   let arr = projects.sort(() => 0.5 - Math.random()).slice(0, 2);
   console.log(arr);
-  React.useEffect(() => {
-    // arr = getRandom(projects, 2);
-  }, [projects]);
+
   return (
     <div>
       <FeaturedBar>
@@ -70,7 +63,7 @@ const FeaturedProjects = props => {
                 tags: arr[0]?.keywords.map(({ keyword }) => keyword),
                 description: arr[0]?.catchPhrase,
                 href: arr[0]?.slug,
-                imageSrc: env().STRAPI_URL + arr[0]?.heroImage.url,
+                imageSrc: arr[0]?.heroImage.url,
                 actions: (
                   <>
                     <Link href={`projects/${arr[0]?.slug}` || ""} passHref>
@@ -89,7 +82,7 @@ const FeaturedProjects = props => {
                 tags: project?.keywords.map(({ keyword }) => keyword),
                 description: project?.catchPhrase,
                 href: project?.slug,
-                imageSrc: env().STRAPI_URL + project?.heroImage.url,
+                imageSrc: project?.heroImage.url,
                 actions: (
                   <>
                     <Link href={`projects/${project?.slug}` || ""} passHref>
@@ -110,33 +103,3 @@ const FeaturedProjects = props => {
 };
 
 export default withTheme(FeaturedProjects);
-
-/*<Card
-isLinkingInside
-style={{ margin: 0, width: "100%", height: "100%" }}
-cardData={{
-    id: projects[0].id,
-    title: projects[0].title,
-    secondaryText:
-        "Commitment level: " + projects[0].commitmentLevel,
-    tags: projects[0].keywords.map(
-        ({ keyword }) => keyword
-    ),
-    description: projects[0].catchPhrase,
-    href: projects[0].slug,
-    imageSrc: env().STRAPI_URL + projects[0].heroImage.url,
-    actions: (
-        <>
-            <Link
-                href={`projects/${projects[0].slug}` || ""}
-                passHref
-            >
-                <a>LEARN MORE</a>
-            </Link>
-            <Link href="support-us" passHref>
-                <a>DONATE</a>
-            </Link>
-        </>
-    ),
-}}
-/>*/
