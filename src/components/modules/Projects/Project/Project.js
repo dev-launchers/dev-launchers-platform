d61d09dc63843ea1bf14ff222da46725d0fbe4b4
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-07-27 08:02:43 +0300

    add fallback option

--- src/components/modules/Projects/Project/Project.js
+++ src/components/modules/Projects/Project/Project.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import { withTheme } from "styled-components";
+import { useRouter } from "next/router";
 import Button from "../../../common/Button";
 
 import {
@@ -18,135 +19,145 @@ const truncateText = (text, truncateAt, replaceWith) => {
   return text.slice(0, truncateAt) + replaceWith;
 };
 
-const Project = (props) => (
-  <Wrapper>
-    <ProjectHero projectData={props} />
-    <div
-      style={{
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto",
-      }}
-    >
+const Project = (props) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+  return (
+    <Wrapper>
+      <ProjectHero projectData={props} />
       <div
         style={{
-          fontFamily: props.theme.fonts.headline,
-          marginTop: "4rem",
-          marginBottom: "4rem",
-          padding: "1rem",
-          fontSize: "2.5rem",
-          border: "1px solid black",
+          width: "90%",
+          marginLeft: "auto",
+          marginRight: "auto",
         }}
       >
-        {props?.vision}
-      </div>
-      <div style={{ marginTop: "4rem", marginBottom: "4rem" }}>
-        <h3 style={{ display: "inline" }}>Description:</h3>{" "}
-        {props?.description?.split("\n").map((text, i) => (
-          <p key={i}> {text} </p>
-        ))}
-      </div>
-      <CategoriesContainer>
-        <div>
-          {/* }
+        <div
+          style={{
+            fontFamily: props.theme.fonts.headline,
+            marginTop: "4rem",
+            marginBottom: "4rem",
+            padding: "1rem",
+            fontSize: "2.5rem",
+            border: "1px solid black",
+          }}
+        >
+          {props?.vision}
+        </div>
+        <div style={{ marginTop: "4rem", marginBottom: "4rem" }}>
+          <h3 style={{ display: "inline" }}>Description:</h3>{" "}
+          {props?.description?.split("\n").map((text, i) => (
+            <p key={i}> {text} </p>
+          ))}
+        </div>
+        <CategoriesContainer>
+          <div>
+            {/* }
             <CategoryTitle>
               Project
             </CategoryTitle>
             { */}
-          <CategoryContainer>
-            <h4>Commitment Level</h4>
-            <p>{props?.commitmentLevel}</p>
+            <CategoryContainer>
+              <h4>Commitment Level</h4>
+              <p>{props?.commitmentLevel}</p>
 
-            <h4>Project References</h4>
-            {props?.projectReferenceURLs.map((element, i) => (
-              <p key={i}>
-                <a href={element.url} rel="noopener noreferrer" target="_blank">
-                  {element.title}
-                </a>
-              </p>
-            ))}
-          </CategoryContainer>
-        </div>
-        <div>
-          {/* }
+              <h4>Project References</h4>
+              {props?.projectReferenceURLs.map((element, i) => (
+                <p key={i}>
+                  <a
+                    href={element.url}
+                    rel="noopener noreferrer"
+                    target="_blank"
+                  >
+                    {element.title}
+                  </a>
+                </p>
+              ))}
+            </CategoryContainer>
+          </div>
+          <div>
+            {/* }
             <CategoryTitle>Commitment/Meetings</CategoryTitle>
             { */}
-          <CategoryContainer>
-            <h4>Meeting Times</h4>
-            {props?.meetingTimes.map((meeting, i) => (
-              <p key={i}>
-                {meeting.title} {meeting.dateTime}
-              </p>
-            ))}
-            <h4>Meeting Links</h4>
-            {props?.meetingLinkURLs.map((url) => (
-              <p key={url.id}>
-                <a href={url.url} rel="noopener noreferrer" target="_blank">
-                  {url.roomName}
-                </a>
-              </p>
-            ))}
-          </CategoryContainer>
+            <CategoryContainer>
+              <h4>Meeting Times</h4>
+              {props?.meetingTimes.map((meeting, i) => (
+                <p key={i}>
+                  {meeting.title} {meeting.dateTime}
+                </p>
+              ))}
+              <h4>Meeting Links</h4>
+              {props?.meetingLinkURLs.map((url) => (
+                <p key={url.id}>
+                  <a href={url.url} rel="noopener noreferrer" target="_blank">
+                    {url.roomName}
+                  </a>
+                </p>
+              ))}
+            </CategoryContainer>
+          </div>
+        </CategoriesContainer>
+        <br />
+      </div>
+      {props.openPositions.length ? (
+        <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+          <OpenPositions projectData={props} />
         </div>
-      </CategoriesContainer>
-      <br />
-    </div>
-    {props.openPositions.length ? (
-      <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
-        <OpenPositions projectData={props} />
-      </div>
-    ) : (
-      ""
-    )}
-    {props.team.leaders.length || props.team.members.length ? (
-      <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
-        <Team projectData={props} />
-      </div>
-    ) : (
-      ""
-    )}
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        padding: "2rem",
-        color: "white",
-      }}
-    ></div>
-    <div
-      style={{
-        width: "100%",
-        marginBottom: "2rem",
-        display: "flex",
-        justifyContent: "center",
-      }}
-    >
-      <Button
-        rel="noopener noreferrer"
-        target="_blank"
-        href={props?.signupFormUrl}
+      ) : (
+        ""
+      )}
+      {props.team.leaders.length || props.team.members.length ? (
+        <div style={{ width: "90%", marginLeft: "auto", marginRight: "auto" }}>
+          <Team projectData={props} />
+        </div>
+      ) : (
+        ""
+      )}
+      <div
         style={{
-          fontSize: "3rem",
-          paddingLeft: "20vw",
-          paddingRight: "20vw",
+          display: "flex",
+          justifyContent: "center",
+          padding: "2rem",
+          color: "white",
+        }}
+      ></div>
+      <div
+        style={{
+          width: "100%",
+          marginBottom: "2rem",
+          display: "flex",
+          justifyContent: "center",
         }}
       >
-        JOIN NOW
-      </Button>
-    </div>
-    <div
-      style={{
-        width: "90%",
-        marginLeft: "auto",
-        marginRight: "auto",
-        marginBottom: "2rem",
-      }}
-    >
-      <Link href="/projects" passHref>
-        <a>{"<<"} Back to Projects</a>
-      </Link>
-    </div>
-  </Wrapper>
-);
+        <Button
+          rel="noopener noreferrer"
+          target="_blank"
+          href={props?.signupFormUrl}
+          style={{
+            fontSize: "3rem",
+            paddingLeft: "20vw",
+            paddingRight: "20vw",
+          }}
+        >
+          JOIN NOW
+        </Button>
+      </div>
+      <div
+        style={{
+          width: "90%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          marginBottom: "2rem",
+        }}
+      >
+        <Link href="/projects" passHref>
+          <a>{"<<"} Back to Projects</a>
+        </Link>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default withTheme(Project);
--- src/pages/projects/[slug].js
+++ src/pages/projects/[slug].js
@@ -11,7 +11,7 @@ export const getStaticPaths = async () => {
   }));
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
 
