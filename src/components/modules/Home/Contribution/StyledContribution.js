fb5c6be4cdce4b54a06f12735dc061428c29e8b0
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-07-30 03:21:37 +0300

    fix(Enjoy2Live): contribution section shifted to the top

--- src/components/modules/Home/Contribution/StyledContribution.js
+++ src/components/modules/Home/Contribution/StyledContribution.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
+import Button from "../../../common/Button";
 
 export const CtaSectionContent = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   align-items: center;
+  width: 70%;
+  margin: auto;
 `;
 
 export const CtaSectionDescription = styled.div`
@@ -12,8 +15,12 @@ export const CtaSectionDescription = styled.div`
 `;
 
 export const CtaSectionButtonArea = styled.div`
-  margin-top: 4%;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-wrap: wrap;
+
+  ${Button} {
+    margin: 1rem 1rem 0 1rem;
+  }
 `;
