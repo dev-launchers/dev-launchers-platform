5470ebcd0dc24c1a2e6743ac00f0d1e1b03bc058
Enjoy2Live <iMohammedMaqbol@gmail.com>
2021-08-03 01:03:25 +0300

    plz work

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -2,11 +2,13 @@ import axios from "axios";
 import Header from "../../components/common/Header";
 import Projects from "../../components/modules/Projects";
 import Footer from "../../components/common/Footer";
-import { env } from "../../utils/EnvironmentVariables";
+// import { env } from "../../utils/EnvironmentVariables";
 import { ProjectsDataProvider } from "../../context/ProjectsContext";
 
 export const getStaticProps = async () => {
-  const { data: projects } = await axios(`${env().STRAPI_URL}/projects`);
+  const { data: projects } = await axios(
+    `https://jsonplaceholder.typicode.com/users`
+  );
   // const res = await fetch("https://cms-api-staging.devlaunchers.com/projects");
   // const data = await res.json();
   return {
