f0283ddb1ab9b999c384c1ca42d250b8a3352b3b
Kris Gano <kris@pyxld.com>
2021-09-01 23:54:30 -0500

    fix(pyxld-kris): Fix Dynamic Page component (change from === to ==)

--- src/pages/page/[...pid].js
+++ src/pages/page/[...pid].js
@@ -1,18 +1,18 @@
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 import Header from "../../components/common/Header";
 import DynamicPage from "../../components/modules/DynamicPage";
 import Footer from "../../components/common/Footer";
 
 export default function DynamicPageRoute() {
-    const router = useRouter();
-    const { pid } = router.query;
+  const router = useRouter();
+  const { pid } = router.query;
 
-    return (
-        <div>
-            <Header />
-            <DynamicPage pageId={pid} />
-            <Footer />
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <DynamicPage pageId={pid} />
+      <Footer />
+    </div>
+  );
 }
