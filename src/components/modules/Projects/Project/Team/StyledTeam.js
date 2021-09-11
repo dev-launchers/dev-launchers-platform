cc6bd4ffb366b8d4e2f62e664bb72b8eed3710d2
Hredhya <hredhya3107@gmail.com>
2021-09-10 22:37:24 -0400

    Modified Team.js
    Modified Team.js file for project frontend component #439

--- src/components/modules/Projects/Project/Team/StyledTeam.js
+++ src/components/modules/Projects/Project/Team/StyledTeam.js
@@ -2,24 +2,45 @@ import styled from "styled-components";
 
 export const Wrapper = styled.div``;
 
-export const TeamContentContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-
-  h4 {
-    margin-top: 0;
-  }
+export const TeamContentBlock = styled.div`
+  width: 100%;
+  h4,
   p {
     margin: 0;
   }
 `;
-
 export const MembersContainer = styled.ul`
-  display: flex;
+  display: inline-flex;
+  justify-content: flex-start;
+  align-items: flex-start;
+  flex: 1;
   flex-wrap: wrap;
-
-  & > li {
-    margin-right: 5rem;
-    margin-bottom: 1rem;
+  list-style-type: none;
+  font-size: 13px;
+  color: #008080;
+  & p {
+    padding: 0;
+    margin: 0;
+  }
+  & li {
+    margin-top: 0.5rem;
+    max-width: 300px;
+    :nth-child(2) {
+      font-weight: bold;
+      font-size: 16px;
+    }
+    :first-child {
+      flex-shrink: 0;
+    }
+  }
+`;
+export const MemberImage = styled.img`
+  border-radius: 50%;
+  width: 20%;
+  height: 20%;
+  flex-shrink: 0;
+  float: left;
+  &:hover {
+    opacity: 0.5;
   }
 `;
--- src/components/modules/Projects/Project/Team/Team.js
+++ src/components/modules/Projects/Project/Team/Team.js
@@ -1,44 +1,63 @@
 import React from "react";
 import { withTheme } from "styled-components";
-import { Wrapper, TeamContentContainer, MembersContainer } from "./StyledTeam";
-// import { env } from "../../../../../utils/EnvironmentVariables";
+import {
+  Wrapper,
+  TeamContentBlock,
+  MembersContainer,
+  MemberImage,
+} from "./StyledTeam";
+import boyFrontImage from "../../../../../images/people-cutouts/boy-front.png?webp";
 
 const Team = (props) => {
   const { projectData } = props;
-
   return (
     <Wrapper>
       <h4>
         <u>Team</u>
       </h4>
-      <TeamContentContainer>
-        <p style={{ width: "50%" }}>
-          <h4>Leader(s)</h4>
+      <TeamContentBlock>
+        <p>
+          <h4> Project Leader(s)</h4>
           <MembersContainer>
             {projectData?.team.leaders.map((leader) => (
               <p key={leader.id}>
-                <p>{leader.name}</p>
-                <p>{leader.role}</p>
-                <p>
+                <li>
+                  <a href="#">
+                    <MemberImage
+                      src={boyFrontImage}
+                      alt="Image of Team member"
+                    ></MemberImage>
+                  </a>
+                </li>
+                <li>{leader.name}</li>
+                <li>Role:{leader.role}</li>
+                <li>
                   <a href={`mailto:${leader.email}`}>Send Email</a>
-                </p>
+                </li>
               </p>
             ))}
           </MembersContainer>
         </p>
-
-        <p style={{ width: "50%" }}>
-          <h4>Members</h4>
+        <p>
+          <h4>Team Members</h4>
           <MembersContainer>
             {projectData?.team.members.map((member) => (
               <p key={member.id}>
-                <p>{member.name}</p>
-                <p>{member.role}</p>
+                <li>
+                  <a href="#">
+                    <MemberImage
+                      src={boyFrontImage}
+                      alt="Image of Team member"
+                    ></MemberImage>
+                  </a>
+                </li>
+                <li>{member.name}</li>
+                <li>Role:{member.role}</li>
               </p>
             ))}
           </MembersContainer>
         </p>
-      </TeamContentContainer>
+      </TeamContentBlock>
     </Wrapper>
   );
 };
