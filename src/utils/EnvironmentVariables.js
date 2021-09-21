c7fad92f9bfd2492e286d3f81b192ed426e3f9eb
Kris Gano <kris@pyxld.com>
2021-09-21 15:46:36 -0500

    fix(pyxld-kris): Remove trailing slashes from API endpoints

--- src/utils/EnvironmentVariables.js
+++ src/utils/EnvironmentVariables.js
@@ -1,7 +1,7 @@
 const PROD = {
   envType: "PROD",
-  API_URL: "https://api.devlaunchers.org/",
-  STRAPI_URL: "https://api.devlaunchers.org/",
+  API_URL: "https://api.devlaunchers.org",
+  STRAPI_URL: "https://api.devlaunchers.org",
   GOOGLE_AUTH_URL: "https://api.devlaunchers.org/connect/google",
   DISCORD_AUTH_URL:
     "https://discord.com/api/oauth2/authorize?client_id=709889509864636496&redirect_uri=https%3A%2F%2Fapi.devlaunchers.com%2Fusers%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify",
@@ -9,8 +9,8 @@ const PROD = {
 
 const STAGING = {
   envType: "STAGING",
-  API_URL: "https://api-staging.devlaunchers.org/",
-  STRAPI_URL: "https://api-staging.devlaunchers.org/",
+  API_URL: "https://api-staging.devlaunchers.org",
+  STRAPI_URL: "https://api-staging.devlaunchers.org",
   GOOGLE_AUTH_URL: "https://api-staging.devlaunchers.org/connect/google",
   DISCORD_AUTH_URL:
     "https://discord.com/api/oauth2/authorize?client_id=815294711983112194&redirect_uri=https%3A%2F%2Fapi-staging.devlaunchers.com%2Fusers%2Fauth%2Fdiscord%2Fcallback&response_type=code&scope=identify",
