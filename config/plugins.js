dd853da592a88960fb4f33c04262585a1f6bcc49
chungthuang <7826979+chungthuang@users.noreply.github.com>
2022-07-31 23:16:40 +0100

    feat(chungthuang): Run postman integration test for each commit

--- config/plugins.js
+++ config/plugins.js
@@ -12,4 +12,12 @@ module.exports = ({env}) => ({
         sendMetadata: true,
       }
     },
+    'users-permissions': {
+      config: {
+        jwt: {
+          expiresIn: '7d',
+        },
+        jwtSecret: env('JWT_SECRET')
+      },
+    },
 });
