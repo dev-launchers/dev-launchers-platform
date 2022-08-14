d50cf20c0090cabd3f3f2d4017c569d1299e9661
chungthuang <7826979+chungthuang@users.noreply.github.com>
2022-08-14 17:30:14 +0100

    feat(chungthuang): Migrate admin, server and plugins config

--- config/admin.js
+++ config/admin.js
@@ -1,8 +1,9 @@
-module.exports = ({ env }) => ({
+// https://docs.strapi.io/developer-docs/latest/setup-deployment-guides/configurations/required/admin-panel.html#available-options
+module.exports = ({ env }) => ({ 
   auth: {
-    secret: env('ADMIN_JWT_SECRET', 'jwt-secret'),
+    secret: env('ADMIN_JWT_SECRET', '9Yh3^gMZbeT6G4jR8R$2syN&'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT', 'token-salt'),
+    salt: env('API_TOKEN_SALT', 'y3a45zNWq%c%5J4WSM6znTF&'),
   },
 });
