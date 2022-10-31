dca71386b1fe30afa2dcf8ba875baf364482c52f
Kris Gano <kris@pyxld.com>
2022-10-30 20:42:59 -0500

    fix(pyxld-kris): Fix comment character counting logic

--- apps/ideas/src/components/modules/WorkshoppingPage/CommentForm.js
+++ apps/ideas/src/components/modules/WorkshoppingPage/CommentForm.js
@@ -1,3 +1,4 @@
+import React from "react";
 import axios from "axios";
 import {
   UserNameCommentBox,
@@ -6,15 +7,22 @@ import {
   UserImageOne,
 } from "./StyledComments.js";
 
+const MAX_COMMENT_CHARS = 250;
+
 function CommentForm(props) {
   const { selectedCard, ...other } = props;
+  const [charsLeft, setCharsLeft] = React.useState(MAX_COMMENT_CHARS);
 
   const handleChange = (e) => {
     props.setHandleChange(e.target.value);
   };
 
   const handleTextChange = (e) => {
-    props.setHandleTextChange(e.target.value);
+    const text = e.target.value;
+    props.setHandleTextChange(text);
+
+    let characterCount = text.length;
+    setCharsLeft(MAX_COMMENT_CHARS-characterCount);
   };
 
   const handleSubmit = (e) => {
@@ -57,12 +65,18 @@ function CommentForm(props) {
               e.target.style.height = `${e.target.scrollHeight}px`; 
               // In case you have a limitation
               // e.target.style.height = `${Math.min(e.target.scrollHeight, limit)}px`;
+              //textCounter(this,'counter',250);
             }}
             style={{width:"100%", overflow:"hidden"}}
             name="text"
-            placeholder="What are your thoughts?"
+            placeholder="What are your thoughts? (max 250 characters)"
             value={props.handleTextChange}
-            onChange={handleTextChange}></textarea>
+            onChange={handleTextChange}
+            maxlength={MAX_COMMENT_CHARS}></textarea>
+          {/* source: https://codepen.io/patrickwestwood/pen/gPPywv */}
+          <div id="the-count">
+            <span id="chars-left">{charsLeft}</span>
+          </div>
         </UserComment>
         <button type="submit">Submit</button>
       </form>
