fea1e72f61b09596b9c77c3cc810e7747a3f7113
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-11-24 08:07:48 +0300

    Added docs for available icons

--- packages/UI/src/assets/icons/Chevron.tsx
+++ packages/UI/src/assets/icons/Chevron.tsx
@@ -10,7 +10,10 @@ function ChevronDown(props: React.SVGProps<SVGSVGElement>) {
       xmlns="http://www.w3.org/2000/svg"
       {...props}
     >
-      <polyline points="6 9 12 15 18 9"></polyline>
+      <polyline
+        stroke={props.stroke || '#24471F'}
+        points="6 9 12 15 18 9"
+      ></polyline>
     </svg>
   );
 }
---
+++ packages/UI/src/assets/icons/index.ts
@@ -0,0 +1,5 @@
+export { default as Bell } from './Bell';
+export { default as Chevron } from './Chevron';
+export { default as Close } from './Close';
+export { default as Error } from './Error';
+export { default as Success } from './Success';
