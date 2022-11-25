8a799c18403f36fa2ec7e6743a53799cc00346ad
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-11-25 15:34:29 +0300

    components: added paddingStyle of 700

--- packages/UI/src/components/atoms/Padding/StyledPadding.ts
+++ packages/UI/src/components/atoms/Padding/StyledPadding.ts
@@ -28,6 +28,8 @@ const comPad500 = `padding: 3rem 6rem;`;
 
 const comPad600 = `padding: 4rem 8rem;`;
 
+const comPad700 = `padding: 1rem 0.5rem 1rem 1.5rem;`;
+
 export const paddingStyles = {
   uniPad100,
   uniPad200,
@@ -44,4 +46,5 @@ export const paddingStyles = {
   comPad400,
   comPad500,
   comPad600,
+  comPad700,
 } as const;
