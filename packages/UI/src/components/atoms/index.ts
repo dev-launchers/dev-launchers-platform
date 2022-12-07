335d0fd521a2d7ef329cc079c675829548590959
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-12-08 02:36:30 +0300

    exported layers to be used

--- packages/UI/src/components/atoms/index.ts
+++ packages/UI/src/components/atoms/index.ts
@@ -2,3 +2,4 @@ export { default as Box } from './Box';
 export { default as ToolTip } from './Tooltip';
 export { default as Slider } from './Slider';
 export { default as Typography } from './Typography';
+export { default as Layer } from './Layer';
