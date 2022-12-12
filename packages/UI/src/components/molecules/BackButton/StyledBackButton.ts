78b3845eda72f18f9791d72998227b5b170ba9d5
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-12-13 01:51:33 +0300

    refactor Backbutton

--- packages/UI/src/components/molecules/BackButton/StyledBackButton.ts
+++ packages/UI/src/components/molecules/BackButton/StyledBackButton.ts
@@ -1,23 +1,23 @@
 import styled from 'styled-components';
+import { paddingStyles } from '../../atoms/Padding';
+import { radiusStyles } from '../../atoms/Radius/StyledRadius';
 import { shadowStyles } from '../../atoms/Shadow';
 import { typographyStyles } from '../../atoms/Typography';
 
 export const BackButton = styled.a`
   ${typographyStyles['label']}
+  ${({ type }) =>
+    type === 'cancel'
+      ? `${shadowStyles.shadow500}
+      ${radiusStyles['radius100']}
+      ${paddingStyles['uniPad200']}`
+      : `box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+      ${radiusStyles['radius200']}
+      ${paddingStyles['comPad100']}`}
   display: inline-flex;
   align-items: center;
   gap: 8px;
-  border-radius: ${({ type }) => (type === 'cancel' ? '4px' : '8px')};
-  padding: ${({ type }) => (type === 'cancel' ? '8px' : '8px 16px')};
   background: ${({ theme }) => theme.colors.GREYSCALE_WHITE};
-  ${({ type }) =>
-    type === 'cancel'
-      ? shadowStyles.shadow500
-      : 'box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25)'};
   color: ${({ theme }) => theme.colors.GREYSCALE_BLACK};
   cursor: pointer;
-
-  @media (max-width: ${({ theme }) => theme.breakpoints.sm + 'px'}) {
-    padding: ${({ type }) => (type === 'cancel' ? '8px' : '12px 16px')};
-  }
 `;
