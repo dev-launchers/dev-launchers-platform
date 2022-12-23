83dcbeedbcf597ddf2cb5863c5f835c8d82952cd
Alston Hsu <alston.hsu@outlook.com>
2022-12-23 13:06:33 -0600

    fix focus link

--- packages/UI/src/components/atoms/Link/StyledLink.ts
+++ packages/UI/src/components/atoms/Link/StyledLink.ts
@@ -27,8 +27,11 @@ export const StyledLink = styled.a`
 
   &:focus {
     font-weight: 400;
-    color: ${({ theme }) => theme.colors.LIGHT_BLUE_200};
-    border: solid 2px ${({ theme }) => theme.colors.BLUE_600};
+    color: ${({ theme }) => theme.colors.BLUE_600};
+    border: dotted 0.4px ${({ theme }) => theme.colors.LIGHT_BLUE_200};
+    outline: 1px dashed black;
+    border-radius: 4px;
+    background-color: ${({ theme }) => theme.colors.LIGHT_BLUE_200};
     @media only screen and (min-width: 62.5rem) {
       font-size: 1.25rem;
     }
