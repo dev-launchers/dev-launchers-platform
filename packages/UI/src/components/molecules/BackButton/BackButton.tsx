f65d43790092564f4dbd275f8872426554df25ff
Nouri Anouar <79311912+Nouri-Anouar@users.noreply.github.com>
2022-12-26 18:13:35 +0000

    added browsing back functionality to backbutton

--- packages/UI/src/components/molecules/BackButton/BackButton.tsx
+++ packages/UI/src/components/molecules/BackButton/BackButton.tsx
@@ -1,11 +1,16 @@
+import { useRouter } from 'next/router';
 import Chevron from '../../../assets/icons/Chevron';
 import Close from '../../../assets/icons/Close';
 import { BackButton } from './StyledBackButton';
 import type { BackButtonProps } from '.';
 
 function BackButtonComponent({ type, withLabel = true }: BackButtonProps) {
+  const router = useRouter();
+  const finalSlashIndex = router.asPath.lastIndexOf('/');
+  const previousPath = router.asPath.slice(0, finalSlashIndex);
+
   return (
-    <BackButton type={type}>
+    <BackButton type={type} onClick={() => router.push(previousPath)}>
       {type === 'cancel' ? (
         <Close stroke="black" width="24px" height="24px" />
       ) : (
