cb7f6b3e8426180bae5287a34ad33e5205b162d1
mfvil <rarefyev@gmail.com>
2023-01-10 09:18:57 -0600

    Roman/product-card: Adding card description component

--- packages/UI/src/components/organisms/cards/ProductCard/ProductCard.tsx
+++ packages/UI/src/components/organisms/cards/ProductCard/ProductCard.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { atoms } from '@devlaunchers/components/src/components';
-import { Container } from './Styled.ProductCard';
 import { CardDescription } from '../StyledCommonComponents';
+import { Container } from './Styled.ProductCard';
 
 const ProductCard = () => {
   return (
-    <Container>
-      <CardDescription title='test' subtitle='test' body='test'/>
-    </Container>
-  )
+    <atoms.Layer type="white">
+      <Container>
+        <CardDescription
+          title="Product Name"
+          subtitle="Product Type"
+          body="This is a tagline for a product"
+        />
+      </Container>
+    </atoms.Layer>
+  );
 };
 export default ProductCard;
