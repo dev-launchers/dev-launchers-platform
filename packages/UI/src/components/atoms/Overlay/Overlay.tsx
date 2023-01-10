a7b0e2b225386252d5ab1d40129180d6fc37c5d5
Alston Hsu <alston.hsu@outlook.com>
2023-01-10 09:29:55 -0600

    added opacity for colors

--- packages/UI/src/components/atoms/Overlay/Overlay.tsx
+++ packages/UI/src/components/atoms/Overlay/Overlay.tsx
@@ -4,24 +4,6 @@ import type { OverlayProps } from '.';
 const Overlay = styled.div<OverlayProps>`
   ${({ type, status }) => {
     switch (type) {
-      case 'dark':
-        if (status === 'default')
-          return css`
-            background-color: ${({ theme }) => theme.colors.GREYSCALE_GREY};
-          `;
-        else if (status === 'error')
-          return css`
-            background-color: ${({ theme }) => theme.colors.ERROR};
-          `;
-        else if (status === 'success')
-          return css`
-            background-color: ${({ theme }) => theme.colors.SUCCESS};
-          `;
-        else if (status === 'notification')
-          return css`
-            background-color: ${({ theme }) => theme.colors.NOTIFICATION};
-          `;
-        break;
       case 'light':
         if (status === 'default')
           return css`
@@ -31,14 +13,38 @@ const Overlay = styled.div<OverlayProps>`
         else if (status === 'error')
           return css`
             background-color: ${({ theme }) => theme.colors.ERROR};
+            opacity: 0.1;
           `;
         else if (status === 'success')
           return css`
             background-color: ${({ theme }) => theme.colors.SUCCESS};
+            opacity: 0.1;
           `;
         else if (status === 'notification')
           return css`
             background-color: ${({ theme }) => theme.colors.NOTIFICATION};
+            opacity: 0.1;
+          `;
+        break;
+      case 'dark':
+        if (status === 'default')
+          return css`
+            background-color: ${({ theme }) => theme.colors.GREYSCALE_GREY};
+          `;
+        else if (status === 'error')
+          return css`
+            background-color: ${({ theme }) => theme.colors.ERROR};
+            opacity: 0.3;
+          `;
+        else if (status === 'success')
+          return css`
+            background-color: ${({ theme }) => theme.colors.SUCCESS};
+            opacity: 0.3;
+          `;
+        else if (status === 'notification')
+          return css`
+            background-color: ${({ theme }) => theme.colors.NOTIFICATION};
+            opacity: 0.3;
           `;
         break;
       default:
