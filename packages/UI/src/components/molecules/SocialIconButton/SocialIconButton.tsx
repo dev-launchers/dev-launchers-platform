b4950a611a58d93a4c1863ef1514d5fcd13b98be
Nouri Anouar <79311912+Nouri-Anouar@users.noreply.github.com>
2023-01-12 12:46:24 +0000

    moved getIcons outside of the socialButton

--- packages/UI/src/components/molecules/SocialIconButton/SocialIconButton.tsx
+++ packages/UI/src/components/molecules/SocialIconButton/SocialIconButton.tsx
@@ -3,29 +3,30 @@ import Box from '../../atoms/Box';
 import { Wrapper } from './StyledSocialIconButton';
 import type { BackButtonProps } from '.';
 
+const getIcon = (type: string) => {
+  switch (type) {
+    case 'twitter':
+      return <Icons.Twitter width="29px" height="24px" />;
+    case 'facebook':
+      return <Icons.Facebook width="24px" height="24px" />;
+    case 'instagram':
+      return <Icons.Instagram width="24px" height="24px" />;
+    case 'linkedin':
+      return <Icons.Linkedin width="24px" height="24px" />;
+    case 'slack':
+      return <Icons.Slack width="24px" height="24px" />;
+    case 'github':
+      return <Icons.Github width="24px" height="24px" />;
+    case 'discord':
+      return <Icons.Discord width="32px" height="24px" />;
+  }
+};
+
 function SocialButton({ type }: BackButtonProps) {
-  const getIcon = () => {
-    switch (type) {
-      case 'twitter':
-        return <Icons.Twitter width="29px" height="24px" />;
-      case 'facebook':
-        return <Icons.Facebook width="24px" height="24px" />;
-      case 'instagram':
-        return <Icons.Instagram width="24px" height="24px" />;
-      case 'linkedin':
-        return <Icons.Linkedin width="24px" height="24px" />;
-      case 'slack':
-        return <Icons.Slack width="24px" height="24px" />;
-      case 'github':
-        return <Icons.Github width="24px" height="24px" />;
-      case 'discord':
-        return <Icons.Discord width="32px" height="24px" />;
-    }
-  };
   return (
     <Wrapper>
       <Box alignItems="center" justifyContent="center" padding="8px">
-        {getIcon()}
+        {getIcon(type)}
       </Box>
     </Wrapper>
   );
