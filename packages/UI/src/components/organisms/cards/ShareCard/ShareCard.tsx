db4e77859471cbe7c3db477357e49ccf9fba1eb6
Alston Hsu <alston.hsu@outlook.com>
2023-02-21 09:42:44 -0600

    update modal with padding, width and height

--- packages/UI/src/components/organisms/cards/ShareCard/ShareCard.tsx
+++ packages/UI/src/components/organisms/cards/ShareCard/ShareCard.tsx
@@ -3,20 +3,30 @@ import { atoms } from '@devlaunchers/components/src/components';
 import Discord from '../../../../assets/icons/Discord';
 import Mail from '../../../../assets/icons/Mail';
 import Close from '../../../../assets/icons/Close';
+import Link from '../../../../assets/icons/Link';
 import type { ShareCardProps } from '.';
 
 const ShareCard = ({ title, platforms, link }: ShareCardProps) => {
   return (
-    <atoms.Layer type="light">
-      <atoms.Box padding="40px" flexDirection="row">
-        <atoms.Box flexDirection="column"></atoms.Box>
-        <atoms.Box flexDirection="column">
-          <atoms.Typography type="h5">Test</atoms.Typography>
-          <Discord width="22px" height="16px" />
-          <atoms.Typography type="label">Test</atoms.Typography>
-        </atoms.Box>
-        <atoms.Box flexDirection="column"></atoms.Box>
+    <atoms.Layer
+      type="light"
+      css={{
+        borderRadius: '10px',
+        maxWidth: '480px',
+        maxHeight: '300px',
+        padding: '12px',
+      }}
+    >
+      <atoms.Box flexDirection="column" alignItems="end">
         <Close width="22px" height="16px" />
+      </atoms.Box>
+      <atoms.Box flexDirection="column" alignItems="center">
+        <atoms.Typography type="h5">Test</atoms.Typography>
+        <Discord width="22px" height="16px" />
+      </atoms.Box>
+      <atoms.Box flexDirection="column" alignItems="center">
+        <atoms.Typography type="label">Test</atoms.Typography>
+        <Link width="22px" height="16px" />
       </atoms.Box>
     </atoms.Layer>
   );
---
+++ packages/UI/src/components/organisms/cards/ShareCard/Styled.ShareCard.ts
@@ -0,0 +1,4 @@
+import styled from 'styled-components';
+import { radiusStyles } from '../../../atoms/Radius/StyledRadius';
+import { shadowStyles } from '../../../atoms/Shadow';
+import type { ShareCardProps } from '.';
