0f9507d7010532180498e3a8cb2009220f73a7a3
remi <remitoutin@gmail.com>
2023-02-25 21:59:19 -0600

    feat(RETproject): adding an idea owner tag

--- apps/ideaspace/src/components/modules/WorkshoppingPage/SingleComment.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/SingleComment.js
@@ -12,7 +12,7 @@ const SingleCommentComponent = props => (
     <UserImage alt="user_image" src={`https://picsum.photos/70?random=${props.id}`} />
     <div className="textContent">
       <SingleCommentContent>
-        <h3>{props.author}</h3>
+        <h3>{props.author} -  idea owner</h3> {/* TODO: replace "idea onwer" w/ a variable that contains the user's role pertaining to the idea */}
         <div source={props.children} ><p>{props.children}</p></div>
       </SingleCommentContent>
       <SingleCommentButtons>
