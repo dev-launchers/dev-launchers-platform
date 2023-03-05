ea76ff87aaccde10206b8e4d5daab36485a6b58c
remi <remitoutin@gmail.com>
2023-03-04 23:33:12 -0600

    feat(RETprojects): comment author role in progress

--- apps/ideaspace/src/components/modules/WorkshoppingPage/SingleComment.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/SingleComment.js
@@ -13,7 +13,7 @@ const SingleCommentComponent = props => (
     <UserImage alt="user_image" src={`https://picsum.photos/70?random=${props.id}`} />
     <div className="textContent">
       <SingleCommentContent>
-        <h3>{props.author} -  idea owner</h3> {/* TODO: replace "idea owner" w/ a variable that contains the user's role pertaining to the idea */}
+        <h3>{props.author} - {props.author.role}</h3> {/* TODO: replace "idea owner" w/ a variable that contains the user's role pertaining to the idea (Iris is working on that) */}
         <div source={props.children} ><p>{props.children}</p></div>
       </SingleCommentContent>
       <SingleCommentButtons>
