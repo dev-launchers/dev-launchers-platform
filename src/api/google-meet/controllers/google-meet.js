75b4bd7733e799ab25b331d3a9f9ff32962e4df6
oriyomibadmus <badmus.oriyomi1@gmail.com>
2023-03-08 02:47:23 +0400

    google-meet controller

--- src/api/google-meet/controllers/google-meet.js
+++ src/api/google-meet/controllers/google-meet.js
@@ -6,12 +6,4 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::google-meet.google-meet', ({strapi}) => ({
-    userFacingMeets(internalMeet) {
-        console.log(internalMeet);
-        return {
-            name: internalMeet.name,
-            meetingCode: internalMeet.meetingCode,
-        };
-    }
-}));
+module.exports = createCoreController('api::google-meet.google-meet');
