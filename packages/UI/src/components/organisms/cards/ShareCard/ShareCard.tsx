c3faf0645e7325d115fb10bb000d02f48827d799
Alston Hsu <alston.hsu@outlook.com>
2023-03-09 14:43:54 -0600

    refactor ShareCard elements

--- packages/UI/src/components/organisms/cards/ShareCard/ShareCard.tsx
+++ packages/UI/src/components/organisms/cards/ShareCard/ShareCard.tsx
@@ -1,14 +1,20 @@
 import SocialIconButton from '../../../molecules/SocialIconButton';
 import React from 'react';
 import { atoms } from '@devlaunchers/components/src/components';
-import Close from '../../../../assets/icons/Close';
-import Link from '../../../../assets/icons/Link';
-import Button from '../../../atoms/Button';
 import type { ShareCardProps } from '.';
 import { useState } from 'react';
-import FormField from '../../../organisms/FormField';
+import Link from '../../../../assets/icons/Link';
 import ShareIdea from './ShareUrl';
-import { ShareLinkButton, ShareInput } from './StyledShareCard';
+import {
+  Container,
+  ShareLinkButton,
+  ShareInput,
+  CloseButton,
+  CloseIcon,
+  ShareIdeaText,
+  ShareLinkText,
+  ShareLinkBox,
+} from './StyledShareCard';
 
 const ShareCard = ({ title, platforms, link }: ShareCardProps) => {
   const [show, setShow] = useState(false);
@@ -25,52 +31,20 @@ const ShareCard = ({ title, platforms, link }: ShareCardProps) => {
   ] as const;
 
   return (
-    <atoms.Layer
-      type="light"
-      css={{
-        borderRadius: '10px',
-        maxWidth: '480px',
-        maxHeight: '300px',
-        padding: '12px',
-        display: 'inline-block',
-      }}
-    >
+    <Container type="light">
       <atoms.Box flexDirection="column" alignItems="end">
-        <Button
+        <CloseButton
           onClick={handleClose}
           buttonSize="standard"
           buttonType="alternative"
-          css={{
-            position: 'relative',
-            width: '30px',
-            height: '30px',
-          }}
         >
-          <Close
-            width="20px"
-            height="20px"
-            css={{
-              top: '50%',
-              left: '50%',
-              position: 'absolute',
-              transform: 'translate(-50%, -50%)',
-            }}
-          />
-        </Button>
+          <CloseIcon width="20px" height="20px" />
+        </CloseButton>
       </atoms.Box>
       <atoms.Box flexDirection="column" alignItems="center">
-        <atoms.Typography type="h5" css={{ padding: '0px 0px 22px 0px' }}>
-          Share This {title}
-        </atoms.Typography>
+        <ShareIdeaText type="h5">Share This {title}</ShareIdeaText>
       </atoms.Box>
-      <atoms.Box
-        flexDirection="row"
-        css={{
-          display: 'flex',
-          columnGap: '5px',
-          padding: '0px 0px 22px 0px',
-        }}
-      >
+      <atoms.Box flexDirection="row" gap="5px" padding="0px 0px 22px 0px">
         {ALL_SOCIAL_PLATFORMS.map((SocialPlatform, index) => (
           <SocialIconButton
             onClick={() => ShareIdea(SocialPlatform, currentLocation)}
@@ -79,22 +53,16 @@ const ShareCard = ({ title, platforms, link }: ShareCardProps) => {
           />
         ))}
       </atoms.Box>
-      <atoms.Box flexDirection="row">
-        <atoms.Typography type="label" css={{ padding: '0px 0px 0px 15px' }}>
-          Share With A Link
-        </atoms.Typography>
+      <atoms.Box flexDirection="row" gap="15px">
+        <ShareLinkText type="label">Share With A Link</ShareLinkText>
       </atoms.Box>
-      <atoms.Box flexDirection="row" css={{ position: 'relative' }}>
+      <ShareLinkBox flexDirection="row">
         <ShareLinkButton>
           <Link width="29px" height="24px" />
         </ShareLinkButton>
-        <ShareInput
-          type="text"
-          value={currentLocation}
-          style={{ paddingLeft: '55px', height: '40px' }}
-        />
-      </atoms.Box>
-    </atoms.Layer>
+        <ShareInput type="text" value={currentLocation} />
+      </ShareLinkBox>
+    </Container>
   );
 };
 
--- packages/UI/src/components/organisms/cards/ShareCard/StyledShareCard.ts
+++ packages/UI/src/components/organisms/cards/ShareCard/StyledShareCard.ts
@@ -1,26 +1,62 @@
 import styled from 'styled-components';
+import Box from '../../../atoms/Box';
+import Close from '../../../../assets/icons/Close';
+import Button from '../../../atoms/Button';
+import Layer from '../../../atoms/Layer';
 import { radiusStyles } from '../../../atoms/Radius/StyledRadius';
 import { shadowStyles } from '../../../atoms/Shadow';
+import Typography, { typographyStyles } from '../../../atoms/Typography';
 import type { ShareCardProps } from '.';
-import { typographyStyles } from '../../../atoms/Typography';
 
 export const ShareLinkButton = styled.button`
   background-color: ${({ theme }) => theme.colors.GREYSCALE_WHITE};
   color: ${({ theme }) => theme.colors.GREYSCALE_BLACK};
   border: none;
   cursor: pointer;
-  ${shadowStyles['shadow500']};
-  ${radiusStyles['radius100']};
+  ${radiusStyles.radius100};
   position: absolute;
   left: 20px;
   top: 5px;
 `;
 
 export const ShareInput = styled.input`
-  ${typographyStyles['placeHolder']};
+  ${typographyStyles.placeHolder};
   color: ${({ theme }) => theme.colors.GREYSCALE_GREY};
   border: 1px solid black;
-  ${radiusStyles['radius200']};
+  ${radiusStyles.radius200};
   padding-left: 55px;
   height: 40px;
+`;
+
+export const Container = styled(Layer)`
+  border-radius: 10px;
+  max-width: 480px;
+  max-height: 300px;
+  padding: 12px;
+  display: inline-block;
+`;
+
+export const CloseButton = styled(Button)`
+  position: relative;
+  width: 30px;
+  height: 30px;
+`;
+
+export const CloseIcon = styled(Close)`
+  top: 50%;
+  left: 50%;
+  position: absolute;
+  transform: translate(-50%; -50%);
+`;
+
+export const ShareIdeaText = styled(Typography)`
+  padding: 0px 0px 22px 0px;
+`;
+
+export const ShareLinkText = styled(Typography)`
+  padding: 0px 0px 0px 15px;
+`;
+
+export const ShareLinkBox = styled(Box)`
+  position: relative;
 `;
