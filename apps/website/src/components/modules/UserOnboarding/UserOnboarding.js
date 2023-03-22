0c39ac0ffa365b4b7bda6103a6ef60ef353642df
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-03-22 08:53:27 +0100

    Add all imports

--- apps/website/src/components/modules/UserOnboarding/UserOnboarding.js
+++ apps/website/src/components/modules/UserOnboarding/UserOnboarding.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import FormField from '../../../../../../packages/UI/src/components/organisms/FormField'
+import OpenResponse from '../../../../../../packages/UI/src/components/organisms/OpenResponse'
+import { Checkbox } from '@devlaunchers/components/components/atoms'
+import { Link } from '@devlaunchers/components/components/atoms'
 
 const UserOnboarding = () => {
     return (
-        <div>UserOnboarding</div>
+        <h1>UserOnboarding</h1>
     )
 }
 
