7617e4fb17d061c48b59d63b732bdb7d899fc681
Havre Loic <dongmoabenahavreloic030@gmail.com>
2023-03-29 10:34:06 +0100

    Move UserOnboarding component in to a new component that will contain first time user data collection through the onboarding process

---
+++ apps/website/src/components/modules/UserOnboardingProcess/UserOnboarding/OnboardingForm/StyledOnboardingForm.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const OnboardingFormSection = styled.section`
+    position: relative;
+    padding: 2rem;
+    margin-bottom: 2rem;
+    
+    .separator {
+        height: 2rem;
+    }
+    .continue-btn {
+        position: absolute;
+        bottom: 0;
+        right: 4rem;
+    }
+    .form-title-mb {
+        margin-bottom: 2.5rem;
+    }
+`
+export default OnboardingFormSection;
