e7bf68c8e9cd28bdc25ad801d157658f09b2ec86
Hredhya <hredhya3107@gmail.com>
2023-04-01 18:10:30 -0400

    feat(Hredhya) Add Progress Bar Component

--- packages/UI/src/components/molecules/ProgressBar/StyledProgressBar.ts
+++ packages/UI/src/components/molecules/ProgressBar/StyledProgressBar.ts
@@ -1,7 +1,54 @@
-import { radiusStyles } from '../../atoms/Radius';
+import type { DefaultTheme } from 'styled-components';
 import styled from 'styled-components';
+import { radiusStyles } from '../../atoms/Radius';
+import type { ProgressBarProps } from '.';
 
-export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.NOTIFICATION};
+const getColor = (percentage: number, theme: DefaultTheme) => {
+  return percentage === 100 ? theme.colors.SUCCESS : theme.colors.NOTIFICATION;
+};
+
+export const Wrapper = styled.div<ProgressBarProps>`
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  position: relative;
   ${radiusStyles['radius300']};
+  height: 26px;
+  width: 320px;
+  top: 36px;
+  background-color: ${({ theme }) => theme.colors.GREYSCALE_OFF_WHITE};
+  border: 2px solid ${({ percentage, theme }) => getColor(percentage, theme)};
+`;
+export const Progress = styled.div<ProgressBarProps>`
+  box-sizing: border-box;
+  border: 2px solid ${({ percentage, theme }) => getColor(percentage, theme)};
+  background-color: ${({ percentage, theme }) => getColor(percentage, theme)};
+  ${radiusStyles['radius300']};
+  height: 26px;
+  // top: 36px;
+  width: ${(props) => props.percentage}%;
+`;
+export const PercentageWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  position: absolute;
+  top: -26px;
+  width: 100%;
+  gap: 12px;
+`;
+export const Percentage = styled.span<ProgressBarProps>`
+  font-family: 'Nunito Sans';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 24px;
+  color: ${({ percentage, theme }) => getColor(percentage, theme)};
+  width: 35px;
+  height: 24px;
+`;
+export const CheckIcon = styled.img`
+  width: 20px;
+  height: 20px;
 `;
