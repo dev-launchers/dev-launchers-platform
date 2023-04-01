b95dd642aa119daea0dc61a2f03b8a79d69b7276
Hredhya <hredhya3107@gmail.com>
2023-04-01 18:10:30 -0400

    feat(Hredhya) Add Progress Bar Component

--- packages/UI/src/components/molecules/ProgressBar/ProgressBar.tsx
+++ packages/UI/src/components/molecules/ProgressBar/ProgressBar.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
+import Icon from '../../../assets/icons/rawSvg/check-circle.svg';
+import {
+  Wrapper,
+  Progress,
+  CheckIcon,
+  PercentageWrapper,
+  Percentage,
+} from './StyledProgressBar';
 import type { ProgressBarProps } from '.';
-import { Wrapper } from './StyledProgressBar';
 
 const ProgressBar: React.FC<ProgressBarProps> = (props) => {
-  return <Wrapper>{props.percentage}%</Wrapper>;
+  return (
+    <Wrapper percentage={props.percentage}>
+      <Progress percentage={props.percentage} />
+      <PercentageWrapper>
+        <Percentage percentage={props.percentage}>
+          {props.percentage}%
+        </Percentage>
+        {props.percentage === 100 && <CheckIcon src={Icon} />}
+      </PercentageWrapper>
+    </Wrapper>
+  );
 };
-
 export default ProgressBar;
