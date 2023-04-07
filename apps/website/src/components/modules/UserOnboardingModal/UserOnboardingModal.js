18680da03183bd4a6786157b1dc2f91d5db435c3
Jude Amos <code@judeamos.com>
2023-04-07 15:27:37 +0100

    978, Part 1, Creating onboarding modal component

---
+++ apps/website/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
@@ -0,0 +1,46 @@
+import {useState, useEffect} from "react";
+import Modal from "react-modal";
+
+import PlatformOnboarding from "./PlatformOnboarding/PlatformOnboarding";
+import Typography from "@devlaunchers/components/components/atoms/Typography";
+import { ModalContainer, userUnboardingModalStyle, ModalHeader, ModalBody } from "./StyledUserOnboardingModal";
+
+Modal.setAppElement("#__next");
+
+/**
+ * @description This is custom modal for the user onboarding. 
+ */
+export default function UserOnboardingModal({isOpen}) {
+    const [modalIsOpen, setModalIsOpen] = useState(isOpen);
+    
+    const openModal = () => {
+        setModalIsOpen(true);
+    }
+    // const afterOpenModal = () => {
+    //     // references are now sync'd and can be accessed.
+    // }
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+    return (
+        <>
+         {/* "modalIsOpen ? true : false" set this way until we start adding typescript for 
+         boolean type */}
+        <Modal
+        isOpen={modalIsOpen ? true : false}
+        onRequestOpen={openModal}
+        onRequestClose={closeModal}
+        style={userUnboardingModalStyle}
+        contentLabel="User Onboarding"
+        >
+            <ModalContainer>
+                <ModalHeader><Typography type="h4"> Modal Title</Typography></ModalHeader>
+                <ModalBody>
+                <Typography type="p"> body</Typography>                    
+                {/* Onboarding Card Component */}
+                </ModalBody>
+            </ModalContainer>
+        </Modal>
+        </>
+    );
+}
