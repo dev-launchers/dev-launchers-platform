86d3f92370fcf7290177b8df872ed8f8e7ad2aa6
oriyomibadmus <badmus.oriyomi1@gmail.com>
2023-04-07 13:48:00 +0400

    github-repo migration

--- src/api/github-repo/controllers/github-repo.js
+++ src/api/github-repo/controllers/github-repo.js
@@ -3,36 +3,41 @@
 /**
  *  github-repo controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::github-repo.github-repo' , ({ strapi }) => ({
+
     async listCodeFreq(ctx) {
       const { user, repo } = ctx.params;
-      let freq = await strapi.services['github'].repoCodeFreq(user, repo);
+      let freq = await strapi.service('api::github-repo.github-repo').repoCodeFreq(user, repo);
       const entity = {
-        url: strapi.services['github'].repoURL(user, repo),
+        url: strapi.service('api::github-repo.github-repo').repoURL(user, repo),
         changes: freq,
       };
-      return sanitizeEntity(entity, { model: strapi.models.repo });
+      return sanitizeEntity(entity, strapi.getModel('api::github-repo.github-repo'));
     },
+
     async listIssues(ctx) {
       const { user, repo } = ctx.params;
-      let issues = await strapi.services['github'].repoIssues(user, repo);
+      let issues = await strapi.service('api::github-repo.github-repo').repoIssues(user, repo);
       const entity = {
-        url: strapi.services['github'].repoURL(user, repo),
+        url: strapi.service('api::github-repo.github-repo').repoURL(user, repo),
         issues: issues,
       };
-      return sanitizeEntity(entity, { model: strapi.models.repo });
+      return sanitizeEntity(entity, { model: strapi.getModel('api::github-repo.github-repo') });
     },
+
     async listContributors(ctx) {
       const { user, repo } = ctx.params;
-      let contributors = await strapi.services['github'].repoContributors(user, repo);
+      let contributors = await strapi.service('api::github-repo.github-repo').repoContributors(user, repo);
       const entity = {
-        url: strapi.services['github'].repoURL(user, repo),
+        url: strapi.service('api::github-repo.github-repo').repoURL(user, repo),
         contributors: contributors,
       };
-      return sanitizeEntity(entity, { model: strapi.models.repo });
+      return sanitizeEntity(entity, strapi.getModel('api::github-repo.github-repo'));
     }
+
 }));
   
--- src/api/github-repo/routes/github-repo.js
+++ src/api/github-repo/routes/github-repo.js
@@ -6,4 +6,33 @@
 
 const { createCoreRouter } = require('@strapi/strapi').factories;
 
-module.exports = createCoreRouter('api::github-repo.github-repo');
+//module.exports = createCoreRouter('api::github-repo.github-repo');
+
+module.exports = {
+    "routes": [
+      {
+        method: "GET",
+        path: "/github-repo/:user/:repo/code_freq",
+        handler: "github-repo.listCodeFreq",
+        config: {
+            "policies": []
+        }
+      },
+      {
+        method: "GET",
+        path: "/github-repo/:user/:repo/issues",
+        handler: "github-repo.listIssues",
+        config: {
+            "policies": []
+        }
+      },
+      {
+        method: "GET",
+        path: "/github-repo/:user/:repo/contributors",
+        handler: "github-repo.listContributors",
+        config: {
+            "policies": []
+        }
+      }
+    ]
+}
