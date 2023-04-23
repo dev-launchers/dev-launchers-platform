f13d01d3bfa537512fb64038694c242b1871bd94
Iris <2300018151@qq.com>
2023-04-23 16:29:36 -0500

    fix(alicefate): return error page when Url wrong

--- apps/ideaspace/src/components/modules/WorkshoppingPage/WorkshoppingPage.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/WorkshoppingPage.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
+import Error from "next/error";
 import { useRouter } from 'next/router';
 import CommentList from './CommentsForm/DisplayComments';
 import CommentForm from './CommentsForm/CommentForm';
 import { IdeaOverview } from './IdeaOverview/IdeaOverview';
 import theme from '../../../styles/theme';
 import CircularIndeterminateLoader from '../Loader/CircularIndeterminateLoader'
+import useConfirm from '../../common/DialogBox/DialogBox';
 import {
   Form,
   Comments
@@ -22,31 +24,50 @@ function WorkshoppingPage() {
   const { ideaId } = router.query;
   const [handleChange, setHandleChange] = useState('');
   const [handleTextChange, setHandleTextChange] = useState('');
-  const { data, loading } = useFetchIdea(ideaId)
-  return (
-    <Container theme={theme}>
-      {loading === true ?
-        <CircularIndeterminateLoader
-          text="Loading..."
-          color="white"
-        />
-        :
-        <Wrapper>
-          <IdeaOverview selectedCard={data} />
+  const { data, loading, hidden, getError } = useFetchIdea(ideaId)
 
-          <Form>
-            <CommentForm setHandleChange={setHandleChange} data={data} handleChange={handleChange} setHandleTextChange={setHandleTextChange} handleTextChange={handleTextChange} selectedCard={data} />
-          </Form>
-
-          <Comments>
-            {/* a count of the comments in the comment feed: */}
-            {/* <h6>Comment Feed: {data.comments.length}</h6> */}
-            <CommentList selectedCard={data} />
-          </Comments>
-        </Wrapper>
-      }
-    </Container>
+  const [ArchivedIdea, confirmArchived] = useConfirm(
+    ["This Idea has been archived.", '', ''],
+    "You can't workshop on it.",
+    ['primary', 'got it', ''],
   );
+
+  React.useEffect(async () => {
+    if (hidden) {
+      await confirmArchived();
+      window.history.back(-1);
+    }
+  }, [hidden]);
+
+  if (getError) {
+    return <Error statusCode={404} title="page Not Found" />;
+  } else {
+    return (
+      <Container theme={theme}>
+        <ArchivedIdea />
+        {loading === true ?
+          <CircularIndeterminateLoader
+            text="Loading..."
+            color="white"
+          />
+          :
+          <Wrapper>
+            <IdeaOverview selectedCard={data} />
+
+            <Form>
+              <CommentForm setHandleChange={setHandleChange} data={data} handleChange={handleChange} setHandleTextChange={setHandleTextChange} handleTextChange={handleTextChange} selectedCard={data} />
+            </Form>
+
+            <Comments>
+              {/* a count of the comments in the comment feed: */}
+              {/* <h6>Comment Feed: {data.comments.length}</h6> */}
+              <CommentList selectedCard={data} />
+            </Comments>
+          </Wrapper>
+        }
+      </Container>
+    );
+  }
 }
 
 export default WorkshoppingPage;
