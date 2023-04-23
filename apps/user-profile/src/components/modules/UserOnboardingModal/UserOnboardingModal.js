c286c282351854a452041d979ff72708aea8ab2b
Jingwei Wang <wang-jing-wei@hotmail.com>
2023-04-23 11:46:28 -0500

    merge remote conflic through vscode

--- apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import Modal from "react-modal";
 import OnboardingCard from "./OnboardingCard/OnboardingCard.js";
-import LogoMonogram from '../../../images/logo-monogram.png'
 import PlatformOnboarding from "./PlatformOnboarding/PlatformOnboarding";
 import Typography from "@devlaunchers/components/components/atoms/Typography";
 import LogoMonogram from '../../../images/logo-monogram.png'
@@ -28,45 +27,49 @@ export default function UserOnboardingModal({ isOpen }) {
         <>
             {/* "modalIsOpen ? true : false" set this way until we start adding typescript for 
          boolean type */}
-        <Modal
-        isOpen={modalIsOpen ? true : false}
-        onRequestOpen={openModal}
-        onRequestClose={closeModal}
-        style={userUnboardingModalStyle}
-        contentLabel="User Onboarding"
-        >
-            <ModalContainer>
-                <ModalHeader><Typography type="h4"> Modal Title</Typography></ModalHeader>
-                <ModalBody>
-                <Typography type="p"> body</Typography>                    
-                {/* Onboarding Card Component */}
-                {/* passing a prop to decide show checked or unchecked 
-                    @iconImg: Login
-                             Onboarding
-                             Zenhub
-                             Discord
-                        type: string
-                    @title same with figma
-                    @subtitle same with figma
-                    checked: true or false based on the user
-                */}
-                <OnboardingCard 
+            <Modal
+                isOpen={modalIsOpen ? true : false}
+                onRequestOpen={openModal}
+                onRequestClose={closeModal}
+                style={userUnboardingModalStyle}
+                contentLabel="User Onboarding"
+            >
+                <ModalContainer>
+                    <ModalHeader>
+                        <img height="33.6px" src={LogoMonogram} alt="dev-launchers" />
+                        <Typography type="h4">Dev Launchers</Typography>
+                    </ModalHeader>
+                    <ModalBody>
+                        <Typography type="h2">Get Started with Devlaunchers</Typography>
+                        <Typography type="p">Please complete the following onboarding tasks</Typography>
+                        {/* Onboarding Card Component */}
+                    <OnboardingCard 
+                    iconImg={"Login"} 
+                    title={"Create Devlanchers Login"} 
+                    subtitle={"All set. Now let’s get started with the rest!"} 
+                    completed={false}
+                />
+                   <OnboardingCard 
                     iconImg={"Onboarding"} 
-                    // Platform Onboarding
-                    // Dev Launchers Platform Tour
-                    // Create Devlanchers Login
-                    // All set. Now let’s get started with the rest!
-                    // Sign up on Zenhub and github
-                    // Connect your account on these platforms
-                    // Sign up on discord
-                    // Create an account to communicate with your team
+                    title={"Platform Onboarding"} 
+                    subtitle={"Dev Launchers Platform Tour"} 
+                    completed={false}
+                />
+                  <OnboardingCard 
+                    iconImg={"Zenhub"} 
+                    title={"Sign up on Zenhub and github"} 
+                    subtitle={"Connect your account on these platforms"} 
+                    completed={false}
+                />
+                 <OnboardingCard 
+                    iconImg={"Discord"} 
                     title={"Sign up on discord"} 
                     subtitle={"Create an account to communicate with your team"} 
-                    completed={true}
+                    completed={false}
                 />
-                </ModalBody>
-            </ModalContainer>
-        </Modal>
+                    </ModalBody>
+                </ModalContainer>
+            </Modal>
         </>
     );
 }
