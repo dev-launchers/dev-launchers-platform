12090ed1ded91134dd5e9d55d130ec9f7931b6e0
Kris Gano <kris@pyxld.com>
2023-05-22 14:08:57 -0500

    Add editor notification for leaders to Project Pages, alongside small page touchups

---
+++ apps/site-projects/src/components/modules/Projects/Project/EditorNotification/StyledEditorNotification.js
@@ -0,0 +1,62 @@
+
+import styled from "styled-components";
+
+
+export const Wrapper = styled.div`
+   font-family: "Abel";
+  align-content: center;
+  text-align: center;
+  display: grid;
+  /* flex-wrap: wrap ; */
+  row-gap: 1rem;
+  margin: auto;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  h3 {
+    align-content: center;
+    text-align: center;
+    margin: auto;
+    font-size: 2rem;
+    }
+ p {
+    align-content: center;
+    text-align: center;
+    margin: auto;
+    /* flex: 4; */
+  }
+  @media (max-width: 52em) {
+    flex-direction: row;
+  }
+`;
+
+export const ButtonsContainer = styled.div`
+ display: flex;
+  flex-direction: row;
+  column-gap: 2rem;
+  
+  margin: auto;
+  i {
+    font-size: 1.5rem;
+  }
+
+  @media (max-width: 52em) {
+    flex-direction: row;
+  }
+`;
+
+export const Button = styled.a`
+  font-size: 1.13rem;
+  display: flex;
+  justify-content: center;
+  margin: auto;
+  align-items: center;
+  gap: 0.5rem;
+  width: 12em;
+  padding: 0.5rem;
+  background-color: ${({ bgColor }) =>
+  bgColor || "4A9D48"};
+  color: #F0EDEE;
+  white-space: nowrap;
+  border-radius: 5px;
+  word-wrap: no;
+`;
