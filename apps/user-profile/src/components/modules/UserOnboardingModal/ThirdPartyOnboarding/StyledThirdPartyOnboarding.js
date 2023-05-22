cdf56e60b713d83288bd9b424881128570f04f3f
Noel Moon <nmoon778@gmail.com>
2023-05-22 11:22:28 -0400

    created third party signup page

---
+++ apps/user-profile/src/components/modules/UserOnboardingModal/ThirdPartyOnboarding/StyledThirdPartyOnboarding.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Body = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  gap: 0px;
+`;
--- apps/user-profile/src/components/modules/UserOnboardingModal/ThirdPartyOnboarding/ThirdPartyOnboarding.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/ThirdPartyOnboarding/ThirdPartyOnboarding.js
@@ -1,4 +1,13 @@
+import Typography from '@devlaunchers/components/components/atoms/Typography';
+import { Body } from './StyledThirdPartyOnboarding';
 
 export default function ThirdPartyOnboarding() {
-    return(<></>);
+  return (
+    <Body>
+      <Typography type="h2">Sign Up with Third Party Sites</Typography>
+      <Typography type="p">
+        These are the platforms we’ll use to collaborate on projects
+      </Typography>
+    </Body>
+  );
 }
