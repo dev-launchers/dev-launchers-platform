f26f025754ef900197f2a6be7ff8c4fc0dce2745
mfvil <rarefyev@gmail.com>
2023-05-28 14:34:36 -0500

    Roman/tailwind-implemetation: tailwind config.

---
+++ packages/tailwind-constructor/build.js
@@ -0,0 +1,86 @@
+const StyleDictionaryPackage = require('style-dictionary');
+const {createArray} = require('./fns');
+
+// HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
+
+StyleDictionaryPackage.registerFormat({
+  name: 'css/variables',
+  formatter: function (dictionary, config) {
+    return `${this.selector} {\n${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}\n}`
+  }
+});
+
+StyleDictionaryPackage.registerTransform({
+    name: 'sizes/px',
+    type: 'value',
+    matcher: function(prop) {
+        // You can be more specific here if you only want 'em' units for font sizes    
+        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing", "Highlight", "Shadow"].includes(prop.attributes.category);
+    },
+    transformer: function(prop) {
+        // You can also modify the value here if you want to convert pixels to ems
+        console.log(prop);
+
+        switch (prop.attributes.category) {
+          case 'fontSizes':
+              return parseFloat(prop.original.value) + 'rem';
+          case 'spacing':
+              return parseFloat(prop.original.value) + 'rem';
+          case 'borderRadius':
+              return parseFloat(prop.original.value) + 'rem';
+          case 'borderWidth':
+              return parseFloat(prop.original.value) + 'rem';
+          case 'sizing':
+              return parseFloat(prop.original.value) + 'rem';
+          case 'Highlight':
+          case 'Shadow':
+              const {x, y, blur, spread, color} = prop.original.value;
+              return prop.original.value ? `${x}px ${y}px ${blur}px ${spread}px ${color}` : 'none';
+          default:
+              return prop.original.value;
+  }
+}});
+
+function getStyleDictionaryConfig(theme) {
+  return {
+    "source": [
+      `tokens/${theme}.json`,
+    ],
+    "format": {
+      createArray
+    },
+    "platforms": {
+      "web": {
+        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
+        "buildPath": `output/`,
+        "files": [{
+          "destination": `${theme}.json`,
+          "format": "createArray"
+        }, {
+          "destination": `${theme}.css`,
+          "format": "css/variables",
+          "selector": `.${theme}-theme`
+        }]
+      }
+    }
+  };
+}
+
+console.log('Build started...');
+
+// PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
+
+['core', 'dark', 'light'].map(function (theme) {
+
+    console.log('\n==============================================');
+    console.log(`\nProcessing: [${theme}]`);
+
+    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
+
+    StyleDictionary.buildPlatform('web');
+
+    console.log('\nEnd processing');
+})
+
+console.log('\n==============================================');
+console.log('\nBuild completed!');
---
+++ packages/tailwind-constructor/tailwind.config.js
@@ -0,0 +1,15 @@
+
+const { filterTokensByType } = require("./fns");
+// const tokens = require("./output/light.json")
+
+// const colors = filterTokensByType('color', tokens)
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    // colors,
+  },
+  variants: {},
+  plugins: [],
+}
