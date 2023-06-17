5f86e8ae31b1a1d220eec67a41b591a5d2a6d4b0
jamilsonh <2jamilson@gmail.com>
2023-06-17 20:22:56 -0300

    build: convert text on singup form to lower case

--- apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
+++ apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
@@ -157,7 +157,7 @@ export default function SignUpForm() {
               </atoms.Box>
               <atoms.Box flexDirection="column">
                 <atoms.Typography type="pSmall">
-                  HOW MANY HOURS A WEEK WOULD YOU LIKE TO VOLUNTEER?
+                  How many hours a week would you like to volunteer?
                 </atoms.Typography>
 
                 <atoms.Slider
