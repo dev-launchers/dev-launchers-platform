a6a215ef6ad9890924a4d9a0c439271db1a3651f
jordanmcousins <52937546+jordanmcousins@users.noreply.github.com>
2023-06-29 19:54:44 -0400

    Add like button to idea card

--- apps/ideaspace/src/components/common/IdeaCard/IdeaCard.js
+++ apps/ideaspace/src/components/common/IdeaCard/IdeaCard.js
@@ -2,30 +2,36 @@ import React, { useState } from 'react';
 import Link from 'next/link';
 import axios from 'axios';
 import { atoms } from '@devlaunchers/components/src/components';
-import {
-  ImgButton,
-  StatuBox,
-} from './StyledIdeaCard';
+import { ImgButton, StatuBox } from './StyledIdeaCard';
 import IdeaCardImg from './IdeaCardImg';
 import IdeaCardTag from './IdeaCardTag';
 import IdeaCardComment from './IdeaCardComment';
 import IdeaCardUpdated from './IdeaCardUpdated';
 import useConfirm from '../DialogBox/DialogBox';
+import { LikeButton } from '@devlaunchers/components/src/components/molecules';
+import { useUserDataContext } from '@devlaunchers/components/src/context/UserDataContext';
 
 function IdeaCard({ cards, cardType }) {
   const [tagContent, setTagContent] = useState(cards.status);
   const [buttonContent, setButtonContent] = useState('');
   const [urlPath, setUrlPath] = useState('');
+  const [isFilled, setIsFilled] = useState(false);
+  const [liked, setLiked] = useState(false);
+  const { userData } = useUserDataContext();
 
   const [UpdateFailure, confirmFailure] = useConfirm(
     ['Unable to reactivate your idea', '', ''],
     'Please try again.',
-    ['primary', 'close'],
+    ['primary', 'close']
   );
 
+  const handleToggleLike = () => {
+    setIsFilled(!isFilled);
+  };
+
   React.useEffect(() => {
-    if (cardType == "mine") {
-      if (tagContent !== "archived") {
+    if (cardType == 'mine') {
+      if (tagContent !== 'archived') {
         setButtonContent(`WORKSHOP THIS IDEA`);
       } else {
         setButtonContent(`REACTIVATE THIS IDEA`);
@@ -38,7 +44,7 @@ function IdeaCard({ cards, cardType }) {
   }, [tagContent]);
 
   const reactivateIdea = async () => {
-    cards["status"] = "workshopping";
+    cards['status'] = 'workshopping';
     setButtonContent(`WAIT`);
 
     try {
@@ -48,52 +54,62 @@ function IdeaCard({ cards, cardType }) {
       );
 
       if (res.status === 200) {
-        setTagContent("workshopping");
+        setTagContent('workshopping');
       }
     } catch (error) {
       confirmFailure();
       setButtonContent(`REACTIVATE THIS IDEA`);
     }
-  }
+  };
 
   return (
-    <atoms.Box flexDirection='column'
-      style={{ border: "0.05rem solid rgba(240, 237, 238, 1)", borderRadius: "1rem" }}
+    <atoms.Box
+      flexDirection="column"
+      style={{
+        border: '0.05rem solid rgba(240, 237, 238, 1)',
+        borderRadius: '1rem',
+      }}
     >
-
       <atoms.Box>
-        <IdeaCardTag
-          status={tagContent}
-        />
+        <IdeaCardTag status={tagContent} />
       </atoms.Box>
 
-      <IdeaCardImg
-        cardId={cards.id}
-        cardImg={cards.imgSrc}
-      />
+      <IdeaCardImg cardId={cards.id} cardImg={cards.imgSrc} />
 
       <Link href={{ pathname: urlPath }}>
-        <atoms.Box flexDirection='column' alignItems='flex-start' justifyContent='space-between'
-          padding='0rem 2rem 2rem' style={{ maxWidth: '18.5rem' }}>
-
-          <atoms.Typography type='h3' style={{ fontSize: '1.5rem', marginBottom: '2rem' }}>
+        <atoms.Box
+          flexDirection="column"
+          alignItems="flex-start"
+          justifyContent="space-between"
+          padding="0rem 2rem 2rem"
+          style={{ maxWidth: '18.5rem' }}
+        >
+          <atoms.Typography
+            type="h3"
+            style={{ fontSize: '1.5rem', marginBottom: '2rem' }}
+          >
             {cards.ideaName}
           </atoms.Typography>
 
-          <IdeaCardComment
-            commentLength={cards.comments.length}
-          />
+          <IdeaCardComment commentLength={cards.comments.length} />
 
-          <IdeaCardUpdated
-            updatedAt={cards.updated_at}
-          />
+          <IdeaCardUpdated updatedAt={cards.updated_at} />
         </atoms.Box>
       </Link>
 
-      {tagContent == "archived" ? (
+      <atoms.Box padding="0rem 2rem 2rem">
+        <LikeButton
+          onClick={() => setLiked((prev) => !prev)}
+          filled={liked}
+          text={liked ? 1 : ''}
+          disabled={useUserDataContext().isAuthenticated ? false : true}
+        ></LikeButton>
+      </atoms.Box>
+
+      {tagContent == 'archived' ? (
         <atoms.Button
-          buttonSize='standard'
-          buttonType='alternative'
+          buttonSize="standard"
+          buttonType="alternative"
           style={{ margin: '0rem 2rem 1.5rem' }}
           onClick={reactivateIdea}
         >
@@ -102,8 +118,8 @@ function IdeaCard({ cards, cardType }) {
       ) : (
         <Link href={{ pathname: urlPath }}>
           <atoms.Button
-            buttonSize='standard'
-            buttonType='alternative'
+            buttonSize="standard"
+            buttonType="alternative"
             style={{ margin: '0rem 2rem 1.5rem' }}
           >
             {buttonContent}
@@ -111,9 +127,8 @@ function IdeaCard({ cards, cardType }) {
         </Link>
       )}
       <UpdateFailure />
-
     </atoms.Box>
-  )
+  );
 }
 
-export default IdeaCard
+export default IdeaCard;
