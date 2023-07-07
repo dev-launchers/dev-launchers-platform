749f75b0accf41e371110842ffe4aba70d0b58b1
Enjoy2Live <iMohammedMaqbol@gmail.com>
2023-07-07 20:59:30 +0300

    improve-alert-component

--- packages/UI/src/components/molecules/Alert/Alert.tsx
+++ packages/UI/src/components/molecules/Alert/Alert.tsx
@@ -4,14 +4,32 @@ import Typography from '../../atoms/Typography';
 import { Container } from './StyledAlert';
 import type { AlertProps } from '.';
 
+const variants = {
+  open: { opacity: 1, y: 0 },
+  closed: { opacity: 0, y: '-100%' },
+};
+
 const Alert: React.FC<React.PropsWithChildren<AlertProps>> = ({
   signal = 'notify',
   handleClose,
+  isInitallyOpen = true,
   children,
   ...rest
 }) => {
+  const [isOpen, setIsOpen] = React.useState(isInitallyOpen);
   return (
-    <Container signal={signal} {...rest}>
+    <Container
+      signal={signal}
+      {...rest}
+      variants={variants}
+      initial={{ opacity: 0, y: '-100%' }}
+      animate={isOpen ? 'open' : 'closed'}
+      transition={{
+        type: 'spring',
+        stiffness: 260,
+        damping: 20,
+      }}
+    >
       {signal === 'error' ? (
         <Icons.Error />
       ) : signal === 'success' ? (
@@ -22,8 +40,13 @@ const Alert: React.FC<React.PropsWithChildren<AlertProps>> = ({
         <></>
       )}
       <Typography css={{ flex: 1 }}>{children}</Typography>
-      <button>
-        <Icons.Close onClick={handleClose} />
+      <button
+        onClick={(e) => {
+          setIsOpen((prev) => !prev);
+          handleClose?.(e);
+        }}
+      >
+        <Icons.Close />
       </button>
     </Container>
   );
