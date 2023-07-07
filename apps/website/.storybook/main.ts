42c432216ad647b5291001d0a53ad81fcffc9411
Enjoy2Live <iMohammedMaqbol@gmail.com>
2023-07-07 22:45:45 +0300

    feat: added storybook to website

---
+++ apps/website/.storybook/main.ts
@@ -0,0 +1,15 @@
+import type { StorybookConfig } from '@storybook/nextjs';
+const path = require('path');
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+  ],
+  framework: path.resolve(require.resolve('@storybook/nextjs/preset'), '..'),
+  docs: {
+    autodocs: 'tag',
+  },
+};
+export default config;
