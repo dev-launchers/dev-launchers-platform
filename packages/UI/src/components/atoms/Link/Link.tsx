68241cf529767a00dae78bf55a0b8ffbba3a25aa
Alston Hsu <alston.hsu@outlook.com>
2023-07-17 15:11:49 -0500

    update percy config

--- packages/UI/src/components/atoms/Link/Link.tsx
+++ packages/UI/src/components/atoms/Link/Link.tsx
@@ -2,7 +2,7 @@ import type { FC } from 'react';
 import { StyledLink } from './StyledLink';
 import type { LinkProps } from './index';
 
-const Link: FC<LinkProps> = ({ href, text, props }) => {
+const Link: FC<LinkProps> = ({ href, text, ...props }) => {
   return (
     <StyledLink href={href} {...props}>
       {text}
