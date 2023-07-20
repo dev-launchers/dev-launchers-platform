0b35c96bb157b4f1dfcdc92e20f4866ba9741b36
devanshu <devanshusenp@gmail.com>
2023-07-20 01:13:31 -0500

    fixed warning on vision.js page

--- apps/site-projects/src/components/modules/Projects/Project/Vision/Vision.js
+++ apps/site-projects/src/components/modules/Projects/Project/Vision/Vision.js
@@ -1,42 +1,39 @@
-import React from "react";
-import { useTheme, withTheme } from "styled-components";
-import Section from "../Section";
-import { Wrapper, ButtonsContainer, Button } from "./StyledVision";
-import Link from "next/link";
+import React from 'react';
+import { useTheme, withTheme } from 'styled-components';
+import Section from '../Section';
+import { Wrapper, ButtonsContainer, Button } from './StyledVision';
+import Link from 'next/link';
 
-const Vision = ({ scrollMethods,vision }) => {
-  const theme = useTheme()
-  if (vision === ""){
+const Vision = ({ scrollMethods, vision }) => {
+  const theme = useTheme();
+  if (vision === '') {
     return null;
   }
-  return(
-  <Section
-    bgColor="#3A7CA5"
-    Title="Vision"
-    Content={
-      <Wrapper>
-        <p>
-          {vision}
-        </p>
-        <ButtonsContainer>
-        <Link href="../join" passHref>
-          <Button
-            style={{ cursor: "pointer" }}
-          >
-            <i className="fas fa-info"></i> Join Now
-          </Button>
-          <Link href="../support-us" passHref>
-            <Button
-              style={{ cursor: "pointer" }}
-              bgColor={theme?.colors?.ACCENT_2}
-            >
-              <i className="fas fa-coins"></i> Donate
-            </Button>
-          </Link>
-        </ButtonsContainer>
-      </Wrapper>
-    }
-  />
-);
+  return (
+    <Section
+      bgColor="#3A7CA5"
+      Title="Vision"
+      Content={
+        <Wrapper>
+          <p>{vision}</p>
+          <ButtonsContainer>
+            <Link href="../join" passHref>
+              <Button style={{ cursor: 'pointer' }}>
+                <i className="fas fa-info"></i> Join Now
+              </Button>
+            </Link>
+            <Link href="../support-us" passHref>
+              <Button
+                style={{ cursor: 'pointer' }}
+                bgColor={theme?.colors?.ACCENT_2}
+              >
+                <i className="fas fa-coins"></i> Donate
+              </Button>
+            </Link>
+          </ButtonsContainer>
+        </Wrapper>
+      }
+    />
+  );
 };
 export default withTheme(Vision);
