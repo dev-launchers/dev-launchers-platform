c80f9c3f0cbebc8836e000c7a7b78c8206483942
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-07-21 18:55:24 +0100

    added styles

---
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/TeamLinks/StyledTeamLinks.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const ResourceCardsContainer = styled.ul`
+    display:flex;
+    flex-wrap: wrap;
+    gap: 2rem;
+    margin: 0 auto;
+    justify-content: center;
+`;
