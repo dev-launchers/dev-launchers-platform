e8ba2531bbf1d835ba9005581056856db5691931
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-07-21 19:08:25 +0100

    styling resources section

--- apps/site-projects/src/components/modules/Resources/ProjectResources/StyledProjectResources.js
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/StyledProjectResources.js
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const PrejectResourcesContainer = styled.div`
     background: white;
     border-radius: 1rem;
-    padding: 2rem 3rem;
+    padding: 3rem;
     margin: 4rem 0;
 `;
