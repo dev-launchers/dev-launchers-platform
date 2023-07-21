e380d4e902f87d97bc615a762feb226193ddfa96
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-07-21 17:36:40 +0100

    created resources page

---
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/ProjectResources.js
@@ -0,0 +1,12 @@
+import TeamLinks from "./TeamLinks";
+
+function ProjectResources() {
+    return (
+        <div>
+            <h3>Project A resources</h3>
+            <TeamLinks/>
+        </div>
+        );
+}
+
+export default ProjectResources;
