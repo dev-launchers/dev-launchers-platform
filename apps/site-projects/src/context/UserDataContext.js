47286f16d2f6c5904058e7e35fa1598b7d40bbd8
HelioLucio <helio.lucio.jr@hotmail.com>
2023-07-27 16:00:39 -0300

    add role and current team in UserContext

--- apps/site-projects/src/context/UserDataContext.js
+++ apps/site-projects/src/context/UserDataContext.js
@@ -24,6 +24,35 @@ const DEFAULT_USER = {
   },
 };
 
+function findUserRole(userId, dataArray) {
+  for (const data of dataArray) {
+    const team = data.team;
+    const leaders = team.leaders;
+    const members = team.members;
+
+    // Search for the user ID in leaders array
+    const leaderMatch = leaders.find(leader => leader.id === userId);
+
+    // If user ID is not found in leaders, search in members array
+    const memberMatch = members.find(member => member.id === userId);
+
+    // Check if user ID is found in either leaders or members
+    if (leaderMatch || memberMatch) {
+      const teamName = data.title;
+      const userRole = leaderMatch ? leaderMatch.role : memberMatch.role;
+
+      // Return object containing team name and user role
+      return {
+        team: teamName,
+        role: userRole
+      };
+    }
+  }
+
+  // If user ID is not found in any object, return null or any appropriate value
+  return null;
+}
+
 // Built from this article: https://www.sitepoint.com/replace-redux-react-hooks-context-api/
 
 // Step 1: Create a custom hook that contains your state and actions
@@ -49,6 +78,28 @@ function useUserData() {
           availablePoints: currentUser.point.availablePoints,
           volunteerHours: currentUser.point.volunteerHours,
         });
+
+        axios(`${env().STRAPI_URL}/projects?_publicationState=live`, {
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "User-Agent":
+              "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.107 Safari/537.36",
+          },
+        })
+          .then(({ data: projects }) => {
+            const positionInfo = findUserRole(currentUser.id, projects);
+  
+            setUserData(prevUserData => ({
+              ...prevUserData,
+              position: {
+                role: positionInfo.role,
+                team: positionInfo.team,
+              },
+            }));
+  
+          })
+          .catch(() => {});
+  
       })
       .catch(() => {
         // setUserData({ id: "invalid" });
