8c4f85a4e7258a96dbfb03f80ed355e01fe6dd8d
Vanel <vanelnwaba@gmail.com>
2023-07-27 04:23:18 +0100

    create the filter page

---
+++ apps/dev-recruiters/src/pages/filter.tsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import { ThemeProvider } from 'styled-components'
+import FilterPage from '../components/modules/FilterPage';
+import Head from 'next/head';
+import theme from '../styles/theme';
+
+const filter = () => {
+  return (
+      <>
+      <Head>
+      <title>Filter page</title>
+      <meta name="title" content="Dev Discovery"></meta>
+      <meta
+        name="description"
+        content="Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!"
+      ></meta>
+
+      <meta property="og:type" content="website"></meta>
+      <meta
+        property="og:url"
+        content="https://devlaunchers.org/projects"
+      ></meta>
+      <meta
+        property="og:image"
+        content="/images/DevlaunchersGitHubThumb.png"
+      ></meta>
+      <meta property="og:title" content="Dev Discovery"></meta>
+      <meta
+        property="og:description"
+        content="Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!"
+      ></meta>
+
+      <meta property="twitter:card" content="summary_large_image"></meta>
+      <meta
+        property="twitter:url"
+        content="https://devlaunchers.org/projects"
+      ></meta>
+      <meta property="twitter:title" content="Dev Discovery"></meta>
+      <meta
+        property="twitter:description"
+        content="Create, discover, and join open-source software projects! We help members to contribute meaningfully and gain industry-ready experience along the way. Build epic products, tools, and games used by real people while learning valuable skills and meeting awesome people!"
+      ></meta>
+      <meta
+        property="twitter:image"
+        content="/images/DevlaunchersGitHubThumb.png"
+      ></meta>
+      <meta content="#ff7f0e" data-react-helmet="true" name="theme-color" />
+          </Head>
+          <ThemeProvider theme={theme}>
+              <FilterPage/>
+          </ThemeProvider>
+      </>
+  )
+}
+
+export default filter
