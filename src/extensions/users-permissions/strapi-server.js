a7700ff9951eb19ea1d309d7318bff9fa4bd3933
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-07-29 20:18:38 +0100

    fix(chungthuang): Add endpoint to logout

--- src/extensions/users-permissions/strapi-server.js
+++ src/extensions/users-permissions/strapi-server.js
@@ -1,7 +1,13 @@
 const { jwt } = require('./server/middlewares');
+const { logout } = require('./controllers/auth');
+const auth_routes = require('./routes/content-api/auth');
 
 module.exports = (plugin) => {
   plugin.middlewares['jwt'] = jwt
   plugin.services['providers-registry'] = require('./server/services/providers-registry');
+  plugin.controllers.auth.logout = logout;
+  for (const route of auth_routes) {
+    plugin.routes['content-api'].routes.push(route);
+  }
   return plugin;
 };
