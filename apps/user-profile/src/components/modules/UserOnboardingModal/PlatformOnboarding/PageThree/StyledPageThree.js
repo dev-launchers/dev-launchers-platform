e09ef9037c53803fdec8f37b23f85a4cba219cd6
rinabo <rinaneman96@gmail.com>
2023-07-30 19:21:02 -0500

    Impoted image source as SVG

--- apps/user-profile/src/components/modules/UserOnboardingModal/PlatformOnboarding/PageThree/StyledPageThree.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/PlatformOnboarding/PageThree/StyledPageThree.js
@@ -1,10 +1,13 @@
 import styled from "styled-components";
 
 export const PageThereeContainer = styled.div`
+    padding: 10%;
     text-align: center;
 `
 
 export const ThreeRoleCards = styled.div`
+    padding-left: 5%;
+    padding-right: 5%;
     display: flex;
     flex-direction: row;
 `
