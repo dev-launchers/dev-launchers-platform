7e01ebb1850b4f1a0d3efb37c88e425fab74e5ae
Vanel <vanelnwaba@gmail.com>
2023-08-04 05:05:17 +0100

    create and style the filterPage

--- apps/dev-recruiters/src/components/modules/FilterPage/index.tsx
+++ apps/dev-recruiters/src/components/modules/FilterPage/index.tsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import RoleCard from './RoleCard'
+import BoxContainer from '../../common/BoxContainer';
+import { FilterConatiner, ResultContainer, ResultTitle, RolesContainer } from './styledFilterPage'
 
 const FilterPage = () => {
   return (
-    <RoleCard/>
+    <div>
+
+      <ResultTitle>Search Result</ResultTitle>
+      <ResultContainer>
+        <FilterConatiner>
+        </FilterConatiner>
+        <RolesContainer>
+          <RoleCard />
+          <RoleCard />
+          <RoleCard />
+          <RoleCard/>
+        </RolesContainer>
+
+      </ResultContainer>
+       
+    </div>
+   
   )
 }
 
---
+++ apps/dev-recruiters/src/components/modules/FilterPage/styledFilterPage.jsx
@@ -0,0 +1,37 @@
+import styled from 'styled-components';
+
+export const HeadingContainer = styled.div`
+height: 15rem;
+flex-shrink: 0;
+background-color:rgba(0, 0, 0, 0.63)
+`;
+
+export const Headingtext = styled.div`
+color:#fff;
+`;
+
+export const ResultTitle = styled.div`
+width: 100%;
+text-align: center;
+margin-top: 100px;
+font-size: 2rem;
+font-weight: 500;
+`
+
+export const ResultContainer = styled.div`
+width: 100%;
+display: flex;
+padding: 2rem;
+`
+
+export const FilterConatiner = styled.div`
+background-color: gray;
+width: 20%;
+height: 20vh;
+`
+
+export const RolesContainer = styled.div`
+width: 75%;
+display: flex;
+flex-wrap: wrap;
+`
