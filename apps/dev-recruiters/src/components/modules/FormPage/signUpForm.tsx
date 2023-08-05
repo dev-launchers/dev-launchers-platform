13dd1abc886b51236c24efd85f4f57e805c6d8dc
Kirubel <takirubel@gmail.com>
2023-08-05 15:41:59 -0500

    update checkbox style

--- apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
+++ apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
@@ -1,21 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { Field, Form, Formik, FormikHelpers } from 'formik';
-import * as Yup from 'yup';
-import { useRouter } from 'next/router';
-import { useUserDataContext } from '../../../contexts/UserDataContext';
-import { NewApplicant } from '@devlaunchers/models/newApplicant';
-import { SkillLevel } from '@devlaunchers/models/level';
 import { atoms, organisms } from '@devlaunchers/components/src/components';
-import { agent } from '@devlaunchers/utility';
+import theme from '@devlaunchers/components/src/styles/theme';
 import FormErrorScroller from '@devlaunchers/components/src/utils/formErrorScroller';
+import { Opportunity } from '@devlaunchers/models';
+import { NewApplicant } from '@devlaunchers/models/newApplicant';
+import { agent } from '@devlaunchers/utility';
+import { Field, Form, Formik, FormikHelpers } from 'formik';
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
+import * as Yup from 'yup';
+import Checkbox from '../../common/Checkbox/CheckBox';
+import ConfirmationModal from '../DetailedPage/Confirmation/ConfirmationModal';
 import {
   CloseButton,
   CloseIcon,
-  ColorBox,
 } from '../DetailedPage/PositionCard/StyledPositionCard';
 import { GradientLine } from './styledSignupForm';
-import { Opportunity } from '@devlaunchers/models';
-import ConfirmationModal from '../DetailedPage/Confirmation/ConfirmationModal';
 
 interface FormFields extends Omit<NewApplicant, 'level'> {
   level: NewApplicant['level'] | '';
@@ -46,13 +45,16 @@ export default function SignUpForm({ handleCloseModal, position }: Props) {
     accepted: Yup.boolean().required('Acceptance Field Entry is Required'),
   });
   const [showConfirmationModal, setShowConfirmationModal] = useState(false);
+  const [checkCheckbox, setCheckCheckbox] = useState();
+
+  const handleSetCheckCheckbox = () => {
+    setCheckCheckbox(checkCheckbox!);
+  };
 
   const handleOpenConfirmationModal = () => {
     setShowConfirmationModal(true);
   };
-  const handleCloseConfirmationModal = () => {
-    setShowConfirmationModal(false);
-  };
+
   // const router = useRouter();
   // const { userData } = useUserDataContext();
 
@@ -64,221 +66,231 @@ export default function SignUpForm({ handleCloseModal, position }: Props) {
   // }, [router, userData.id]);
 
   return (
-    <Formik
-      initialValues={{
-        discordUsername: '',
-        name: '',
-        email: '',
-        age: 0,
-        level: 'Intermediate',
-        accepted: false,
-        commitment: 0,
-        extraInfo: '',
-        portfolioLink: null,
-        experience: '',
-        reason: '',
-        zip: 0,
-        role: position.title as string,
-        id: position.id as string,
-        // project: router.query.slug as string,
-        skills: [{ skill: '' }],
-      }}
-      onSubmit={(
-        values: NewApplicant,
-        { setSubmitting }: FormikHelpers<NewApplicant>
-      ) => {
-        setSubmitting(true);
-        agent.Applicant.post({
-          ...values,
-          //@ts-ignore
-          level: values.level.toLowerCase(),
-          skills: values.skills
-            .toString()
-            .split(',')
-            .map((skill) => ({ skill: skill })),
+    <ThemeProvider theme={theme}>
+      <Formik
+        initialValues={{
+          discordUsername: '',
+          name: '',
+          email: '',
+          age: 0,
+          level: 'Intermediate',
+          accepted: false,
+          commitment: 0,
+          extraInfo: '',
+          portfolioLink: null,
+          experience: '',
+          reason: '',
+          zip: 0,
           role: position.title as string,
-          // project:  router.query.project as string,
           id: position.id as string,
-        })
-          .then((res) => {
-            handleCloseModal();
-            setSubmitting(false);
-            handleOpenConfirmationModal();
+          // project: router.query.slug as string,
+          skills: [{ skill: '' }],
+        }}
+        onSubmit={(
+          values: NewApplicant,
+          { setSubmitting }: FormikHelpers<NewApplicant>
+        ) => {
+          setSubmitting(true);
+          agent.Applicant.post({
+            ...values,
+            //@ts-ignore
+            level: values.level.toLowerCase(),
+            skills: values.skills
+              .toString()
+              .split(',')
+              .map((skill) => ({ skill: skill })),
+            role: position.title as string,
+            // project:  router.query.project as string,
+            id: position.id as string,
           })
-          .catch((error) => {
-            setSubmitting(false);
-            console.log(error);
-            console.log(error.response);
-            console.log(error.response.data);
-            console.log(error.response.status);
-          });
-      }}
-      validationSchema={SignupSchema}
-    >
-      {({ errors, setFieldValue, touched, values }) => (
-        <atoms.Box paddingInline="0.5rem" justifyContent="center">
-          <CloseButton onClick={handleCloseModal}>
-            <CloseIcon
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M6 18L18 6M6 6l12 12"
-              />
-            </CloseIcon>
-          </CloseButton>
-          <Form>
-            <atoms.Box flexDirection="column" margin="auto">
-              <atoms.Box flexDirection="column">
-                <atoms.Layer hasRainbow>
-                  <atoms.Typography type="h2" textAlign="center">
-                    Volunteer Application for <strong>{position.title}</strong>
-                  </atoms.Typography>
-                </atoms.Layer>
-              </atoms.Box>
-              <GradientLine />
-              <atoms.Box
-                flexDirection="column"
-                gap="32px"
-                paddingBlock="2rem"
-                maxWidth="fit-content"
+            .then((res) => {
+              handleCloseModal();
+              setSubmitting(false);
+              handleOpenConfirmationModal();
+            })
+            .catch((error) => {
+              setSubmitting(false);
+              console.log(error);
+              console.log(error.response);
+              console.log(error.response.data);
+              console.log(error.response.status);
+            });
+        }}
+        validationSchema={SignupSchema}
+      >
+        {({ errors, setFieldValue, touched, values }) => (
+          <atoms.Box paddingInline="0.5rem" justifyContent="center">
+            <CloseButton onClick={handleCloseModal}>
+              <CloseIcon
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
               >
-                <Field
-                  as={organisms.FormField}
-                  label="Your Full Name"
-                  placeholder="John Smith"
-                  id="name"
-                  name="name"
-                  required
-                  // onChange={handleChange}
-                  touched={touched['name']}
-                  error={errors.name}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
                 />
-                <Field
-                  as={organisms.FormField}
-                  label="Your Email"
-                  placeholder="johnsmith@gmail.com"
-                  id="email"
-                  name="email"
-                  required
-                  touched={touched['email']}
-                  error={errors.email}
-                />
-                <atoms.Box gap="32px" flexDirection="column">
+              </CloseIcon>
+            </CloseButton>
+            <Form>
+              <atoms.Box flexDirection="column" margin="auto">
+                <atoms.Box flexDirection="column">
+                  <atoms.Layer hasRainbow>
+                    <atoms.Typography type="h2" textAlign="center">
+                      Volunteer Application for{' '}
+                      <strong>{position.title}</strong>
+                    </atoms.Typography>
+                  </atoms.Layer>
+                </atoms.Box>
+                <GradientLine />
+                <atoms.Box
+                  flexDirection="column"
+                  gap="32px"
+                  paddingBlock="2rem"
+                  maxWidth="fit-content"
+                >
                   <Field
                     as={organisms.FormField}
-                    label={
-                      <atoms.Box gap="1rem" alignItems="center">
-                        What are your relevant skills?
-                        <atoms.ToolTip
-                          content="Please Separate skills with a comma."
-                          direction="left"
-                          delay={100}
-                        >
-                          ℹ️
-                        </atoms.ToolTip>
-                      </atoms.Box>
-                    }
-                    placeholder="javascript, react, backend"
-                    id="skills"
-                    name="skills"
+                    label="Your Full Name"
+                    placeholder="John Smith"
+                    id="name"
+                    name="name"
+                    required
+                    // onChange={handleChange}
+                    touched={touched['name']}
+                    error={errors.name}
                   />
-                </atoms.Box>
-                <atoms.Box flexDirection="column">
-                  <atoms.Typography type="pSmall">
-                    HOW MANY HOURS A WEEK WOULD YOU LIKE TO VOLUNTEER?
-                  </atoms.Typography>
+                  <Field
+                    as={organisms.FormField}
+                    label="Your Email"
+                    placeholder="johnsmith@gmail.com"
+                    id="email"
+                    name="email"
+                    required
+                    touched={touched['email']}
+                    error={errors.email}
+                  />
+                  <atoms.Box gap="32px" flexDirection="column">
+                    <Field
+                      as={organisms.FormField}
+                      label={
+                        <atoms.Box gap="1rem" alignItems="center">
+                          What are your relevant skills?
+                          <atoms.ToolTip
+                            content="Please Separate skills with a comma."
+                            direction="left"
+                            delay={100}
+                          >
+                            ℹ️
+                          </atoms.ToolTip>
+                        </atoms.Box>
+                      }
+                      placeholder="javascript, react, backend"
+                      id="skills"
+                      name="skills"
+                    />
+                  </atoms.Box>
+                  <atoms.Box flexDirection="column">
+                    <atoms.Typography type="pSmall">
+                      HOW MANY HOURS A WEEK WOULD YOU LIKE TO VOLUNTEER?
+                    </atoms.Typography>
 
-                  <atoms.Slider
-                    min={5}
-                    max={40}
-                    initialValue={5}
-                    onChange={(value) => setFieldValue('commitment', +value)}
-                    withLabels
-                    suffix=" hrs"
-                    maxWidth="430px"
+                    <atoms.Slider
+                      min={5}
+                      max={40}
+                      initialValue={5}
+                      onChange={(value) => setFieldValue('commitment', +value)}
+                      withLabels
+                      suffix=" hrs"
+                      maxWidth="430px"
+                    />
+                    <atoms.Typography type="pSmall" css={{ color: 'red' }}>
+                      {errors.commitment}
+                    </atoms.Typography>
+                  </atoms.Box>
+                  <Field
+                    as={organisms.OpenResponse}
+                    cols={50}
+                    touched={touched['experience']}
+                    error={errors.experience}
+                    label="Please briefly describe your experience in development or design"
+                    placeholder="My experience with development / design is..."
+                    required
+                    rows={5}
+                    id="experience"
+                    name="experience"
+                    // onChange={handleChange}
                   />
-                  <atoms.Typography type="pSmall" css={{ color: 'red' }}>
-                    {errors.commitment}
+                  <Field
+                    as={organisms.OpenResponse}
+                    cols={50}
+                    label="Why would you like to be a Dev Launcher?"
+                    placeholder="My experience with development / design is..."
+                    rows={5}
+                    id="reason"
+                    name="reason"
+                    // onChange={handleChange}
+                  />
+                  <Field
+                    as={organisms.OpenResponse}
+                    cols={50}
+                    label="Anything else you would like to share with us?"
+                    placeholder="I just want the Team Lead to know..."
+                    rows={5}
+                    id="extraInfo"
+                    name="extraInfo"
+                    // onChange={handleChange}
+                  />
+                  <Field
+                    as={organisms.FormField}
+                    label="Portfolio/Resume Link"
+                    placeholder="https://myportfolio.com"
+                    id="portfolioLink"
+                    name="portfolioLink"
+                    // onChange={handleChange}
+                    touched={touched.portfolioLink && !!values['portfolioLink']}
+                    error={errors.portfolioLink}
+                  />
+                  <atoms.Typography type="p">
+                    We require users to be 18 years old or older. Please confirm
+                    below.
                   </atoms.Typography>
-                </atoms.Box>
-                <Field
-                  as={organisms.OpenResponse}
-                  cols={50}
-                  touched={touched['experience']}
-                  error={errors.experience}
-                  label="Please briefly describe your experience in development or design"
-                  placeholder="My experience with development / design is..."
-                  required
-                  rows={5}
-                  id="experience"
-                  name="experience"
-                  // onChange={handleChange}
-                />
-                <Field
-                  as={organisms.OpenResponse}
-                  cols={50}
-                  label="Why would you like to be a Dev Launcher?"
-                  placeholder="My experience with development / design is..."
-                  rows={5}
-                  id="reason"
-                  name="reason"
-                  // onChange={handleChange}
-                />
-                <Field
-                  as={organisms.OpenResponse}
-                  cols={50}
-                  label="Anything else you would like to share with us?"
-                  placeholder="I just want the Team Lead to know..."
-                  rows={5}
-                  id="extraInfo"
-                  name="extraInfo"
-                  // onChange={handleChange}
-                />
-                <Field
-                  as={organisms.FormField}
-                  label="Portfolio/Resume Link"
-                  placeholder="https://myportfolio.com"
-                  id="portfolioLink"
-                  name="portfolioLink"
-                  // onChange={handleChange}
-                  touched={touched.portfolioLink && !!values['portfolioLink']}
-                  error={errors.portfolioLink}
-                />
-                <atoms.Typography type="p">
-                  We require users to be 18 years old or older. Please confirm
-                  below.
-                </atoms.Typography>
-                <atoms.Checkbox
-                  label="I am 18 years old or older."
-                  disabled={false}
-                  required
-                />
-                <atoms.Box maxWidth="50%">
-                  <atoms.Button
-                    buttonSize="standard"
-                    buttonType="primary"
-                    type="submit"
-                  >
-                    Submit
-                  </atoms.Button>
+                  <Checkbox
+                    id={''}
+                    label={'I am 18 years old or older.'}
+                    checked={checkCheckbox}
+                    onChange={handleSetCheckCheckbox}
+                  />
+                  <atoms.Checkbox
+                    label="I am 18 years old or older."
+                    disabled={false}
+                    onChange={handleSetCheckCheckbox}
+                    required
+                  />
+                  <atoms.Box maxWidth="50%">
+                    <atoms.Button
+                      buttonSize="standard"
+                      buttonType="primary"
+                      type="submit"
+                    >
+                      Submit
+                    </atoms.Button>
+                  </atoms.Box>
                 </atoms.Box>
               </atoms.Box>
-            </atoms.Box>
-            <FormErrorScroller focusAfterScroll />
-            <ConfirmationModal
-              showModal={showConfirmationModal}
-              handleOpenModal={handleOpenConfirmationModal}
-              handleCloseModal={handleCloseModal}
-            />
-          </Form>
-        </atoms.Box>
-      )}
-    </Formik>
+              <FormErrorScroller focusAfterScroll />
+              <ConfirmationModal
+                showModal={showConfirmationModal}
+                handleOpenModal={handleOpenConfirmationModal}
+                handleCloseModal={handleCloseModal}
+              />
+            </Form>
+          </atoms.Box>
+        )}
+      </Formik>
+    </ThemeProvider>
   );
 }
