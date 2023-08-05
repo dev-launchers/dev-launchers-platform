962b5abff28b12ddf6ff84a345d1155dbf3fedfe
Kirubel <takirubel@gmail.com>
2023-08-05 15:11:46 -0500

    connect confirmation screen with apply

--- apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
+++ apps/dev-recruiters/src/components/modules/FormPage/signUpForm.tsx
@@ -95,15 +95,13 @@ export default function SignUpForm({ handleCloseModal, position }: Props) {
             .split(',')
             .map((skill) => ({ skill: skill })),
           role: position.title as string,
-          // project: router.query.project as string,
+          // project:  router.query.project as string,
           id: position.id as string,
         })
           .then((res) => {
-            setSubmitting(false);
             handleCloseModal();
+            setSubmitting(false);
             handleOpenConfirmationModal();
-
-            // router.push('/join/confirmation');
           })
           .catch((error) => {
             setSubmitting(false);
@@ -257,7 +255,7 @@ export default function SignUpForm({ handleCloseModal, position }: Props) {
                 <atoms.Checkbox
                   label="I am 18 years old or older."
                   disabled={false}
-                  // required
+                  required
                 />
                 <atoms.Box maxWidth="50%">
                   <atoms.Button
@@ -274,7 +272,7 @@ export default function SignUpForm({ handleCloseModal, position }: Props) {
             <ConfirmationModal
               showModal={showConfirmationModal}
               handleOpenModal={handleOpenConfirmationModal}
-              handleCloseModal={handleCloseConfirmationModal}
+              handleCloseModal={handleCloseModal}
             />
           </Form>
         </atoms.Box>
