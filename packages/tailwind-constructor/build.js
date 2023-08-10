9d63b1fe24ef1a7a782850f2c4a015da089b83ce
Enjoy2Live <iMohammedMaqbol@gmail.com>
2023-08-11 00:51:35 +0300

    fix tailwind transpiling tokens wrongly

--- packages/tailwind-constructor/build.js
+++ packages/tailwind-constructor/build.js
@@ -1,75 +1,134 @@
 const StyleDictionaryPackage = require('style-dictionary');
-const {createArray} = require('./fns');
+const { createArray } = require('./fns');
 
 // HAVE THE STYLE DICTIONARY CONFIG DYNAMICALLY GENERATED
 
 StyleDictionaryPackage.registerFormat({
   name: 'css/variables',
   formatter: function (dictionary, config) {
-    return `${this.selector} {\n${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}\n}`
-  }
+    const typographyProps = dictionary.allProperties.filter(
+      ({ type }) => type === 'typography'
+    );
+    const typographyValues = typographyProps.map(({ value }) =>
+      Object.fromEntries(
+        Object.entries(value).map(([key, value]) => [
+          key
+            .split(/(?=[A-Z])/)
+            .join('-')
+            .toLowerCase(),
+          value,
+        ])
+      )
+    );
+    return `${this.selector} {\n${dictionary.allProperties
+      .filter(({ type }) => type !== 'typography')
+      .map((prop) => `  --${prop.name}: ${prop.value};`)
+      .join('\n')}\n}
+        \n${typographyProps
+          .map(
+            ({ name }, i) =>
+              `.${name} {\n${Object.entries(typographyValues[i])
+                .map(
+                  ([nameOfRule, rule]) =>
+                    `  ${
+                      nameOfRule === 'paragraph-spacing'
+                        ? nameOfRule.replace('paragraph', 'word')
+                        : nameOfRule.replace('case', 'transform')
+                    }: ${rule};`
+                )
+                .join('\n')}\n}`
+          )
+          .join('\n')}
+      `;
+  },
 });
 
 //creater a new style dictionary transform
 
 StyleDictionaryPackage.registerTransform({
-    name: 'sizes/px',
-    type: 'value',
-   
-    matcher: function(prop) {
-      console.log(prop);
-        // You can be more specific here if you only want 'em' units for font sizes    
-        return ["fontSizes", "spacing", "borderRadius", "borderWidth", "sizing", "Highlight", "Shadow", "Universal", "Link Text", "Default", "Mobile"].includes(prop.attributes.category);
-    },
-    transformer: function(prop) {
-        // You can also modify the value here if you want to convert pixels to ems
-      
+  name: 'sizes/px',
+  type: 'value',
 
-        switch (prop.attributes.category) {
-          case 'fontSizes':
-          case 'spacing':
-          case 'borderRadius':
-          case 'borderWidth':
-          case 'sizing': 
-              return parseFloat(prop.original.value) + 'rem';
-          case 'Default':
-          case 'Mobile':
-          case 'Link Text':  
-          case 'Universal':
-              const {fontFamily, fontSize, fontWeight, lineHeight, letterSpacing, paragraphSpacing, textCase, textDecoration} = prop.original.value;
-              return prop.original.value ? `${fontFamily} ${fontSize}px ${fontWeight} ${lineHeight}px ${letterSpacing} ${paragraphSpacing} ${textCase} ${textDecoration}` : 'none';
-          case 'Highlight':
-          case 'Shadow':
-              const {x, y, blur, spread, color} = prop.original.value;
-              return prop.original.value ? `${x}px ${y}px ${blur}px ${spread}px ${color}` : 'none';
-          default:
-              return prop.original.value;
-  }
-}});
+  matcher: function (prop) {
+    // console.log(prop);
+    // You can be more specific here if you only want 'em' units for font sizes
+    return [
+      'fontSizes',
+      'spacing',
+      'borderRadius',
+      'borderWidth',
+      'sizing',
+      'Highlight',
+      'Shadow',
+      'Universal',
+      'Link Text',
+      'Default',
+      'Mobile',
+    ].includes(prop.attributes.category);
+  },
+  transformer: function (prop) {
+    // You can also modify the value here if you want to convert pixels to ems
+
+    switch (prop.attributes.category) {
+      case 'fontSizes':
+      case 'spacing':
+      case 'borderRadius':
+      case 'borderWidth':
+      case 'sizing':
+        return parseFloat(prop.original.value) + 'rem';
+      case 'Default':
+      case 'Mobile':
+      case 'Link Text':
+      case 'Universal':
+        const {
+          fontFamily,
+          fontSize,
+          fontWeight,
+          lineHeight,
+          letterSpacing,
+          paragraphSpacing,
+          textCase,
+          textDecoration,
+        } = prop.original.value;
+        return prop.original.value
+          ? `${fontFamily} ${fontSize}px ${fontWeight} ${lineHeight}px ${letterSpacing} ${paragraphSpacing} ${textCase} ${textDecoration}`
+          : 'none';
+      case 'Highlight':
+      case 'Shadow':
+        const { x, y, blur, spread, color } = prop.original.value;
+        return prop.original.value
+          ? `${x}px ${y}px ${blur}px ${spread}px ${color}`
+          : 'none';
+      default:
+        return prop.original.value;
+    }
+  },
+});
 
 function getStyleDictionaryConfig(theme) {
   return {
-    "source": [
-      `tokens/${theme}.json`,
-    ],
-    "format": {
-      createArray
+    source: [`tokens/${theme}.json`],
+    format: {
+      createArray,
     },
-    "platforms": {
-      "web": {
-        "transforms": ["attribute/cti", "name/cti/kebab", "sizes/px"],
-        "buildPath": `output/`,
-        "files": [{
-          "destination": `${theme}.json`,
-          "format": "createArray"
-        }, {
-          "destination": `${theme}.css`,
-          "format": "css/variables",
-          "selector": `.${theme}-theme`
-          //pass over the theme name to the css/variables formatter
-        }]
-      }
-    }
+    platforms: {
+      web: {
+        transforms: ['attribute/cti', 'name/cti/kebab', 'sizes/px'],
+        buildPath: `output/`,
+        files: [
+          {
+            destination: `${theme}.json`,
+            format: 'createArray',
+          },
+          {
+            destination: `${theme}.css`,
+            format: 'css/variables',
+            selector: `.${theme}-theme`,
+            //pass over the theme name to the css/variables formatter
+          },
+        ],
+      },
+    },
   };
 }
 
@@ -78,16 +137,17 @@ console.log('Build started...');
 // PROCESS THE DESIGN TOKENS FOR THE DIFFEREN BRANDS AND PLATFORMS
 
 ['core', 'dark', 'light'].map(function (theme) {
+  console.log('\n==============================================');
+  console.log(`\nProcessing: [${theme}]`);
 
-    console.log('\n==============================================');
-    console.log(`\nProcessing: [${theme}]`);
+  const StyleDictionary = StyleDictionaryPackage.extend(
+    getStyleDictionaryConfig(theme)
+  );
+  //extend the style dictionary config with typography
+  StyleDictionary.buildPlatform('web');
 
-    const StyleDictionary = StyleDictionaryPackage.extend(getStyleDictionaryConfig(theme));
-//extend the style dictionary config with typography
-    StyleDictionary.buildPlatform('web');
-
-    console.log('\nEnd processing');
-})
+  console.log('\nEnd processing');
+});
 
 console.log('\n==============================================');
 console.log('\nBuild completed!');
--- packages/tailwind-constructor/tailwind.config.js
+++ packages/tailwind-constructor/tailwind.config.js
@@ -1,45 +1,25 @@
 const { filterTokensByType } = require('./fns');
 const tokens = require('./output/core.json');
 
-const categories = [
-  'color',
-  'lineHeights',
-  'letterSpacing',
-  'paragraphSpacing',
-  'boxShadow',
-  'dropShadow',
-  'innerShadow',
-  'fontFamilies',
-  'fontWeights',
-  'typography',
-  'textCase',
-  'textDecoration',
-  'borderRadius',
-  'spacing',
-  'sizing',
-  'fontSizes',
-];
-const compatibleTokens = categories
-  .map((category) => filterTokensByType(category, tokens))
-  .reduce((r, c) => Object.assign(r, c), {});
-
-function removeRedundant(obj) {
-  return Object.fromEntries(
-    Object.entries(obj).map(([key, value]) => {
-      const category = `${key.split('-')[0]}-`;
-      if (typeof value === 'object') {
-        return removeRedundant(value);
-      }
-      return [key.replace(category, ''), value.replace(category, '')];
-    })
-  );
-}
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: removeRedundant(compatibleTokens),
+  content: {
+    relative: true,
+    files: [
+      '../UI/src/components/**/*.{html,js,jsx,tsx}',
+      '../../apps/app/src/**/*.{html,js,jsx,ts,tsx}',
+      '../../apps/dev-recruiters/src/**/*.{html,js,jsx,ts,tsx}',
+      '../../apps/ideaspace/src/**/*.{html,js,jsx,ts,tsx}',
+      '../../apps/site-projects/src/**/*.{html,js,jsx,ts,tsx}',
+      '../../apps/website/src/**/*.{html,js,jsx,ts,tsx}',
+    ],
+  },
+  // darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: filterTokensByType('color', tokens),
+    fontFamily: filterTokensByType('fontFamilies', tokens),
+    spacing: filterTokensByType('spacing', tokens),
+  },
   variants: {},
   plugins: [],
 };
