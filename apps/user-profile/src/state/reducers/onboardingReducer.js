555f7218093eef5e4337c6e39616748b1cb18a77
Jude Amos <code@judeamos.com>
2023-08-13 15:49:25 +0100

    adding state implementation p1

---
+++ apps/user-profile/src/state/reducers/onboardingReducer.js
@@ -0,0 +1,41 @@
+
+import { onboardingActions } from './../actions';
+
+export const initialOnboardingState = {
+    showIntroductionModal: true,
+    showPlatformOnboardingModal: false,
+}
+
+export const onboardingReducer = (state, action) => {
+    switch (action.type) {
+        case onboardingActions.SHOW_PLATFORM_ONBOARDING_MODAL: {
+            return {
+                ...state,
+                showIntroductionModal: false,
+                showPlatformOnboardingModal: true
+            }
+        }
+        case onboardingActions.SHOW_INTRODUCTION_MODAL: {
+            return {
+                ...state,
+                showPlatformOnboardingModal:false,
+                showIntroductionModal: true
+            }
+        }
+        case onboardingActions.HIDE_PLATFORM_ONBOARDING_MODAL: {
+            return {
+                ...state,
+                showPlatformOnboardingModal: false
+            }
+        }
+        case onboardingActions.HIDE_INTRODUCTION_MODAL: {
+            return {
+                ...state,
+                showIntroductionModal: false
+            }
+        }
+        default: {
+            return state;
+        }
+    }
+}
