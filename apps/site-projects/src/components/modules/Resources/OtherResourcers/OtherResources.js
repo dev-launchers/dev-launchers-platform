922a0e03106facb9c28e92ead1ef81f96ed3ed25
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-14 15:16:49 -0300

    fix: names and adding links

--- apps/site-projects/src/components/modules/Resources/OtherResourcers/OtherResources.js
+++ apps/site-projects/src/components/modules/Resources/OtherResourcers/OtherResources.js
@@ -1,6 +1,6 @@
 import { OtherResourcesContainer, TitleResources, OtherResourcersCards, CardsBtn, OtherResourcersIcon, OtherResourcersIconInfo } from "./StyledOtherResources";
-import Vector from '@/public/images/projects/resources/logos/Vector.png';
-import AI from '@/public/images/projects/resources/logos/AI.png';
+import StoryBook from '@/public/images/projects/resources/logos/Vector.png';
+import Strapi from '@/public/images/projects/resources/logos/strapi.svg';
 import chromatic from '@/public/images/projects/resources/logos/chromatic.svg';
 import moreInfo from '@/public/images/projects/resources/logos/moreInfo.svg';
 
@@ -10,31 +10,37 @@ import moreInfo from '@/public/images/projects/resources/logos/moreInfo.svg';
 const OtherResources = () => {
 
   return (
-    <OtherResourcesContainer>
-<TitleResources>
+  <OtherResourcesContainer>
+  <TitleResources>
   Other Resources
 
   </TitleResources>
 
   <OtherResourcersCards>
-  <OtherResourcersIcon src={chromatic} alt="Strapi AI" />
-  <CardsBtn>Designer System</CardsBtn>
-   <OtherResourcersIconInfo src={moreInfo} />
+  <OtherResourcersIcon src={chromatic} alt="chromatic" />
+  <a href="https://www.chromatic.com/start" target="_blank">
+  <CardsBtn>Chromatic</CardsBtn>
+  </a>
+  <OtherResourcersIconInfo src={moreInfo} alt="more info" />
   </OtherResourcersCards>
   <OtherResourcersCards>
-    <OtherResourcersIcon src={Vector} alt="Strapi AI" />
-  <CardsBtn>Designer System</CardsBtn>
-   <OtherResourcersIconInfo src={moreInfo} />
+  <OtherResourcersIcon src={StoryBook} alt="storybook" />
+  <a href="https://storybook.js.org/" target="_blank">
+  <CardsBtn>StoryBook</CardsBtn>
+  </a>
+  <OtherResourcersIconInfo src={moreInfo} alt="more info"  />
   </OtherResourcersCards>
   <OtherResourcersCards>
    
-    <OtherResourcersIcon src={AI} alt="Strapi AI" />
-    <CardsBtn>Strapi AI</CardsBtn>
-    <OtherResourcersIconInfo src={moreInfo} />
+  <OtherResourcersIcon src={Strapi} alt="strapi"/>
+  <a href="https://github.com/dev-launchers/strapiv4/" target="_blank">
+  <CardsBtn>Strapi</CardsBtn>
+  </a>
+  <OtherResourcersIconInfo src={moreInfo} alt="more info"  />
   </OtherResourcersCards>
 
  
-    </OtherResourcesContainer>
+  </OtherResourcesContainer>
   )
 
 }
