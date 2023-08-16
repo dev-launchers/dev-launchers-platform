445d7648fb85854d2c20f6b6bde47a02b7cf1800
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-16 17:48:46 -0300

    feat:main resourcers projects, fix: styles,

--- apps/site-projects/src/components/modules/Resources/ProjectResources/TeamLinks/TeamLinks.js
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/TeamLinks/TeamLinks.js
@@ -1,15 +1,20 @@
 import ResourceCard from "./ResourceCard";
-import { ResourceCardsContainer } from "./StyledTeamLinks";
+import {  ResourcesContainer, TitleResources  } from "./StyledResourceCard";
+import moreInfo from '@/public/images/projects/resources/logos/moreInfo.svg';
   
 function TeamLinks({ selectedCard }) {
     return (
         <div>
             <h3>{selectedCard ? selectedCard.title : "None project available"}</h3>
-            <ResourceCardsContainer>
+            <ResourcesContainer>
+
+            <TitleResources>
+            Project Resources
+            </TitleResources>
                 {
                     linksList.map((resource,i)=> <ResourceCard key={i} resource={resource} />)
                 }
-            </ResourceCardsContainer>
+            </ResourcesContainer>
         </div>
     )
 }
@@ -21,36 +26,42 @@ const linksList = [
         img: "figma.svg",
         title: "project files",
         guideTitle : "figma",
-        guideLink : "https://creativemarket.com/blog/figma-guide"
+        guideLink : "https://creativemarket.com/blog/figma-guide",
+        moreInfo: moreInfo
     },
     {
         img: "figma.svg",
         title: "design system",
         guideTitle : "figma",
-        guideLink : ""
+        guideLink : "",
+        moreInfo: moreInfo
     },
     {
         img: "google-drive.svg",
         title: "project files",
         guideTitle : "google drive",
-        guideLink : "https://www.androidauthority.com/how-to-use-google-drive-860193/"
+        guideLink : "https://www.androidauthority.com/how-to-use-google-drive-860193/",
+        moreInfo: moreInfo
     },
     {
         img: "discord.svg",
         title: "discord",
         guideTitle : "discord",
-        guideLink : "https://support.discord.com/hc/en-us/articles/360045138571-Beginner-s-Guide-to-Discord"
+        guideLink : "https://support.discord.com/hc/en-us/articles/360045138571-Beginner-s-Guide-to-Discord",
+        moreInfo: moreInfo
     },
     {
         img: "github.svg",
         title: "github",
         guideTitle : "gitub",
-        guideLink : "https://docs.github.com/en/get-started/quickstart/hello-world"
+        guideLink : "https://docs.github.com/en/get-started/quickstart/hello-world",
+        moreInfo: moreInfo
     },
     {
         img: "zenhub.svg",
         title: "zenhub",
         guideTitle : "zenhub",
-        guideLink : "https://medium.com/chingu/zenhub-101-the-essential-guide-da46ce7aff9d"
+        guideLink : "https://medium.com/chingu/zenhub-101-the-essential-guide-da46ce7aff9d",
+        moreInfo: moreInfo
     },
 ];
