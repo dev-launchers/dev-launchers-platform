bf3d12f436d3d6566043f08599de93e116e12777
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-16 17:48:46 -0300

    feat:main resourcers projects, fix: styles,

--- apps/site-projects/src/components/modules/Resources/ProjectResources/ProjectResources.js
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/ProjectResources.js
@@ -3,9 +3,9 @@ import TeamLinks from "./TeamLinks";
 
 function ProjectResources({ selectedCard }) {
     return (
-        <PrejectResourcesContainer>
+       
             <TeamLinks selectedCard={selectedCard} />
-        </PrejectResourcesContainer>
+  
         );
 }
 
