786f9f5d7cb31b06595353fdbf98bc85c7c38810
jamilsonh <2jamilson@gmail.com>
2023-08-17 09:44:07 -0300

    feat: create the logic of time commitment

--- apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/RolesFilterList/CollapsibleContainerFilter/Commitment/index.tsx
+++ apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/RolesFilterList/CollapsibleContainerFilter/Commitment/index.tsx
@@ -1,22 +1,43 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { CommitmentText, IconImage, TimeCommitment } from './styles';
 import CheckBox from '../../../../../../../images/icons/checkbox.png';
 import StyledCheckbox from './checkBox';
+import { useOpportunitiesContext } from '../../../../../../../contexts/SelectRoleContext';
 
-function Commitment({ handleCommitmentFilter, isFilterActive }) {
+function Commitment() {
+  const [selectedCheckbox, setSelectedCheckbox] = useState(null);
+  const [checkBoxLow, setCheckBoxLow] = useState(null);
+  const [checkBoxMed, setCheckBoxMed] = useState(null);
+  const [checkBoxHigh, setCheckBoxHigh] = useState(null);
+
+  const { selectedRole, setCommitmentRange, commitmentRange } =
+    useOpportunitiesContext();
+
   const handleCheckboxChange = (min, max) => {
-    if (isFilterActive) {
-      handleCommitmentFilter(null);
+    setCheckBoxLow(min === checkBoxLow ? null : min);
+    setCheckBoxMed(min === checkBoxMed ? null : min);
+    setCheckBoxHigh(min === checkBoxHigh ? null : min);
+
+    if (min === checkBoxLow || min === checkBoxMed || min === checkBoxHigh) {
+      setCommitmentRange(null);
     } else {
-      handleCommitmentFilter({ min, max });
+      setCommitmentRange({ min, max });
     }
   };
+
+  console.log(selectedCheckbox);
+
+  useEffect(() => {
+    console.log('commitmentRange:', commitmentRange);
+  }, [commitmentRange]);
+
+  console.log(selectedRole);
 
   return (
     <>
       <TimeCommitment>
         <StyledCheckbox
-          isChecked={isFilterActive}
+          isChecked={checkBoxLow === 0}
           onChange={() => handleCheckboxChange(0, 4)}
         />
         <CommitmentText>Low (0-4 hrs/wk)</CommitmentText>
@@ -24,7 +45,7 @@ function Commitment({ handleCommitmentFilter, isFilterActive }) {
 
       <TimeCommitment>
         <StyledCheckbox
-          isChecked={isFilterActive}
+          isChecked={checkBoxMed === 5}
           onChange={() => handleCheckboxChange(5, 9)}
         />
         <CommitmentText>Med (5-9 hrs/wk)</CommitmentText>
@@ -32,7 +53,7 @@ function Commitment({ handleCommitmentFilter, isFilterActive }) {
 
       <TimeCommitment>
         <StyledCheckbox
-          isChecked={isFilterActive}
+          isChecked={checkBoxHigh === 10}
           onChange={() => handleCheckboxChange(10, 20)}
         />
         <CommitmentText>High (10-20 hrs/wk)</CommitmentText>
