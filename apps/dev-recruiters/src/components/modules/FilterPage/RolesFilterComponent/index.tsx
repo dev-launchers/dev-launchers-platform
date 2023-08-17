603df338c9cb1ce85ad7f44bc828e465988f51b7
jamilsonh <2jamilson@gmail.com>
2023-08-17 11:44:05 -0300

    fix: bug fix
    rebase

--- apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/index.tsx
+++ apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/index.tsx
@@ -1,9 +1,13 @@
 import { Project, ProjectLite } from '@devlaunchers/models/project';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { useEffect } from 'react';
 =======
 import { useEffect, useState } from 'react';
 >>>>>>> 1fc468f4 (feat: creating logic to change the button color when active)
+=======
+import { useEffect } from 'react';
+>>>>>>> e1eeac24 (fix: bug fix)
 import { Opportunity } from '@devlaunchers/models/opportunity';
 import useProjectRole from '../../NewJoinPageComponent/UseProjectRole/index';
 import RolesFilterList from '../RolesFilterComponent/RolesFilterList';
