91d0e7014cffc228274acbbc49648bc8b33ab01c
HelioLucio <helio.lucio.jr@hotmail.com>
2023-08-18 18:35:39 -0300

    first commit

---
+++ apps/site-projects/src/components/modules/Resources/SectionResources/SectionResources.js
@@ -0,0 +1,11 @@
+import { SectionResourcesContainer } from "./StyledSectionResources";
+
+function SectionResources({ children }) {
+    return (
+        <SectionResourcesContainer>
+            {children}
+        </SectionResourcesContainer>
+    )
+}
+
+export default SectionResources;
---
+++ apps/site-projects/src/components/modules/Resources/TeamResources/StyledTeamResources.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const TeamResourcesContainer = styled.fieldset`
+    display: flex;
+    gap: 2rem;
+    background: white;
+    border: 1px solid #B9B4B4 ;
+    /* border-radius: 1rem; */
+    padding: 2rem 3rem;
+    margin: 1.5rem 0;
+    padding-bottom: 2rem;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+
+    > legend {
+        font-family: "Abel",sans-serif;
+        font-size: 2.3rem;
+        font-weight: bold;
+        padding: 0 1.5rem;
+    }
+`
