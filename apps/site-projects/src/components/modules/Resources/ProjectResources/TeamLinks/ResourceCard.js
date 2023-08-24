6cd1ab0bf4165523fac118979c4ec2467a9e5f9b
kamui-Hebertt <heberttaferreira.com@gmail.com>
2023-08-24 04:38:55 -0300

    feat: button from UI lib, fix Button components themes, adding btn in other resources

--- apps/site-projects/src/components/modules/Resources/ProjectResources/TeamLinks/ResourceCard.js
+++ apps/site-projects/src/components/modules/Resources/ProjectResources/TeamLinks/ResourceCard.js
@@ -1,13 +1,17 @@
 
 import { ResourcersCards, ResourcersIcon, CardsBtn, ResourcersIconInfo } from "./StyledResourceCard";
+import Button from "@devlaunchers/components/src/components/atoms/Button";
 
 function ResourceCard({resource : {img, title, guideTitle,guideLink, moreInfo}}) {
+ 
     return (
            <ResourcersCards>
             <ResourcersIcon src={`/images/projects/resources/logos/${img}`}  alt={title}/>
             <a href={guideLink} target="_blank">
-            <CardsBtn>{title}</CardsBtn>
+            <Button buttonSize="standard" buttonType="secondary">{title}</Button>
             </a>
+
+            
             <ResourcersIconInfo src={moreInfo} /> 
             </ResourcersCards>
     );
