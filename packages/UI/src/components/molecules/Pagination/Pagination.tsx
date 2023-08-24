46a22ef59ec9e82ddf409c6f86187f291535c209
Alston Hsu <alston.hsu@outlook.com>
2023-08-24 10:22:50 -0500

    debug tailwind styling

--- packages/UI/src/components/molecules/Pagination/Pagination.tsx
+++ packages/UI/src/components/molecules/Pagination/Pagination.tsx
@@ -1,15 +1,68 @@
 import React, { FC } from 'react';
-import { tv, VariantProps } from 'tailwind-variants';
+import { tv, type VariantProps } from 'tailwind-variants';
 
 const paginationStyles = tv({
-  base: 'inline-flex flex-col gap-2,5 rounded-[10px] py-2 px-4 bg-GreyScale-white hover:bg-GreyScale-off-white',
+  base: 'justify-center',
 });
 
-interface PaginationProps extends VariantProps<typeof paginationStyles>  {}
+type PaginationProps = VariantProps<typeof paginationStyles>;
 
 const Pagination: FC<PaginationProps> = ({}) => {
-  const styles = paginationStyles();
-  return <div className={styles}>1</div>;
+  return (
+    <nav>
+      <ul className={paginationStyles()}>
+        <li>
+          <a href="www.google.com">Previous</a>
+        </li>
+        <li>
+          <a href="www.google.com" className={paginationStyles()}>
+            1
+          </a>
+        </li>
+        <li>
+          <a
+            href="www.google.com"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            2
+          </a>
+        </li>
+        <li>
+          <a
+            href="www.google.com"
+            aria-current="page"
+            className="flex items-center justify-center px-3 h-8 text-blue-600 border border-gray-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
+          >
+            3
+          </a>
+        </li>
+        <li>
+          <a
+            href="www.google.com"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            4
+          </a>
+        </li>
+        <li>
+          <a
+            href="www.google.com"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            5
+          </a>
+        </li>
+        <li>
+          <a
+            href="www.google.com"
+            className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
+          >
+            Next
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Pagination;
