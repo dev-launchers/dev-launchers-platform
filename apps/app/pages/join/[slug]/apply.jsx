a9fb638cc3ae339a2df377ebbb88ca3b4f8ca40a
jamilsonh <2jamilson@gmail.com>
2023-08-31 16:52:14 -0300

    fix: bug fix

--- apps/app/pages/join/[slug]/apply.jsx
+++ apps/app/pages/join/[slug]/apply.jsx
@@ -1,4 +1,4 @@
-import Page from '@devlaunchers/dev-recruiters/src/pages/[slug]/apply';
+/*import Page from '@devlaunchers/dev-recruiters/src/pages/[slug]/apply';
 import App from '@devlaunchers/dev-recruiters/src/pages/_app';
 export { getStaticProps, getStaticPaths } from '@devlaunchers/dev-recruiters/src/pages/[slug]/apply';
 
@@ -6,4 +6,4 @@ export { getStaticProps, getStaticPaths } from '@devlaunchers/dev-recruiters/src
 
 import { constructAppPage } from '../../../utils/routingTools.js';
 export default constructAppPage(App, Page);
-
+*/
