7eb07f272f49f514610e6c44e1ab16590acc641a
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-09-04 22:11:57 +0100

    adding styles to the meetings components

--- apps/site-projects/src/components/modules/Resources/TeamMeetings/MeetingTimeCard/StyledMeetingTimeCard.js
+++ apps/site-projects/src/components/modules/Resources/TeamMeetings/MeetingTimeCard/StyledMeetingTimeCard.js
@@ -2,21 +2,39 @@ import styled from "styled-components";
 
 export const StyledMeetingTimeCard = styled.a`
     width: 100%;
+    max-width: 18rem;
     display: flex;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-    gap: 5px;
     border: 1px solid #B9B4B4;
     border-radius: 2rem;
-    padding: 2rem 0rem;
-    color: ${({theme})=>(theme.colors.GREYSCALE_OFF_BLACK)} !important
+    padding: 2rem 1rem;
+    color: ${({theme})=>(theme.colors.GREYSCALE_OFF_BLACK)} !important;
+    text-align: center;
+
+    &:hover{
+        h3{
+            color: ${({theme})=>(theme.colors.BLUE)};
+        }
+    }
+
+    img{
+        width: 5rem;
+
+        @media(min-width:37.5rem){
+            width: 6rem;
+        }
+    }
 `;
 
 export const StyledMeetingTimeCardTitle = styled.h3`
-    font-size: ${({theme})=>(theme.fontSizes.medium)};
+    font-size: 1.8rem;
 `;
 
 export const StyledMeetingTimeCardTime = styled.p`
-    font-size: 1.5rem;
+    font-size: 1.3rem;
+    font-weight: 500;
+    margin-bottom: 1.6rem;
+    margin-top: 0.5rem;
 `;
