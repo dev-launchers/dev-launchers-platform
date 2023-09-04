457e7a0909a9ec7bada95602367ac91617538eab
yacineKahlerras <60440238+yacineKahlerras@users.noreply.github.com>
2023-09-04 20:02:06 +0100

    created team meetings section and styled components

---
+++ apps/site-projects/src/components/modules/Resources/TeamMeetings/StyledTeamMeetings.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const StyledTeamMeetingsContainer = styled.fieldset`
+    display: flex;
+    gap: 2rem;
+    background: white;
+    border: 1px solid #B9B4B4 ;
+    padding: 2rem 3rem;
+    margin: 1.5rem 0;
+    padding-bottom: 2rem;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+
+    > legend {
+        font-family: "Abel",sans-serif;
+        font-size: 2.3rem;
+        font-weight: bold;
+        padding: 0 1.5rem;
+    }
+`
