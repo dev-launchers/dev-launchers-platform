43a65616ee3831285e8641e96350b5cac651d262
Rutuja177 <rutuja.dukhande7@gmail.com>
2023-09-05 13:56:11 -0500

    CSS changes to SaveIdea button

---
+++ apps/ideaspace/src/components/modules/SaveIdea/StyledSaveIdea.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import SaveButton, { SaveButtonProps } from '@devlaunchers/components/src/components/molecules/InteractionButtons/SaveButton';
+
+export const StyledSaveIdea = styled.div`
+    font-weight: 400;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    z-index: 1;
+    position: relative;
+    left: 5rem;
+    top: 1rem;
+`;
