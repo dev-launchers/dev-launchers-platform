11c4edfa6a41727c199dee0a58402f7089e779c9
Jude Amos <code@judeamos.com>
2023-09-11 17:11:43 +0100

    stepper-enhancments

--- apps/user-profile/src/components/common/Stepper/Stepper.js
+++ apps/user-profile/src/components/common/Stepper/Stepper.js
@@ -5,13 +5,19 @@ import {
   StepperFooter,
   ProgressBarContainer,
   Bar,
-  EmptyBar,
+  StepNumber,
+  StepName,
+  StepperContainer
 } from './StyledStepper';
-import { Button } from '@devlaunchers/components/components/atoms';
+import { Button, Typography } from '@devlaunchers/components/components/atoms';
+import chevronLeftImg from './../../../images/btn-chevron-left.svg';
+import chevronRightImg from './../../../images/btn-chevron-right.svg';
+import checkImg from './../../../images/Onboarding/stepper/check.svg';
 
 const stepsMockData = [
   {
     component: <p>Step 1</p>,
+    label: 'Step 3',
     config: {
       buttons: {
         next: {
@@ -34,6 +40,7 @@ const stepsMockData = [
   },
   {
     component: <p>Step 2</p>,
+    label: 'Step 2',
     config: {
       buttons: {
         next: {
@@ -55,6 +62,7 @@ const stepsMockData = [
   },
   {
     component: <p> Step 3</p>,
+    label: 'Step 2',
     config: {
       buttons: {
         next: {
@@ -79,17 +87,24 @@ const stepsMockData = [
 /**
  * @description This is a stepper component that is used for the user onboarding
  */
-export default function Stepper({ steps = stepsMockData }) {
+export default function Stepper({ steps = stepsMockData, startIndex = 0 }) {
   const [stepsData, setStepsData] = useState(steps);
-  const [index, setIndex] = useState(0);
+  const [index, setIndex] = useState(startIndex);
+  const [activeComponent, setActiveComponent] = useState(stepsData[index].component);
+  const [buttonConfig, setButtonConfig] = useState(stepsData[index].config.buttons);
 
-  const [activeComponent, setActiveComponent] = useState(
-    stepsData[index].component
-  );
-  const [activeConfig, setActiveConfig] = useState(stepsData[index].config);
-  const [buttonConfig, setButtonConfig] = useState(
-    stepsData[index].config.buttons
-  );
+  const lastStepIndex = stepsData.length - 1;
+
+  /**
+   * Everytime users navigates update the activeComponent & config
+   */
+  useEffect(() => {
+    stepsData[index]?.skip == true && setIndex(index + 1);
+    setActiveComponent(stepsData[index].component);
+    setButtonConfig(stepsData[index].config.buttons);
+  }, [index]);
+
+
 
   const backOnClickHandler = () => {
     if (buttonConfig?.back?.useDefaultOnClick) {
@@ -103,7 +118,7 @@ export default function Stepper({ steps = stepsMockData }) {
 
   const nextOnClickHandler = () => {
     if (buttonConfig?.next?.useDefaultOnClick) {
-      if (index < steps.length - 1) {
+      if (index < lastStepIndex) {
         setIndex(index + 1);
       }
     } else {
@@ -111,52 +126,81 @@ export default function Stepper({ steps = stepsMockData }) {
     }
   };
 
-  /**
-   * Everytime users navigates update the activeComponent & config
-   */
-  useEffect(() => {
-    setActiveComponent(stepsData[index].component);
-    setActiveConfig(stepsData[index].config);
-    setButtonConfig(stepsData[index].config.buttons);
-  }, [index]);
-
-  const bars = steps.map((step, i) => {
-    const width = `${100 / steps.length}%`;
-
-    return index >= i ? (
-      <Bar key={i} width={width} />
-    ) : (
-      <EmptyBar key={i} width={width} />
-    );
-  });
-
-  const backButton =
-    // This hides the button if index == 0
-    index === 0 ? (
-      <div></div>
-    ) : (
-      <Button onClick={backOnClickHandler}>
+  const showBackButton = () => {
+    const backButtonHtml =
+      <Button buttonType="alternative" buttonSize="xl" onClick={backOnClickHandler} className='stepper-btn-icon-text back-button'>
+        {buttonConfig.back.hideIcons ? null : <img src={chevronLeftImg} />}
         {buttonConfig.back.label ? buttonConfig.back.label : 'Back'}
       </Button>
-    );
+    // This hides the button if index == 0
+    return index === 0 || buttonConfig.back.hide ? <span></span> : backButtonHtml;
+  }
+
+  const showNextButton = () => {
+    const nextButtonHtml =
+      <Button className="next-btn" buttonType="primary" buttonSize="xl" onClick={nextOnClickHandler}>
+        <div className='stepper-btn-icon-text'>
+          {buttonConfig?.next?.label ? buttonConfig.next.label : 'Next'}
+          {buttonConfig?.next?.hideIcons ? null : <img src={chevronRightImg} />}
+        </div>
+      </Button>;
+    return nextButtonHtml;
+  };
+
+  const showStepNumber = (mapIndex) => {
+    const stepConfig = stepsData[mapIndex].config;
+    const stepCompleted = index > mapIndex;
+    const onActivestep = index == mapIndex;
+    const onLastStep = index == lastStepIndex;
+    const stepNumberHtml =
+      <StepNumber className={stepCompleted || onActivestep ? 'completed-step' : 'uncompleted-step'}>
+        {stepCompleted || onLastStep ? <img src={checkImg} /> : stepConfig.stepNumber ? stepConfig.stepNumber : mapIndex + 1}
+        <StepName>{stepConfig.stepName}</StepName>
+      </StepNumber>;
+    return stepConfig?.hideStepNumber ? null : stepNumberHtml;
+  };
+
+  const showBar = (mapIndex) => {
+    const stepConfig = stepsData[mapIndex]?.config;
+    const width = (stepConfig.barSize == 's' ? 50 : 100) + '%';
+    const nextStepConfig = mapIndex === lastStepIndex ? lastStepIndex : stepsData[mapIndex + 1].config;
+    const addSpaceToRight = nextStepConfig.hideStepNumber;
+    const stepCompleted = index > mapIndex;
+
+    const barHtml = <Bar key={mapIndex} width={width} spacing={{ addSpaceToRight }} className={stepCompleted ? 'completed-step' : 'uncompleted-step'} />
+
+    return stepConfig?.hideBar ? null : barHtml;
+  };
+
+
+  const stepTracker = stepsData.map((step, mapIndex) => {
+    const trackerHtml =
+      <>
+        <div>{showStepNumber(mapIndex)}</div>
+        {showBar(mapIndex)}
+      </>
+    return trackerHtml;
+  });
 
   return (
     <>
-      <StepperHeader>
-        <ProgressBarContainer>{bars}</ProgressBarContainer>
-      </StepperHeader>
+      <StepperContainer>
+        <StepperHeader>
+          <ProgressBarContainer>
+            {stepTracker}
+          </ProgressBarContainer>
+        </StepperHeader>
 
-      <StepperBody>
-        {/* activeComponent holds the component e.g */}
-        {activeComponent}
-      </StepperBody>
+        <StepperBody>
+          {/* activeComponent holds the component e.g */}
+          {activeComponent}
+        </StepperBody>
 
-      <StepperFooter>
-        {backButton}
-        <Button onClick={nextOnClickHandler}>
-          {buttonConfig.next.label ? buttonConfig.next.label : 'Next'}
-        </Button>
-      </StepperFooter>
+        <StepperFooter>
+          {showBackButton()}
+          {showNextButton()}
+        </StepperFooter>
+      </StepperContainer>
     </>
   );
 }
--- apps/user-profile/src/components/modules/UserOnboardingModal/PlatformOnboarding/PlatformOnboarding.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/PlatformOnboarding/PlatformOnboarding.js
@@ -1,7 +1,9 @@
 import { useEffect } from 'react';
 import Stepper from '../../../common/Stepper';
+import PageOne from './PageOne/PageOne';
 import PageTwo from './PageTwo/PageTwo';
 import PageThree from './PageThree/PageThree';
+import PageFour from './PageFour/PageFour';
 import { PlatformOnboardingContainer } from './StyledPlatformOnboarding';
 import { useOnboardingDataContext } from './../../../../context/OnboardingDataContext';
 import { onboardingActions } from './../../../../state/actions';
@@ -15,8 +17,11 @@ export default function PlatformOnboarding() {
 
   const stepperConfig = [
     {
-      component: <PageTwo />,
+      component: <p> Skip </p>,
+      skip: true,
       config: {
+        stepName: 'Profile',
+        stepNumber: 1,
         buttons: {
           next: {
             label: 'Next',
@@ -34,11 +39,36 @@ export default function PlatformOnboarding() {
       },
     },
     {
-      component: <PageThree />,
+      component: <PageTwo />,
       config: {
+        stepName: 'DevLaunchers',
+        stepNumber: 2,
         buttons: {
           next: {
-            label: 'Finish',
+            label: 'Next',
+            useDefaultOnClick: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+          back: {
+            label: 'Back',
+            hide: true,
+            useDefaultOnClick: false,
+            onClick: back,
+          },
+        },
+      },
+    },
+    {
+      component: <PageThree />,
+      config: {
+        stepName: 'About you',
+        barSize: 's',
+        stepNumber: 3,
+        buttons: {
+          next: {
+            label: 'Next',
             useDefaultOnClick: true,
             onClick: () => {
               console.log('..some function ');
@@ -54,11 +84,85 @@ export default function PlatformOnboarding() {
         },
       },
     },
+    {
+      component: <PageFour />,
+      config: {
+        barSize: 's',
+        hideStepNumber: true,
+        buttons: {
+          next: {
+            label: 'Next',
+            useDefaultOnClick: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+          back: {
+            label: 'Back',
+            useDefaultOnClick: true,
+            // useDefaultHide: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+        },
+      },
+    },
+    {
+      component: <p> Interest </p>,
+      config: {
+        barSize: 's',
+        hideStepNumber: true,
+        buttons: {
+          next: {
+            label: 'Next',
+            useDefaultOnClick: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+          back: {
+            label: 'Back',
+            useDefaultOnClick: true,
+            // useDefaultHide: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+        },
+      },
+    },
+    {
+      component: <p>Congratulations</p>,
+      config: {
+        stepName: 'Done',
+        hideBar: true,
+        stepNumber: 4,
+        buttons: {
+          next: {
+            label: 'Finished',
+            hideIcons: true,
+            useDefaultOnClick: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+          back: {
+            label: 'Back',
+            useDefaultOnClick: true,
+            // useDefaultHide: true,
+            onClick: () => {
+              console.log('..some function ');
+            },
+          },
+        },
+      },
+    },
   ];
 
   return (
     <PlatformOnboardingContainer>
-      <Stepper steps={stepperConfig} />
+      <Stepper steps={stepperConfig} startIndex={1} />
     </PlatformOnboardingContainer>
   );
 }
