ad2b048e3125ba66b5f47aa5afa6c2a21f483824
Harri1118 <52708099+Harri1118@users.noreply.github.com>
2023-10-19 14:25:56 -0400

    Added chatbot frontend + js script

--- apps/gptbot/src/pages/index.jsx
+++ apps/gptbot/src/pages/index.jsx
@@ -1,12 +1,34 @@
 import Head from "next/head";
-import agent from "@devlaunchers/utility/agent"
+import React, { useEffect } from 'react';
 import Button from "../../../../packages/UI/src/components/atoms/Button";
-import Button from "@devlaunchers/components";
-// const projectsData = require("../components/modules/Projects/data.json");
+import OpenResponse from "../../../../packages/UI/src/components/organisms/OpenResponse";
+import BaseLayer from "../../../../packages/UI/src/components/Atoms/BaseLayer";
+import { send } from "../utils/chat.js";
+import { displayText } from "../utils/chat.js";
+import { enterKey } from "../utils/chat.js";
+import { handleKeyBind } from "../utils/chat.js";
 
-const ProjectsList = () => (
-  <>
-    <Head>
+const ProjectsList = () => {
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      // Check if the pressed key is the "Enter" key (key code 13)
+      if (event.key === 'Enter') {
+        send(); // Call the send method
+      }
+    };
+
+    // Add an event listener for keydown when the component mounts
+    document.addEventListener('keydown', handleKeyPress);
+
+    // Remove the event listener when the component unmounts to prevent memory leaks
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []); // Empty dependency array means this effect runs once on mount
+
+  return (
+    <>
+      <Head>
       <title>Our Projects</title>
       <meta name="title" content="Our Projects"></meta>
       <meta
@@ -44,10 +66,43 @@ const ProjectsList = () => (
         content="/images/DevlaunchersGitHubThumb.png"
       ></meta>
       <meta content="#ff7f0e" data-react-helmet="true" name="theme-color" />
-    </Head>
-    <div>hello from GPT TEAM</div>
-    <Button buttonSize="xl" buttonType="primary">hello</Button>
-  </>
-);
+      </Head>
+
+      <div id="chat"
+      onLoad={displayText}
+      >
+      </div>
+
+      <BaseLayer color="offbase" type="light">
+      <div
+        style={{
+          width: '1600px',
+          height: '300px',
+        }}
+
+      />
+        <OpenResponse
+        id="chatbox"
+        cols={50}
+        error=""
+
+        onChange={function noRefCheck() { }}
+        
+        placeholder="Enter text here"
+        width={'1016px'}
+        height={'40px'}
+      />
+        <Button
+          id="sendButton"
+          onClick={send}
+          buttonSize="standard"
+          buttonType="primary"
+        >
+          Send
+        </Button>
+      </BaseLayer>
+    </>
+  );
+};
 
 export default ProjectsList;
