69a62b97b6b01d77e90e5834acd7a6d6a805e2c5
Imaan <imaansoltan@gmail.com>
2023-11-11 19:25:32 -0800

    updated 1179

--- apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
@@ -12,19 +12,15 @@ export default function UserOnboardingModal() {
     const { onboardingData: { showIntroductionModal, showPlatformOnboardingModal }, dispatch } = useOnboardingDataContext();
 
     useEffect(() => {
-        // Check if the onboarding has already been completed
         const onboardingCompleted = localStorage.getItem('onboardingCompleted');
         if (onboardingCompleted === 'true') {
-            // Update the context to reflect that the onboarding modals should not be shown
             dispatch({ type: onboardingActions.HIDE_ALL_MODALS });
         }
     }, [dispatch]);
 
-    // You no longer need this state since the display logic should be driven by the context
-    // const [modalIsOpen, setModalIsOpen] = useState(showIntroductionModal || showPlatformOnboardingModal);
+    
 
     const closeModal = () => {
-        // When closing the modal, ensure to update the context as well
         dispatch({ type: onboardingActions.HIDE_ALL_MODALS });
     }
 
