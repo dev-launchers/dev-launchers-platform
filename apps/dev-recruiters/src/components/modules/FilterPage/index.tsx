1b55788a83b608415014d6958d6231bd27206f5b
unknown <ramyaperumaal@gmail.com>
2023-11-17 11:17:39 -0600

    For #1467

--- apps/dev-recruiters/src/components/modules/FilterPage/index.tsx
+++ apps/dev-recruiters/src/components/modules/FilterPage/index.tsx
@@ -4,7 +4,13 @@ import BoxContainer from '../../common/BoxContainer';
 
 import { HeaderJoinPage } from '../NewJoinPageComponent/HeaderJoinPage';
 import FilterComponent from './RolesFilterComponent';
-import { Wrapper } from './styles';
+import {
+  Wrapper,
+  Footer,
+  FooterFirstText,
+  FooterSecondText,
+  BtnSignUp,
+} from './styles';
 
 export interface FilteringComponentProps {
   projects: Project[];
@@ -24,6 +30,26 @@ const FilterPageComponent: React.FunctionComponent<Props> = ({
       </BoxContainer>
       <BoxContainer paddingVertical={10} paddingHorizontal={20}>
         <FilterComponent projects={projects} opportunities={opportunities} />
+      </BoxContainer>
+      <BoxContainer paddingVertical={0} paddingHorizontal={50}>
+        <Footer>
+          <FooterFirstText>
+            Join the Dev Launchers Talent Community
+          </FooterFirstText>
+          <FooterSecondText>
+            Join the Dev Launchers Talent Community to be notified about new
+            volunteering roles that match your skillset!
+          </FooterSecondText>
+          <BtnSignUp
+            as="a"
+            href={
+              process.env.NEXT_PUBLIC_GOOGLE_AUTH_URL +
+              '?redirectURL=https://devlaunchers.org/users/me'
+            }
+          >
+            Join the Talent Community
+          </BtnSignUp>
+        </Footer>
       </BoxContainer>
     </Wrapper>
   );
--- apps/dev-recruiters/src/components/modules/FilterPage/styles.ts
+++ apps/dev-recruiters/src/components/modules/FilterPage/styles.ts
@@ -3,3 +3,37 @@ import styled from 'styled-components';
 export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme?.colors?.SilverSandT20};
 `;
+
+export const Footer = styled.div`
+  width: 100%;
+  height: 220px;
+  border-radius: 15px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  gap: 25px;
+  margin-bottom: 50px;
+`;
+
+export const FooterFirstText = styled.div`
+  font-size: 40px;
+  font-family: 'Abel', sans-serif;
+`;
+
+export const FooterSecondText = styled.div`
+  font-size: 20px;
+  font-family: 'Abel', sans-serif;
+`;
+
+export const BtnSignUp = styled.button`
+  background-color: black;
+  border-radius: 10px;
+  color: white;
+  padding: 10px 40px;
+  font-size: 16px;
+  cursor: pointer;
+  font-family: 'Nunito Sans', sans-serif;
+  background-color: ${({ theme }) => theme?.colors?.BLUE_700};
+  text-decoration: none;
+`;
