bce30890da6794109fab477ffdaf74f1b074d8bc
User192940 <davfedchuk@gmail.com>
2023-11-22 12:08:26 -0600

    Terms of service checkbox affects button disabled status

--- packages/UI/src/components/molecules/ModalComponents/terms-footer.tsx
+++ packages/UI/src/components/molecules/ModalComponents/terms-footer.tsx
@@ -1,27 +1,33 @@
-import { ChangeEvent, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import Checkbox from '../../atoms/Checkbox/Checkbox';
 
 const termsFooter = () => {
     const [isChecked, setIsChecked] = useState(false);
+    useEffect(() => {
+      const submit = (document.getElementById('submit') as HTMLButtonElement);
+      console.log(submit.disabled);
+      submit.disabled = !isChecked;
+    }, [isChecked])
 
   const handleCheckboxChange = (e: ChangeEvent) => {
-    console.log(e.target);
+    setIsChecked(!isChecked);
+    // console.log((e.target as HTMLInputElement).checked);
   };
     return (
     <div className="flex flex-row items-center justify-between self-stretch shrink-0 relative ">
       <div className="flex flex-row gap-3 items-start justify-center shrink-0 relative">
         <Checkbox disabled={false} label={"I’ve read the terms of services"} required={true} onChange={handleCheckboxChange}/>
       </div>
-      <div className={"bg-brand-alt-nebula-300 rounded-lg pt-2 pr-6 pb-2 pl-6 flex flex-row gap-2.5 items-center justify-center shrink-0 relative"}>
+      <button id={'submit'} className={`${isChecked ? 'bg-brand-alt-nebula-600 text-grayscale-50' : 'bg-brand-alt-nebula-300 text-brand-alt-nebula-600'} rounded-lg pt-2 pr-6 pb-2 pl-6 flex flex-row gap-2.5 items-center justify-center shrink-0 relative`}>
         <div
-          className="text-brand-alt-nebula-600 text-center relative"
+          className="text-center relative"
           style={{
             font: "var(--tosort-universal-button, 400 16px/150% 'NunitoSans-Regular', sans-serif)",
           }}
         >
           Submit{" "}
         </div>
-      </div>
+      </button>
     </div>
 );
 }
