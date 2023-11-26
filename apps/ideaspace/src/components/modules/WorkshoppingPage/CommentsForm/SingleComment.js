bb0879ee4a3e547f830676791e9ff8e5ac182e8c
remi <remitoutin@gmail.com>
2023-11-25 23:57:34 -0600

    Toggle like button

--- apps/ideaspace/src/components/modules/WorkshoppingPage/CommentsForm/SingleComment.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/CommentsForm/SingleComment.js
@@ -8,6 +8,8 @@ import {
 import agent from '@DevLaunchers/utility/agent';
 import { molecules } from '@DevLaunchers/components/src/components';
 import { LikeButton } from '@devlaunchers/components/src/components/molecules';
+import { TtyRounded } from '@mui/icons-material';
+import { useState } from 'react';
 
 // A function to show the date as X hours ago, etc.
 // from: https://stackoverflow.com/a/3177838
@@ -39,33 +41,78 @@ function timeSince(date) {
   return Math.floor(seconds) + " seconds ago";
 }
 
-const SingleCommentComponent = props => (
-  <div className="textContent">
-    <SingleComment>
-      <UserImage alt="user_image" src={`https://picsum.photos/70?random=${props.id}`} />
-      <div className="textContent">
-        <SingleCommentContent>
-          <h3>{props.author}</h3>
-          {/* get the idea ID from the URL if possible and determine the idea owner (maybe do this in another file) */}
-        </SingleCommentContent>
-        <SingleCommentContent>
-          {/* date of creation here, i.e. "2 days ago" */}
-          <h5>{timeSince(new Date(props.createdAt))}</h5>
-        </SingleCommentContent>
-      </div>
-    </SingleComment>
-    <SingleComment>
-      <div className="textContent">
-        <SingleCommentContent>
-          <div source={props.children}><p>{props.children}</p></div>
-        </SingleCommentContent>
-        <SingleCommentButtons style={{ display: "flex" }}>
-          <LikeButton style={{ marginLeft: "auto" }} />
-        </SingleCommentButtons>
-        <hr></hr>
-      </div>
-    </SingleComment>
-  </div>
-);
+// Do we need to make a function returning the comment component to keep track of the likes?
+
+// const SingleCommentComponent = props => (
+//   // const [liked, setLiked] = useState(false);
+//   <div className="textContent">
+//     <SingleComment>
+//       <UserImage alt="user_image" src={`https://picsum.photos/70?random=${props.id}`} />
+//       <div className="textContent">
+//         <SingleCommentContent>
+//           <h3>{props.author}</h3>
+//           {/* get the idea ID from the URL if possible and determine the idea owner (maybe do this in another file) */}
+//         </SingleCommentContent>
+//         <SingleCommentContent>
+//           {/* date of creation here, i.e. "2 days ago" */}
+//           <h5>{timeSince(new Date(props.createdAt))}</h5>
+//         </SingleCommentContent>
+//       </div>
+//     </SingleComment>
+//     <SingleComment>
+//       <div className="textContent">
+//         <SingleCommentContent>
+//           <div source={props.children}><p>{props.children}</p></div>
+//         </SingleCommentContent>
+//         <SingleCommentButtons style={{ display: "flex" }}>
+//           <LikeButton style={{ marginLeft: "auto" }} onClick={handleLikeClick} filled={(false) ? false : true} text={(true) ? "479" : "Like"} />
+//         </SingleCommentButtons>
+//         <hr></hr>
+//       </div>
+//     </SingleComment>
+//   </div>
+// );
+
+function SingleCommentComponent(props) {
+  const [liked, setLiked] = useState(false);
+
+  // a function to keep track of the number of likes and when the user clicks the like button for this comment
+  function handleLikeClick() {
+    if (liked) {
+      setLiked(false);
+    } else {
+      setLiked(true);
+    }
+  }
+
+  return (
+    <div className="textContent">
+      <SingleComment>
+        <UserImage alt="user_image" src={`https://picsum.photos/70?random=${props.id}`} />
+        <div className="textContent">
+          <SingleCommentContent>
+            <h3>{props.author}</h3>
+            {/* get the idea ID from the URL if possible and determine the idea owner (maybe do this in another file) */}
+          </SingleCommentContent>
+          <SingleCommentContent>
+            {/* date of creation here, i.e. "2 days ago" */}
+            <h5>{timeSince(new Date(props.createdAt))}</h5>
+          </SingleCommentContent>
+        </div>
+      </SingleComment>
+      <SingleComment>
+        <div className="textContent">
+          <SingleCommentContent>
+            <div source={props.children}><p>{props.children}</p></div>
+          </SingleCommentContent>
+          <SingleCommentButtons style={{ display: "flex" }}>
+            <LikeButton style={{ marginLeft: "auto" }} onClick={handleLikeClick} filled={liked ? true : false} text={liked ? "479" : "Like"} />
+          </SingleCommentButtons>
+          <hr></hr>
+        </div>
+      </SingleComment>
+    </div>
+  )
+}
 
 export default SingleCommentComponent;
