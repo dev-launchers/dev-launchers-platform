6e26e1d8b90d424fe11389689712a82ad2ace2c0
chungthuang <7826979+chungthuang@users.noreply.github.com>
2023-11-29 21:17:22 +0000

    fix(chungthuang): Sanitize fine one project response and return image of a project

--- src/api/project/controllers/project.js
+++ src/api/project/controllers/project.js
@@ -7,26 +7,28 @@ module.exports = createCoreController('api::project.project', ({ strapi }) => ({
         const slug = ctx.request.params.id;
         const entity = await strapi.db.query('api::project.project').findOne({
             where: { slug },
-            populate: ['team.leaders.leader', 'team.members.member', 'heroImage', 'interests', 'subProjects', 'opportunities']
+            populate: ['team.leaders.leader', 'team.members.member', 'heroImage', 'images', 'interests', 'subProjects', 'opportunities']
         });
 
         if (entity?.team) {
             entity.team.leaders = entity.team.leaders.map((leader) => ({
-                id: leader.leader.id,
-                username: leader.leader.username,
-                profile: leader.leader.profile,
-                email: leader.leader.email,
+                id: leader.leader?.id,
+                username: leader.leader?.username,
+                profile: leader.leader?.profile,
+                email: leader.leader?.email,
                 role: leader.role,
             }));
 
             entity.team.members = entity.team.members.map((member) => ({
-                id: member.member.id,
-                username: member.member.username,
-                profile: member.member.profile,
+                id: member.member?.id,
+                username: member.member?.username,
+                profile: member.member?.profile,
                 role: member.role,
             }));
         }
 
-        return entity;
+        const sanitizedResults = await this.sanitizeOutput(entity, ctx);
+
+        return this.transformResponse(sanitizedResults);
     },
 }));
