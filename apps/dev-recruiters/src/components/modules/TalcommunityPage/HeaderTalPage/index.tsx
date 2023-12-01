d64ab2a42cc1f02a09e3db26b2e44fc143caeb23
unknown <ramyaperumaal@gmail.com>
2023-12-01 11:41:24 -0600

    For #1468

---
+++ apps/dev-recruiters/src/components/modules/TalcommunityPage/HeaderTalPage/index.tsx
@@ -0,0 +1,30 @@
+import { Wrap } from '../StyledTalcommunityPage';
+
+import {
+  HeaderContainer,
+  HeaderText,
+  SubHeaderContainer,
+  SubHeaderText,
+} from './styles';
+
+export function HeaderTalPage() {
+  return (
+    <Wrap>
+      <HeaderContainer>
+        <HeaderText>Dev Launchers Talent Community</HeaderText>
+      </HeaderContainer>
+      <SubHeaderContainer>
+        <SubHeaderText>
+          Join our Talent Community to be notified about new volunteer roles
+          that match your skillset!
+        </SubHeaderText>
+        <SubHeaderText>
+          {' '}
+          Volunteers at Dev Launchers get the chance to learn valuable skills,
+          expand their portfolios, mentor talents, and network with like-minded
+          people while building something that matters.
+        </SubHeaderText>
+      </SubHeaderContainer>
+    </Wrap>
+  );
+}
