b3556639ebc6ea936842aa62165b8d2a368bed66
unknown <ramyaperumaal@gmail.com>
2023-12-19 17:40:54 -0600

    For Talcommunity

---
+++ src/api/dl-tal-community/routes/dl-tal-community.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * dl-tal-community router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::dl-tal-community.dl-tal-community');
