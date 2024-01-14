dbb633cab72487bbbb6f0ebc898bf6488766fa7a
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-14 16:28:01 -0700

    4 states and check state added

--- packages/UI/src/components/Checkbox/Checkbox.tsx
+++ packages/UI/src/components/Checkbox/Checkbox.tsx
@@ -1,12 +1,48 @@
 import { tv } from 'tailwind-variants';
+import Checkmark from './../../assets/icons/Checkmark';
 
 export interface CheckboxProps {
   className?: string;
+  state?: 'focus' | 'disable' | 'hover' | undefined;
+  checked?: boolean;
 }
-export default function Checkbox({ className }: CheckboxProps) {
+export default function Checkbox({
+  className,
+  checked = false,
+  state = 'disable',
+}: CheckboxProps) {
   const checkbox = tv({
-    base: 'h-6 w-6 border-2 border-solid border-gray-500',
+    base: 'flex h-6 w-6 items-center justify-center rounded-sm border-2 border-solid border-brand-alt-nebula-500 bg-grayscale-50',
+    variants: {
+      color: {
+        focus:
+          'outline outline-2 outline-offset-2 outline-brand-alt-nebula-500',
+        hover:
+          'outline outline-4 outline-offset-0 outline-brand-alt-nebula-100',
+        disable: 'border-grayscale-500 bg-grayscale-200',
+      },
+      checked: {
+        true: `${
+          state !== 'disable'
+            ? 'bg-brand-alt-nebula-500'
+            : 'border-grayscale-400 bg-grayscale-400'
+        }`,
+      },
+    },
   });
-  console.log(checkbox({ className }));
-  return <div className={checkbox({ className })}>stuff</div>;
+
+  const checkmark = tv({
+    base: '',
+    variants: {
+      checked: {
+        false: 'hidden',
+      },
+    },
+  });
+
+  return (
+    <div className={checkbox({ className, color: state, checked })}>
+      <Checkmark className={checkmark({ checked })} />
+    </div>
+  );
 }
