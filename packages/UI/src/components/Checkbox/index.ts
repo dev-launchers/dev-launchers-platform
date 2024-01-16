c3261d779aba604cf417382ebc946de8a3a8ef1e
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-16 11:00:13 -0700

    moved props into Checkbox for review

--- packages/UI/src/components/Checkbox/index.ts
+++ packages/UI/src/components/Checkbox/index.ts
@@ -1,6 +1,1 @@
 export { default as Checkbox } from './Checkbox';
-
-export interface CheckboxProps extends React.HTMLAttributes<HTMLDivElement> {
-  state?: 'disable' | undefined;
-  checked?: boolean;
-}
