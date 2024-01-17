3b0f02d92276e387762679cd0bbd877e3444cd29
lamodots <lamodots@gmail.com>
2024-01-17 10:38:52 -0800

    Seperated the Avatar to a different branch

---
+++ packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
@@ -0,0 +1,20 @@
+// import type { Meta, StoryObj } from '@storybook/react';
+// import AvatarComponent from './Avatar';
+
+// const meta: Meta<typeof AvatarComponent> = {
+//   component: AvatarComponent,
+//   title: 'Atoms/Avatar',
+// };
+
+// export default meta;
+
+// export const Avatar: StoryObj<typeof AvatarComponent> = {
+//   args: {
+//     size: 'sm',
+//     alt: 'Lamodot',
+//     src: 'https://bit.ly/3EBQXiH',
+//     delayMs: 600,
+//     rounded: 'sm',
+//     onClick: () => alert('Could open profile'),
+//   },
+// };
