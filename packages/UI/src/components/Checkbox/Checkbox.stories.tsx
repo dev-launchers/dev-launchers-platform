f7e7d767e1a8c168b70f1a46b569bec2d998f4e8
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-18 11:48:47 -0700

    Saving to share doc blocks

--- packages/UI/src/components/Checkbox/Checkbox.stories.tsx
+++ packages/UI/src/components/Checkbox/Checkbox.stories.tsx
@@ -14,10 +14,10 @@ const meta: Meta<typeof Checkbox> = {
       description: 'Indicates whether the checkbox is disabled',
     },
   },
-  args: {
-    checked: false,
-    disabled: false,
-  },
+  // args: {
+  //   checked: false,
+  //   disabled: false,
+  // },
 };
 
 export default meta;
@@ -29,57 +29,17 @@ type Story = StoryObj<typeof Checkbox>;
  * to learn how to use render functions.
  */
 export const DefaultCheckbox: Story = {
-  args: {
-    autoFocus: true,
-  },
   render: (args) => (
     <form className="grid w-fit grid-cols-2 gap-3 rounded-lg border-2 border-dashed border-brand-alt-nebula-500 p-4">
       <h3>Default</h3> <Checkbox />
       <h3>Disabled</h3> <Checkbox {...args} disabled />
       <h3>Checked</h3> <Checkbox {...args} checked />
-      <h3>focus</h3> <Checkbox id="focused" />
+      <h3>Focus</h3> <Checkbox id="focused" />
     </form>
   ),
   parameters: {
     pseudo: {
       focus: '#focused',
     },
-    docs: {
-      docsOnly: true,
-      // disable: true, // Uncomment this code to disable this in docs, but it removes from both hero section and stories
-    },
   },
-};
-
-export const Focus: Story = {
-  render: () => (
-    <div className="flex gap-3">
-      <Checkbox />
-      <Checkbox checked />
-    </div>
-  ),
-  parameters: {
-    pseudo: {
-      focus: true,
-    },
-  },
-};
-
-export const Hover: Story = {
-  render: () => (
-    <div className="flex gap-3">
-      <Checkbox />
-      <Checkbox checked />
-    </div>
-  ),
-  parameters: { pseudo: { hover: true } },
-};
-
-export const Disabled: Story = {
-  render: () => (
-    <div className="flex gap-3">
-      <Checkbox disabled />
-      <Checkbox checked disabled />
-    </div>
-  ),
 };
