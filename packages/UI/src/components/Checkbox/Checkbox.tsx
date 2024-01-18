44cf158dbe43debbfa55a4eebc1ef2f85182ebc8
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-18 13:20:09 -0700

    Removed comments, jsdoc code

--- packages/UI/src/components/Checkbox/Checkbox.tsx
+++ packages/UI/src/components/Checkbox/Checkbox.tsx
@@ -20,13 +20,6 @@ const checkboxStyles = tv({
   },
 });
 
-/**
- * This is a description of the CheckboxProps type.
- * @typedef CheckboxProps
- * @type {boolean} checked - Indicates whether the checkbox is checked
- * @property {boolean} disabled - Indicates whether the checkbox is disabled
- */
-
 type OriginalProps = RadixCheckbox.CheckboxProps &
   VariantProps<typeof checkboxStyles>;
 
