3830092719c1003c43e3c6182bf563cc7d0bba62
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-19 10:07:00 -0700

    removed redunant tailwind

--- packages/UI/src/components/Checkbox/Checkbox.tsx
+++ packages/UI/src/components/Checkbox/Checkbox.tsx
@@ -2,24 +2,6 @@ import * as RadixCheckbox from '@radix-ui/react-checkbox';
 import { tv, type VariantProps } from 'tailwind-variants';
 import CheckmarkImg from './../../assets/icons/Checkmark';
 
-const checkboxStyles = tv({
-  slots: {
-    rootStyle:
-      'flex h-6 w-6 items-center justify-center rounded-sm border-2 border-solid border-brand-alt-nebula-500 bg-grayscale-50 hover:outline hover:outline-4 hover:outline-offset-0 hover:outline-brand-alt-nebula-100 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-brand-alt-nebula-500 disabled:border-grayscale-500 disabled:bg-grayscale-200 disabled:hover:outline-none disabled:hover:outline-0 hover:disabled:outline-0 data-checked:bg-brand-alt-nebula-500 data-checked:disabled:bg-grayscale-200',
-    indicatorStyle: '',
-  },
-  variants: {
-    checked: {
-      true: {
-        rootStyle: 'bg-brand-alt-nebula-500',
-      },
-      false: {
-        indicatorStyle: 'hidden',
-      },
-    },
-  },
-});
-
 type OriginalProps = RadixCheckbox.CheckboxProps &
   VariantProps<typeof checkboxStyles>;
 
@@ -33,6 +15,24 @@ export type CheckboxProps = Omit<OriginalProps, 'checked' | 'disabled'> & {
    */
   disabled?: boolean;
 };
+
+const checkboxStyles = tv({
+  slots: {
+    rootStyle:
+      'flex h-6 w-6 items-center justify-center rounded-sm border-2 border-solid border-brand-alt-nebula-500 bg-grayscale-50 hover:outline hover:outline-4 hover:outline-offset-0 hover:outline-brand-alt-nebula-100 focus:outline focus:outline-2 focus:outline-offset-2 focus:outline-brand-alt-nebula-500 disabled:border-grayscale-500 disabled:bg-grayscale-200 disabled:hover:outline-none hover:disabled:outline-0 data-checked:bg-brand-alt-nebula-500 data-checked:disabled:bg-grayscale-200',
+    indicatorStyle: '',
+  },
+  variants: {
+    checked: {
+      true: {
+        rootStyle: 'bg-brand-alt-nebula-500',
+      },
+      false: {
+        indicatorStyle: 'hidden',
+      },
+    },
+  },
+});
 
 /**
  * @description Created with the Checkbox component from Radix UI.
