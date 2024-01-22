7909bd85b5cbf907647e0f9125a4bf97663b288c
l1keafox <raymond.ed.lewis@gmail.com>
2024-01-22 09:02:58 -0700

    created files

---
+++ packages/UI/src/components/Modal/Modal.tsx
@@ -0,0 +1,16 @@
+
+
+export interface ModalProps{
+  hasCloseBtn: boolean;
+  header
+  content
+  footer
+}
+
+export function Modal({}:ModalProps) {
+  return (
+  <>
+    <h1>Modal</h1>
+  </>
+  );
+}
