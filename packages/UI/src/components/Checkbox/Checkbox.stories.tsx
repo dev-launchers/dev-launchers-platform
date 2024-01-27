8574f99e3ca59d6dbe192e41a486c9795f050708
Enjoy2Live <iMohammedMaqbol@gmail.com>
2024-01-27 15:09:12 +0300

    linked figma node to each story

--- packages/UI/src/components/Checkbox/Checkbox.stories.tsx
+++ packages/UI/src/components/Checkbox/Checkbox.stories.tsx
@@ -25,16 +25,40 @@ type Story = StoryObj<typeof Checkbox>;
 
 export const DefaultUnchecked: Story = {
   render: (args) => <Checkbox {...args} />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/rvdnSOeHdjODE0U7xdNTR9/DL-Universal-Design-System-(NEW)?type=design&node-id=12-3480&mode=dev',
+    },
+  },
 };
 
 export const DefaultChecked: Story = {
-  render: (args) => <Checkbox {...args} />,
+  render: (args) => <Checkbox checked {...args} />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/rvdnSOeHdjODE0U7xdNTR9/DL-Universal-Design-System-(NEW)?type=design&node-id=12-3488&mode=dev',
+    },
+  },
 };
 
 export const DisabledUnchecked: Story = {
   render: (args) => <Checkbox {...args} disabled />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/rvdnSOeHdjODE0U7xdNTR9/DL-Universal-Design-System-(NEW)?type=design&node-id=12-3486&mode=dev',
+    },
+  },
 };
 
 export const DisabledChecked: Story = {
   render: (args) => <Checkbox {...args} disabled defaultChecked />,
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/rvdnSOeHdjODE0U7xdNTR9/DL-Universal-Design-System-(NEW)?type=design&node-id=12-3500&mode=dev',
+    },
+  },
 };
