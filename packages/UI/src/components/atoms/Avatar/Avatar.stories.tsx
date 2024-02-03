bb71d04057a81ac24c5de3d8c051db0f96761ade
Enjoy2Live <iMohammedMaqbol@gmail.com>
2024-02-03 14:00:12 +0300

    added design link to stories

--- packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
+++ packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
@@ -1,19 +1,39 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import AvatarComponent from './Avatar';
+import Avatar from './Avatar';
 
-const meta: Meta<typeof AvatarComponent> = {
-  component: AvatarComponent,
+const meta: Meta<typeof Avatar> = {
+  component: Avatar,
   title: 'Atoms/Avatar',
 };
 
 export default meta;
 
-export const Avatar: StoryObj<typeof AvatarComponent> = {
+export const Default: StoryObj<typeof Avatar> = {
   args: {
-    size: 'sm',
     alt: 'Lamodot',
     src: 'https://bit.ly/3EBQXiH',
     delayMs: 600,
     onClick: () => alert('Could open profile'),
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=7760-6496&mode=design&t=1SqrP5S7pH7kkYa6-4',
+    },
+  },
+};
+
+export const NoImage: StoryObj<typeof Avatar> = {
+  args: {
+    alt: 'Lamodot',
+    src: '',
+    delayMs: 600,
+    onClick: () => alert('Could open profile'),
+  },
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=8575-7191&mode=design&t=1SqrP5S7pH7kkYa6-4',
+    },
   },
 };
--- packages/UI/src/components/atoms/Avatar/Avatar.tsx
+++ packages/UI/src/components/atoms/Avatar/Avatar.tsx
@@ -3,7 +3,7 @@ import * as Avatar from '@radix-ui/react-avatar';
 import { tv, type VariantProps } from 'tailwind-variants';
 
 const AvatarStyles = tv({
-  base: ' w-12 h-12 rounded-full   bg-contain bg-white  font-medium text-base text-center justify-center align-middle',
+  base: ' h-12 w-12 justify-center   rounded-full bg-white  bg-contain text-center align-middle text-base font-medium',
 });
 
 interface AvatarProps
@@ -47,9 +47,9 @@ const AvatarComponent = ({
           <path
             d="M30 32C30 30.4087 29.3679 28.8826 28.2426 27.7574C27.1174 26.6321 25.5913 26 24 26M24 26C22.4087 26 20.8826 26.6321 19.7574 27.7574C18.6321 28.8826 18 30.4087 18 32M24 26C26.2091 26 28 24.2091 28 22C28 19.7909 26.2091 18 24 18C21.7909 18 20 19.7909 20 22C20 24.2091 21.7909 26 24 26ZM34 24C34 29.5228 29.5228 34 24 34C18.4772 34 14 29.5228 14 24C14 18.4772 18.4772 14 24 14C29.5228 14 34 18.4772 34 24Z"
             stroke="black"
-            stroke-width="2"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           />
         </svg>
       </Avatar.Fallback>
