359f0da61cabe84640bc047b27093915255f12d8
lamodots <lamodots@gmail.com>
2024-02-03 02:28:38 -0800

    Fixed size issue

--- packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
+++ packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
@@ -1,20 +1,19 @@
-// import type { Meta, StoryObj } from '@storybook/react';
-// import AvatarComponent from './Avatar';
+import type { Meta, StoryObj } from '@storybook/react';
+import AvatarComponent from './Avatar';
 
-// const meta: Meta<typeof AvatarComponent> = {
-//   component: AvatarComponent,
-//   title: 'Atoms/Avatar',
-// };
+const meta: Meta<typeof AvatarComponent> = {
+  component: AvatarComponent,
+  title: 'Atoms/Avatar',
+};
 
-// export default meta;
+export default meta;
 
-// export const Avatar: StoryObj<typeof AvatarComponent> = {
-//   args: {
-//     size: 'sm',
-//     alt: 'Lamodot',
-//     src: 'https://bit.ly/3EBQXiH',
-//     delayMs: 600,
-//     rounded: 'sm',
-//     onClick: () => alert('Could open profile'),
-//   },
-// };
+export const Avatar: StoryObj<typeof AvatarComponent> = {
+  args: {
+    size: 'sm',
+    alt: 'Lamodot',
+    src: 'https://bit.ly/3EBQXiH',
+    delayMs: 600,
+    onClick: () => alert('Could open profile'),
+  },
+};
--- packages/UI/src/components/atoms/Avatar/utils.ts
+++
@@ -1,8 +0,0 @@
-export function initials(name: string) {
-  const names = name.split(' ');
-  const firstName = names[0] ?? '';
-  const lastName = names.length > 1 ? names[names.length - 1] : '';
-  return firstName && lastName
-    ? `${firstName.charAt(0)}${lastName.charAt(0)}`
-    : firstName.charAt(0);
-}
