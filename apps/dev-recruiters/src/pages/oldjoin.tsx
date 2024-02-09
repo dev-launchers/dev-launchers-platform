734193919f6b37958ead5464b6f7bd0263ac144a
unknown <ramyaperumaal@gmail.com>
2024-02-09 14:59:52 -0600

    #1469

--- apps/dev-recruiters/src/pages/oldjoin.tsx
+++ apps/dev-recruiters/src/pages/oldjoin.tsx
@@ -3,13 +3,18 @@ import ProductHeader from '../components/modules/DetailedPage/ProductHeader';
 import SignUpForm from '../components/modules/TalcommunityPage/TalFormPage/signUpForm';
 import BoxContainer from '../components/common/BoxContainer';
 import React from 'react';
+
 import TalcommunityPage from '../components/modules/TalcommunityPage';
 import { ThankyouPage } from '../components/modules/ThankyouPage';
 
 const IndexPage = () => (
   <>
-    <TalcommunityPage></TalcommunityPage>
-    <ThankyouPage></ThankyouPage>
+    <BoxContainer paddingVertical={0}>
+      <TalcommunityPage></TalcommunityPage>
+    </BoxContainer>
+    <BoxContainer paddingVertical={0}>
+      <ThankyouPage></ThankyouPage>
+    </BoxContainer>
   </>
 );
 
