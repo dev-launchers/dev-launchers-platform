9db1aaf8b74c76af6b426673aca8a545348175a7
Imaan <imaansoltan@gmail.com>
2024-02-09 13:35:00 -0800

    showCloseModal, and all other integrations

--- apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
@@ -2,16 +2,79 @@ import { useState, useEffect } from "react";
 import Modal from "react-modal";
 import PlatformOnboarding from "./PlatformOnboarding/PlatformOnboarding";
 import { ModalContainer, userUnboardingModalStyle } from "./StyledUserOnboardingModal";
-import { useOnboardingDataContext } from './../../../context/OnboardingDataContext';
+import { UseOnboardingData, useOnboardingDataContext } from './../../../context/OnboardingDataContext';
+import { onboardingActions } from './../../../../src/state/actions/onboardingActions';
 import IntroductionModal from "./IntroductionModal/IntroductionModal";
+import CloseModal from "./CloseModal/CloseModal";
+
 Modal.setAppElement("#__next");
 
-/**
- * @description This is custom modal for the user onboarding. 
- */
-export default function UserOnboardingModal() {
-    const { onboardingData: { showIntroductionModal, showPlatformOnboardingModal }, dispatch } = useOnboardingDataContext();
-    const [modalIsOpen, setModalIsOpen] = useState(showIntroductionModal || showPlatformOnboardingModal);
+// /**
+//  * @description This is custom modal for the user onboarding. 
+//  */
+
+// export default function UserOnboardingModal() {
+//     const { onboardingData: { showIntroductionModal, showPlatformOnboardingModal }, dispatch } = useOnboardingDataContext();
+//     const [modalIsOpen, setModalIsOpen] = useState(false);
+
+//     useEffect(() => {
+//         setModalIsOpen(showIntroductionModal || showPlatformOnboardingModal);
+//     }, [showIntroductionModal, showPlatformOnboardingModal]);
+
+//     const openModal = () => {
+//         setModalIsOpen(true);
+//     }
+
+//     const closeModal = () => {
+//         setModalIsOpen(false);
+//     }
+
+//     const showModals = () => {
+        
+//         if (showIntroductionModal) {
+//             return <IntroductionModal />;
+//         } else if (showPlatformOnboardingModal) {
+//             return <PlatformOnboarding />;
+//         } else if (showCloseModal) {
+//             console.log('closemodal', showCloseModal)
+//             return <CloseModal />
+//         } else {
+//             return null;
+//         }
+//     }
+    
+//     return (
+//         <>
+//             <Modal 
+//                 isOpen={modalIsOpen}
+//                 onAfterOpen={openModal} 
+//                 onRequestClose={closeModal}
+//                 style={userUnboardingModalStyle}
+//                 contentLabel="User Onboarding"
+//             >
+//                 <ModalContainer>
+//                     {showModals()}
+//                 </ModalContainer>
+//             </Modal>
+//         </>
+//     );
+// }
+export default function UserOnboardingModal({modalsToShow}) {
+    const { onboardingData, dispatch } = useOnboardingDataContext();
+    const tempData = useOnboardingDataContext();
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+
+    useEffect(() => {
+        console.log('data', tempData)
+        console.log('Modal conditions:', onboardingData?.showIntroductionModal, onboardingData?.showPlatformOnboardingModal);
+        console.log('HELLO', onboardingData)
+        console.log('MODALSTOSHOW', modalsToShow)
+        const showIntro = onboardingData?.showIntroductionModal;
+        const showPlatform = onboardingData?.showPlatformOnboardingModal;
+        const showClose = onboardingData?.showCloseModal;
+        setModalIsOpen(showIntro || showPlatform || showClose);
+    }, []);
+
 
     const openModal = () => {
         setModalIsOpen(true);
@@ -22,10 +85,12 @@ export default function UserOnboardingModal() {
     }
 
     const showModals = () => {
-        if (showIntroductionModal) {
+        if (modalsToShow?.showIntroductionModal) {
             return <IntroductionModal />;
-        } else if (showPlatformOnboardingModal) {
+        } else if (modalsToShow?.showPlatformOnboardingModal) {
             return <PlatformOnboarding />;
+        } else if (modalsToShow?.showCloseModal) {
+            return <CloseModal />
         } else {
             return null;
         }
@@ -33,11 +98,9 @@ export default function UserOnboardingModal() {
     
     return (
         <>
-            {/* "modalIsOpen ? true : false" set this way until we start adding typescript for 
-         boolean type */}
-            <Modal
-                isOpen={showIntroductionModal || showPlatformOnboardingModal}
-                onRequestOpen={openModal}
+            <Modal 
+                isOpen={modalIsOpen}
+                onAfterOpen={openModal} 
                 onRequestClose={closeModal}
                 style={userUnboardingModalStyle}
                 contentLabel="User Onboarding"
--- apps/user-profile/src/context/OnboardingDataContext.js
+++ apps/user-profile/src/context/OnboardingDataContext.js
@@ -2,15 +2,47 @@ import axios from 'axios';
 import { useState, useEffect, useReducer } from "react";
 import constate from 'constate';
 import { useUserDataContext } from './UserDataContext';
-import { onboardingReducer } from './../state/reducers'
+import { onboardingReducer, initialOnboardingState } from './../state/reducers'
+import { onboardingActions } from './../state/actions/onboardingActions';
 
+
+// export const UseOnboardingData = ({ children }) => {
+//     const [onboardingData, dispatch] = useReducer(onboardingReducer.onboardingReducer, onboardingReducer.initialOnboardingState);
+//     const { userData } = useUserDataContext();
+ 
+//     useEffect(() => {
+//         // setOnboardingData to show modal;
+
+
+
+//     }, [])
+//     return { onboardingData, dispatch };
+
+
+// }
+
+// const [OnboardingDataProvider, useOnboardingDataContext] = constate(UseOnboardingData);
+// export { OnboardingDataProvider, useOnboardingDataContext };
 export const UseOnboardingData = ({ children }) => {
     const [onboardingData, dispatch] = useReducer(onboardingReducer.onboardingReducer, onboardingReducer.initialOnboardingState);
     const { userData } = useUserDataContext();
  
     useEffect(() => {
-        // setOnboardingData to show modal;
-    }, [])
+        console.log('onboardingred', onboardingReducer.initialOnboardingState)
+        console.log('onboardingdat', onboardingData)
+        // const fetchOnboardingData = async () => {
+        //     try {
+        //         const response = await axios.get('/api/onboarding-data');
+        //         dispatch({
+        //             type: onboardingActions.SET_ONBOARDING_DATA,
+        //             payload: response.data
+        //         });
+        //     } catch (error) {
+        //     }
+        // };
+
+        // fetchOnboardingData();
+    }, []); 
 
     return { onboardingData, dispatch };
 }
