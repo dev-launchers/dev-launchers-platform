e3d2984d4dcca2c8c3c565f8fd226f5673c2533f
rinabo <rinaneman96@gmail.com>
2024-02-11 15:27:39 -0600

    Make <JourneySectionRight /> and <JourneySectionLeft /> into a single component called < JourneySection />

--- apps/user-profile/src/components/modules/LaunchYourJourney/JourneySection/JourneySection.js
+++ apps/user-profile/src/components/modules/LaunchYourJourney/JourneySection/JourneySection.js
@@ -7,54 +7,49 @@ import { Typography, Button } from '@devlaunchers/components/components/atoms';
  * @description  This component renders the JourneySection Component.
  * You can change the icon image for the section by passing an icon name from "Journey1", "Journey2", "Journey3", "Journey4" and "Journey5".
  * "Journey1" is profile powering, "Journey2" is template creation, "Journey3" is idea space, "Journey4" is learning and "Journey5" is collaboration.
+ * You can set an icon image on the preferred side by passing "right" or "left" for the side property.
  * @returns 
  */
 
-export function JourneySectionRight({
+
+export function JourneySection({
     iconImg,
     title,
     subtitle,
     buttonTitle,
-    clickFunction
+    clickFunction,
+    side,
 }) {
+    if (side=="right"){
+        return(
+            <>
+                <GroupContainer>
+                    <IndividualContainer>
+                        <IconImg iconImg={iconImg}/>
+                        <TextContainer>
+                            <Typography type="pLarge" className="journey-text">{title}</Typography>
+                            <Typography type="pSmall" className="journey-text">{subtitle}</Typography>
+                            <Button onClick={clickFunction} buttonType="alternative" buttonSize="standard" className="journey-button">{buttonTitle}</Button>
+                        </TextContainer>
+                    </IndividualContainer>
+                </GroupContainer>
+            </>
+        );
+    } else {
+        return(
+            <>
+                <GroupContainer>
+                    <IndividualContainer>
+                        <TextContainer>
+                            <Typography type="pLarge" className="journey-text">{title}</Typography>
+                            <Typography type="pSmall" className="journey-text">{subtitle}</Typography>
+                            <Button onClick={clickFunction} buttonType="alternative" buttonSize="standard" className="journey-button">{buttonTitle}</Button>
+                        </TextContainer>
+                        <IconImg iconImg={iconImg}/>
+                    </IndividualContainer>
+                </GroupContainer>
+            </>
+        );
+    }
 
-
-
-    return(
-        <>
-            <GroupContainer>
-                <IndividualContainer>
-                    <IconImg iconImg={iconImg}/>
-                    <TextContainer>
-                        <Typography type="pLarge" className="journey-text">{title}</Typography>
-                        <Typography type="pSmall" className="journey-text">{subtitle}</Typography>
-                        <Button onClick={clickFunction} buttonType="alternative" buttonSize="standard" className="journey-button">{buttonTitle}</Button>
-                    </TextContainer>
-                </IndividualContainer>
-            </GroupContainer>
-        </>
-    );
-};
-
-export function JourneySectionLeft({
-    iconImg,
-    title,
-    subtitle,
-    buttonTitle,
-    clickFunction
-}) {
-    return(
-        <>
-            <GroupContainer>
-                <IndividualContainer>
-                    <TextContainer>
-                        <Typography type="pLarge" className="journey-text">{title}</Typography>
-                        <Typography type="pSmall" className="journey-text">{subtitle}</Typography>
-                        <Button onClick={clickFunction} buttonType="alternative" buttonSize="standard" className="journey-button">{buttonTitle}</Button>
-                    </TextContainer>
-                    <IconImg iconImg={iconImg}/>
-                </IndividualContainer>
-            </GroupContainer>
-        </>
-    );
 };
