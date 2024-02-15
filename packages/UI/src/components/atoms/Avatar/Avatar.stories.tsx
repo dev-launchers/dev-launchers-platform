ed53d4689d59803be2245e7f0e093c3af756c0fc
Lamodot <91544048+lamodots@users.noreply.github.com>
2024-02-15 04:31:13 +0100

    feat(components): Added Avatar
    * Seperated the Avatar to a different branch
    
    * Fixed size issue
    
    * added design link to stories
    
    * Moved the Radix package from global package to package/ui/packages and refactored the components to meet PR Review
    
    * Completed the review changes
    
    * Updated the fallback
    
    * use correct icon
    
    * fix img having bg-gray-100
    
    * added animation for loading state
    
    * added actions and img from figma
    
    * remove state initialization
    
    * improve detectability for tailwind IntelliSense
    
    * refactor to use compoundSlots
    
    ---------
    
    Co-authored-by: Enjoy2Live <iMohammedMaqbol@gmail.com>

---
+++ packages/UI/src/components/atoms/Avatar/Avatar.stories.tsx
@@ -0,0 +1,66 @@
+import { action } from '@storybook/addon-actions';
+import type { Meta, StoryObj } from '@storybook/react';
+import Avatar from './Avatar';
+import base64img from './base64img';
+
+const meta: Meta<typeof Avatar> = {
+  component: Avatar,
+};
+
+export default meta;
+
+export const Default: StoryObj<typeof Avatar> = {
+  render: () => (
+    <Avatar
+      alt="Profile picture of John Doe"
+      src={`data:image/png;base64,${base64img}`}
+      delayMs={600}
+      onLoadingStatusChange={action('onLoadingStatusChange')}
+    />
+  ),
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=7760-6496&mode=design&t=1SqrP5S7pH7kkYa6-4',
+    },
+  },
+};
+
+export const FailToLoadFallback: StoryObj<typeof Avatar> = {
+  render: () => (
+    <Avatar
+      alt="Profile picture of John Doe"
+      src={`https://someUrlThatDoesNotReturnAnything.com/myPicture.png`}
+      delayMs={600}
+      onLoadingStatusChange={action('onLoadingStatusChange')}
+    />
+  ),
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=8575-7191&mode=design&t=1SqrP5S7pH7kkYa6-4',
+    },
+  },
+};
+
+export const Playground: StoryObj<typeof Avatar> = {
+  render({ src, delayMs, alt }) {
+    return (
+      <Avatar
+        src={Array.isArray(src) ? src[0] : src}
+        alt={alt}
+        delayMs={delayMs}
+        onLoadingStatusChange={action('onLoadingStatusChange')}
+      />
+    );
+  },
+  argTypes: { src: { control: { type: 'file', accept: '.png' } } },
+  args: {
+    alt: 'Profile picture of John Doe',
+    src: `data:image/png;base64,${base64img}`,
+    delayMs: 600,
+  },
+  parameters: {
+    docs: { disable: true },
+  },
+};
