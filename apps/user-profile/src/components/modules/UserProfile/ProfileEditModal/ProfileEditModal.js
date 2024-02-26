eb0e1a9f3c653ea255d2c99ebd51c66b6918d963
rinabo <rinaneman96@gmail.com>
2024-02-25 18:10:11 -0600

    Base Function of ProfileEditModal

---
+++ apps/user-profile/src/components/modules/UserProfile/ProfileEditModal/ProfileEditModal.js
@@ -0,0 +1,31 @@
+import { Typography } from "@devlaunchers/components/components/atoms";
+import { ModalHeader, ModalBody,userUnboardingModalStyle} from "./StyledProfileEditModal";
+import rocket from "../../../../../../../packages/UI/src/images/logo-monogram.png"
+import {useState} from "react";
+import Modal from "react-modal";
+Modal.setAppElement("#__next");
+
+export default function ProfileEditModal() {
+    const [modalIsOpen, setModalIsOpen] = useState(true);
+
+    const openModal = () => {
+        setModalIsOpen(true);
+    }
+
+    const closeModal = () => {
+        setModalIsOpen(false);
+    }
+
+    return (
+        <>
+            <Modal style={userUnboardingModalStyle} isOpen={modalIsOpen}>
+                <ModalHeader>
+                    <img src={rocket} className="rocket-img"></img>
+                    <Typography type="h4" className="navbar-title">Dev Launchers</Typography>
+                    <button className="close-button" onClick={() => closeModal(false)}>×</button>
+                </ModalHeader>
+                <ModalBody/>
+            </Modal>
+        </>
+    )
+}
