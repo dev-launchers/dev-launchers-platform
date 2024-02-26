efe42a5787684acd3c5449dce7a3d6db8b46ea77
unknown <ramyaperumaal@gmail.com>
2024-02-26 13:36:15 -0600

    fixing errors
    sfsw

--- apps/dev-recruiters/src/components/modules/TalcommunityPage/TalFormPage/signUpForm.tsx
+++ apps/dev-recruiters/src/components/modules/TalcommunityPage/TalFormPage/signUpForm.tsx
@@ -3,7 +3,7 @@ import theme from '@devlaunchers/components/src/styles/theme';
 import FormErrorScroller from '@devlaunchers/components/src/utils/formErrorScroller';
 import { Opportunity } from '@devlaunchers/models';
 // import { NewApplicant } from '@devlaunchers/models/newApplicant';
-import { Newtalcommuser } from '@devlaunchers/models/talcommuser';
+
 import { agent } from '@devlaunchers/utility';
 import { Field, Form, Formik, FormikHelpers } from 'formik';
 import { useState } from 'react';
@@ -13,6 +13,8 @@ import ConfirmationModal from '../../DetailedPage/Confirmation/ConfirmationModal
 <<<<<<< HEAD
 <<<<<<< HEAD
 import { useRouter } from 'next/router';
+import BoxContainer from '../../../common/BoxContainer';
+import { Wrapper } from '../StyledTalcommunityPage';
 
 // interface FormFields extends Omit<NewApplicant, 'level'> {
 //   level: NewApplicant['level'] | '';
@@ -70,103 +72,105 @@ export default function TalCommForm({ handleCloseModal, position }: Props) {
 >>>>>>> efd5608a (For talcomm)
 
   return (
-    <ThemeProvider theme={theme}>
-      <Formik
-        initialValues={{
-          name: '',
-          email: '',
-          roles: '',
-          skills: '',
-        }}
-        onSubmit={(
-          values: Talcommuser,
-          { setSubmitting }: FormikHelpers<Talcommuser>
-        ) => {
-          setSubmitting(true);
-          agent.Talcommuser.post({
-            ...values,
-            //@ts-ignore
-            // level: values.level.toLowerCase(),
-            skills: values.skills
-              .toString()
-              .split(',')
-              .map((skill) => ({ skill: skill })),
-            roles: 'title' as string, // role: position.title as string,
-            // project:  router.query.project as string,
-            // id: '5' as string, // id: position.id as string,
-          })
-            .then((res) => {
-              handleOpenConfirmationModal();
-              setSubmitting(false);
+    <Wrapper>
+      <BoxContainer>
+        <Formik
+          initialValues={{
+            name: '',
+            email: '',
+            roles: '',
+            skills: '',
+          }}
+          onSubmit={(
+            values: Talcommuser,
+            { setSubmitting }: FormikHelpers<Talcommuser>
+          ) => {
+            setSubmitting(true);
+            agent.Talcommuser.post({
+              ...values,
+              //@ts-ignore
+              // level: values.level.toLowerCase(),
+              skills: values.skills
+                .toString()
+                .split(',')
+                .map((skill) => ({ skill: skill })),
+              roles: 'title' as string, // role: position.title as string,
+              // project:  router.query.project as string,
+              // id: '5' as string, // id: position.id as string,
             })
-            .catch((error) => {
-              setSubmitting(false);
-              console.log(error);
-              console.log(error.response);
-              console.log(error.response.data);
-              console.log(error.response.status);
-            });
-        }}
-        validationSchema={SignupSchema}
-      >
-        {({ errors, setFieldValue, touched, values }) => (
-          <atoms.Box paddingInline="0.5rem" justifyContent="center">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18L18 6M6 6l12 12"
-            />
+              .then((res) => {
+                handleOpenConfirmationModal();
+                setSubmitting(false);
+              })
+              .catch((error) => {
+                setSubmitting(false);
+                console.log(error);
+                console.log(error.response);
+                console.log(error.response.data);
+                console.log(error.response.status);
+              });
+          }}
+          validationSchema={SignupSchema}
+        >
+          {({ errors, setFieldValue, touched, values }) => (
+            <atoms.Box paddingInline="0.5rem" justifyContent="center">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6 18L18 6M6 6l12 12"
+              />
 
-            <Form>
-              <atoms.Box flexDirection="column" margin="auto">
-                <atoms.Box
-                  flexDirection="column"
-                  gap="32px"
-                  paddingBlock="2rem"
-                  maxWidth="fit-content"
-                >
-                  <Field
-                    as={organisms.FormField}
-                    label="Your Full Name"
-                    placeholder="John Smith"
-                    id="name"
-                    name="name"
-                    required
-                    touched={touched['name']}
-                    error={errors.name}
-                  />
-                  <Field
-                    as={organisms.FormField}
-                    label="Your Email"
-                    placeholder="johnsmith@gmail.com"
-                    id="email"
-                    name="email"
-                    required
-                    touched={touched['email']}
-                    error={errors.email}
-                  />
+              <Form>
+                <atoms.Box flexDirection="column" margin="auto">
+                  <atoms.Box
+                    flexDirection="column"
+                    gap="32px"
+                    paddingBlock="2rem"
+                    maxWidth="fit-content"
+                  >
+                    <Field
+                      as={organisms.FormField}
+                      label="Your Full Name"
+                      placeholder="John Smith"
+                      id="name"
+                      name="name"
+                      required
+                      touched={touched['name']}
+                      error={errors.name}
+                    />
+                    <Field
+                      as={organisms.FormField}
+                      label="Your Email"
+                      placeholder="johnsmith@gmail.com"
+                      id="email"
+                      name="email"
+                      required
+                      touched={touched['email']}
+                      error={errors.email}
+                    />
 
-                  <Field
-                    as={organisms.FormField}
-                    label="What skills/expertise do you have?"
-                    placeholder="JavaScript, Python, React, etc.."
-                    required
-                    id="skills"
-                    name="skills"
-                    error={errors.skills}
-                    // onChange={handleChange}
-                  />
-                  <Field
-                    as={organisms.FormField}
-                    label="What roles are you interested in?"
-                    placeholder="Frontend Developer, Product Manager, Marketing etc.."
-                    required
-                    id="roles"
-                    name="roles"
-                    error={errors.roles}
-                    // onChange={handleChange}
-                  />
+                    <Field
+                      as={organisms.FormField}
+                      label="What skills/expertise do you have?"
+                      placeholder="JavaScript, Python, React, etc.."
+                      required
+                      id="skills"
+                      name="skills"
+                      error={errors.skills}
+                      // onChange={handleChange}
+                    />
+                    <Field
+                      as={organisms.FormField}
+                      label="What roles are you interested in?"
+                      placeholder="Frontend Developer, Product Manager, Marketing etc.."
+                      required
+                      id="roles"
+                      name="roles"
+                      error={errors.roles}
+                      // onChange={handleChange}
+                    />
 
+<<<<<<< HEAD
                   <atoms.Typography type="p">
                     We require users to be 18 years old or older. Please confirm
                     below.
@@ -209,19 +213,47 @@ export default function TalCommForm({ handleCloseModal, position }: Props) {
                       SUBMIT
 >>>>>>> 73419391 (#1469)
                     </atoms.Button>
+=======
+                    <atoms.Typography type="p">
+                      We require users to be 18 years old or older. Please
+                      confirm below.
+                    </atoms.Typography>
+                    <atoms.Checkbox
+                      label="I am 18 years old or older."
+                      disabled={false}
+                      onChange={handleSetCheckCheckbox}
+                      required
+                    />
+                    <atoms.Checkbox
+                      label="I have read and agreed to the terms and conditions"
+                      disabled={false}
+                      onChange={handleSetCheckCheckbox}
+                      required
+                    />
+                    <atoms.Box maxWidth="50%">
+                      <atoms.Button
+                        buttonSize="standard"
+                        buttonType="primary"
+                        as="a"
+                        onClick={routeChange} //add submit here to button property once backend is complete
+                      >
+                        SUBMIT
+                      </atoms.Button>
+                    </atoms.Box>
+>>>>>>> 06074bd6 (fixing errors)
                   </atoms.Box>
                 </atoms.Box>
-              </atoms.Box>
-              <FormErrorScroller focusAfterScroll />
-              <ConfirmationModal
-                showModal={showConfirmationModal}
-                handleOpenModal={handleOpenConfirmationModal}
-                handleCloseModal={handleCloseModal}
-              />
-            </Form>
-          </atoms.Box>
-        )}
-      </Formik>
-    </ThemeProvider>
+                <FormErrorScroller focusAfterScroll />
+                <ConfirmationModal
+                  showModal={showConfirmationModal}
+                  handleOpenModal={handleOpenConfirmationModal}
+                  handleCloseModal={handleCloseModal}
+                />
+              </Form>
+            </atoms.Box>
+          )}
+        </Formik>
+      </BoxContainer>
+    </Wrapper>
   );
 }
--- apps/dev-recruiters/src/pages/second.tsx
+++ apps/dev-recruiters/src/pages/second.tsx
@@ -1,12 +1,16 @@
 import Head from 'next/head';
 import ProductHeader from '../components/modules/DetailedPage/ProductHeader';
+import React from 'react';
 import SignUpForm from '../components/modules/TalcommunityPage/TalFormPage/signUpForm';
 import BoxContainer from '../components/common/BoxContainer';
 import TalcommunityPage from '../components/modules/TalcommunityPage';
+import TalFormPage from '../components/modules/TalcommunityPage/TalFormPage';
+import { HeaderTalPage } from '../components/modules/TalcommunityPage/HeaderTalPage';
 
 const SecondPage = () => (
   <>
     <BoxContainer>
-      <TalcommunityPage></TalcommunityPage>
+      {/* <TalcommunityPage></TalcommunityPage> */}
+      <HeaderTalPage></HeaderTalPage>
       <SignUpForm />
   </>
