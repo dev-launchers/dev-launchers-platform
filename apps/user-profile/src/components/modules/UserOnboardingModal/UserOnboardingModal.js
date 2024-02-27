968c5ef6785712806a4d7a757036902c8f73e8f7
Imaan <imaansoltan@gmail.com>
2024-02-27 12:24:19 -0800

    Update UserOnboardingModal.js
    removed comments and console logs

--- apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/UserOnboardingModal.js
@@ -13,62 +13,12 @@ Modal.setAppElement("#__next");
 //  * @description This is custom modal for the user onboarding. 
 //  */
 
-// export default function UserOnboardingModal() {
-//     const { onboardingData: { showIntroductionModal, showPlatformOnboardingModal }, dispatch } = useOnboardingDataContext();
-//     const [modalIsOpen, setModalIsOpen] = useState(false);
-
-//     useEffect(() => {
-//         setModalIsOpen(showIntroductionModal || showPlatformOnboardingModal);
-//     }, [showIntroductionModal, showPlatformOnboardingModal]);
-
-//     const openModal = () => {
-//         setModalIsOpen(true);
-//     }
-
-//     const closeModal = () => {
-//         setModalIsOpen(false);
-//     }
-
-//     const showModals = () => {
-        
-//         if (showIntroductionModal) {
-//             return <IntroductionModal />;
-//         } else if (showPlatformOnboardingModal) {
-//             return <PlatformOnboarding />;
-//         } else if (showCloseModal) {
-//             console.log('closemodal', showCloseModal)
-//             return <CloseModal />
-//         } else {
-//             return null;
-//         }
-//     }
-    
-//     return (
-//         <>
-//             <Modal 
-//                 isOpen={modalIsOpen}
-//                 onAfterOpen={openModal} 
-//                 onRequestClose={closeModal}
-//                 style={userUnboardingModalStyle}
-//                 contentLabel="User Onboarding"
-//             >
-//                 <ModalContainer>
-//                     {showModals()}
-//                 </ModalContainer>
-//             </Modal>
-//         </>
-//     );
-// }
 export default function UserOnboardingModal({modalsToShow}) {
     const { onboardingData, dispatch } = useOnboardingDataContext();
     const tempData = useOnboardingDataContext();
     const [modalIsOpen, setModalIsOpen] = useState(false);
 
     useEffect(() => {
-        console.log('data', tempData)
-        console.log('Modal conditions:', onboardingData?.showIntroductionModal, onboardingData?.showPlatformOnboardingModal);
-        console.log('HELLO', onboardingData)
-        console.log('MODALSTOSHOW', modalsToShow)
         const showIntro = onboardingData?.showIntroductionModal;
         const showPlatform = onboardingData?.showPlatformOnboardingModal;
         const showClose = onboardingData?.showCloseModal;
