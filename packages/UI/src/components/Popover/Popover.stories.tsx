13b2d0bf6a387e4c9ef83d78a8fd0a6852bdfa8f
l1keafox <raymond.ed.lewis@gmail.com>
2024-03-19 08:57:55 -0600

    Saving for push

--- packages/UI/src/components/Popover/Popover.stories.tsx
+++ packages/UI/src/components/Popover/Popover.stories.tsx
@@ -1,7 +1,11 @@
 import type { Meta, StoryObj } from '@storybook/react';
-
-import {Popover,  PopoverContent,
-    PopoverTrigger,} from './Popover';
+import Button from '../atoms/Button';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  PopoverClose,
+} from './Popover';
 
 const meta: Meta<typeof Popover> = {
   component: Popover,
@@ -16,8 +20,54 @@ type Story = StoryObj<typeof Popover>;
  * to learn how to use render functions.
  */
 export const Primary: Story = {
-  render: () => <Popover>
-  <PopoverTrigger>Open</PopoverTrigger>
-  <PopoverContent>Place content for the popover here.</PopoverContent>
-</Popover>,
+  render: () => (
+    <Popover>
+      <PopoverTrigger>
+        <Button buttonType="primary" buttonSize="standard">
+          Open
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent hasCloseBtn={false}>
+        {' '}
+        Click outside of the popover to close
+      </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const WithClose: Story = {
+  render: () => (
+    <Popover>
+      <PopoverTrigger>
+        <Button buttonType="primary" buttonSize="standard">
+          Open
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-96"> Click X to close </PopoverContent>
+    </Popover>
+  ),
+};
+
+export const UsingPopoverClose: Story = {
+  render: () => (
+    <Popover>
+      <PopoverTrigger>
+        <Button buttonType="primary" buttonSize="standard">
+          Open
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        hasCloseBtn={false}
+        className="flex w-96 items-center justify-between"
+      >
+        <p>Click button to close</p>
+        <PopoverClose>
+          <Button buttonType="primary" buttonSize="standard">
+            {' '}
+            Close{' '}
+          </Button>
+        </PopoverClose>
+      </PopoverContent>
+    </Popover>
+  ),
 };
--- packages/UI/src/components/Popover/Popover.tsx
+++ packages/UI/src/components/Popover/Popover.tsx
@@ -1,29 +1,61 @@
 'use client';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
+import { X } from 'lucide-react';
 import * as React from 'react';
-import { cn } from 'utils/classesMerger';
+import { cn } from '../../utils/classesMerger';
+import { VariantProps } from 'tailwind-variants';
+
+/**
+ * Popover component. HasCloseBtn will automatically add a button, if developers need another way to close the popover, you can use PopoverClose
+ * @see http://localhost:6006/?path=/docs/components-popover--docs
+ */
 
 const Popover = PopoverPrimitive.Root;
 const PopoverTrigger = PopoverPrimitive.Trigger;
 const PopoverAnchor = PopoverPrimitive.Anchor;
+const PopoverClose = PopoverPrimitive.Close;
 
 const PopoverContent = React.forwardRef<
   React.ElementRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = 'center', sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
-      ref={ref}
-      align={align}
-      sideOffset={sideOffset}
-      className={cn(
-        'z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2',
-        className
-      )}
-      {...props}
-    />
-  </PopoverPrimitive.Portal>
-))
+  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content> & {
+    /**
+     * Boolean when set to false will remove the close button in the top right corner of the dialog.
+     */
+
+    hasCloseBtn?: boolean;
+  }
+>(
+  (
+    {
+      className,
+      children,
+      align = 'center',
+      hasCloseBtn = true,
+      sideOffset = 4,
+      ...props
+    },
+    ref
+  ) => (
+    <PopoverPrimitive.Portal>
+      <PopoverPrimitive.Content
+        ref={ref}
+        align={align}
+        sideOffset={sideOffset}
+        className={cn(
+          'z-50 rounded-md border bg-white p-8 text-popover-foreground shadow-md outline-none data-open:animate-in data-closed:animate-out data-closed:fade-out-0 data-open:fade-in-0 data-closed:zoom-out-95 data-open:zoom-in-95',
+          className
+        )}
+        {...props}
+      >
+        {children}
+        {hasCloseBtn && (
+          <PopoverPrimitive.Close className="absolute right-8 top-8 flex items-center justify-center rounded-md bg-white drop-shadow-xl">
+            <X className="h-6 w-6" />
+          </PopoverPrimitive.Close>
+        )}
+      </PopoverPrimitive.Content>
+    </PopoverPrimitive.Portal>
+));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor }
+export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor, PopoverClose };
