887403c72d0d2d24cc2ff29d1e07ffadff95ebc9
Jude Amos <code@judeamos.com>
2024-04-20 21:32:48 +0100

    quick code refinement

---
+++ apps/user-profile/src/components/modules/UserProfile/Overview/Overview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ProfileHeader from './../ProfileHeader';
+function Overview() {
+  return (
+    <>
+      <div>
+        <ProfileHeader />
+      </div>
+    </>
+  );
+}
+
+export default Overview;
---
+++ apps/user-profile/src/components/modules/UserProfile/ProfileHeader/ProfileHeader.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function ProfileHeader() {
+  return <div>ProfileHeader</div>;
+}
+
+export default ProfileHeader;
