41819b1b7870bdfc0381d5bee48a045824a96619
l1keafox <raymond.ed.lewis@gmail.com>
2024-05-07 13:01:46 -0600

    Removed Component, doing directive

--- packages/UI/src/components/Typography/Typography.stories.tsx
+++ packages/UI/src/components/Typography/Typography.stories.tsx
@@ -1,20 +1,31 @@
-import type { Meta, StoryObj } from '@storybook/react';
-
-import { Typography } from './Typography';
+import type { Meta, Story } from '@storybook/react';
+// import { Typography } from './Typography';
 
 /**
- * # Typography
+ * @description This component is used to style text in different ways.
  * https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=11487-34822&mode=design&t=VpHuObF3jxmDeewf-0
  */
-
-const meta: Meta<typeof Typography> = {
-  component: Typography,
+const meta: Meta = {
+  title: 'Typography',
 };
 
 export default meta;
 
-type Story = StoryObj<typeof Typography>;
+// type Story = Story;
 
-export const DefaultUnchecked: Story = {
-  render: () => <Typography type="h1">Lorem ipsum</Typography>,
+export const DefaultUnchecked: Story = (args) => (
+  <h1 className="h1" {...args}>
+    Lorem ipsum
+  </h1>
+);
+
+DefaultUnchecked.args = {
+  type: 'h1',
+};
+
+DefaultUnchecked.parameters = {
+  design: {
+    type: 'figma',
+    url: 'https://www.figma.com/file/EwzuhhvTulvFRMvhTD5VAh/DL-Universal-Design-System?type=design&node-id=11487-34822&mode=design&t=VpHuObF3jxmDeewf-0',
+  },
 };
