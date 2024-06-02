3344d6801dc5ab6f1cc4d00c72164fbf40b6a326
[Hindu Chandanala] <hinduchandanala.nitw@gmail.com>
2024-06-02 11:28:12 -0500

    Imported Tailwind CSS into IdeaCardTag

--- apps/ideaspace/src/components/common/IdeaCard/IdeaCardTag.js
+++ apps/ideaspace/src/components/common/IdeaCard/IdeaCardTag.js
@@ -1,31 +1,38 @@
 import React, { useState } from 'react';
-import theme from '@devlaunchers/components/styles/theme';
 import { StatuBox } from './StyledIdeaCard';
+import { orange } from '@mui/material/colors';
 
 const IdeaCardTag = ({ status }) => {
   const [tag, setTag] = useState('');
   const [tagColor, setTagColor] = useState('');
 
   React.useEffect(() => {
-    if (status == 'submitted') {
+    // const rootStyles = getComputedStyle(document.documentElement);
+    // const primaryColorValue = rootStyles.getPropertyValue('--orange-orange-100').trim();
+    // const lightBlueColorValue = rootStyles.getPropertyValue('--light-blue-lightblue-100').trim();
+    // const yellowColorValue = rootStyles.getPropertyValue('--yellow-yellow-100').trim();
+    // const successColorValue = rootStyles.getPropertyValue('--success-success-100').trim();
+    // const blueColorValue = rootStyles.getPropertyValue('--blue-blue-100').trim();
+
+    if (status === 'submitted') {
       setTag('submitted');
-      setTagColor(theme.colors.LIGHT_BLUE_100);
-    } else if (status == 'applying') {
+      setTagColor('--light-blue-lightblue-100');
+    } else if (status === 'applying') {
       setTag('in-review');
-      setTagColor(theme.colors.YELLOW_100);
-    } else if (status == 'approved') {
+      setTagColor('--yellow-yellow-100');
+    } else if (status === 'approved') {
       setTag('approved');
-      setTagColor(theme.colors.SUCCESS_100);
-    } else if (status == 'archived') {
+      setTagColor('--success-success-100');
+    } else if (status === 'archived') {
       setTag('archived');
-      setTagColor(theme.colors.BLUE_100);
+      setTagColor('--blue-blue-100');
     } else {
       setTag('workshopping');
-      setTagColor(theme.colors.ORANGE_100);
+      setTagColor('--orange-orange-100');
     }
   }, [status]);
 
-  return <StatuBox style={{ background: tagColor }}>{tag}</StatuBox>;
+  return <div className={tagColor}> {tag} </div>;
 };
 
 export default IdeaCardTag;
