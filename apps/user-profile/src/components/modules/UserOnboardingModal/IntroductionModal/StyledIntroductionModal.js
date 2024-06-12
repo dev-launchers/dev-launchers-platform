a98200ae165343e64ac8847fdb215b5f5bbca2f2
neelutiwari <tiwarineelu59@gmail.com>
2024-06-12 11:14:12 -0500

    1803 tailwind styling

--- apps/user-profile/src/components/modules/UserOnboardingModal/IntroductionModal/StyledIntroductionModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/IntroductionModal/StyledIntroductionModal.js
@@ -1,27 +1,7 @@
-import styled from "styled-components";
-
-export const IntroductionContainer = styled.div`
-  height: 470px;
-  width: 470px;
-  padding: 30px 45px 50px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`;
-
-export const Header = styled.div`
-  width: 100%;
-`;
-
-export const Body = styled.div`
-  ${'' /* Enter styling */}
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-export const Footer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 15px;
-`
+export const styles = {
+  introductionContainer:
+    'h-[470px] w-[470px] p-[30px_45px_50px] flex flex-col justify-between',
+  header: 'w-full',
+  body: 'flex flex-col gap-[10px]',
+  footer: 'flex flex-col gap-[15px]',
+};
