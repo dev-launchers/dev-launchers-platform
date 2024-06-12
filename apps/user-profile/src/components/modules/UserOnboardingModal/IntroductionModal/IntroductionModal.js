d29e6c19935d61bd2a12858df9a410268aedda8e
neelutiwari <tiwarineelu59@gmail.com>
2024-06-12 11:28:08 -0500

    1813 tailwind styling

--- apps/user-profile/src/components/modules/UserOnboardingModal/IntroductionModal/IntroductionModal.js
+++ apps/user-profile/src/components/modules/UserOnboardingModal/IntroductionModal/IntroductionModal.js
@@ -1,11 +1,9 @@
-import { IntroductionContainer, Header, Body, Footer } from "./StyledIntroductionModal";
+import { styles } from "./StyledIntroductionModal";
 import { Typography } from "@devlaunchers/components/components/atoms";
 import Button from '@devlaunchers/components/components/atoms/Button';
 import RainbowBar from './../../../common/RainbowBar/RainbowBar';
 import { useOnboardingDataContext } from './../../../../context/OnboardingDataContext';
 import { onboardingActions } from './../../../../state/actions';
-
-
 
 /**
  * @description This is custom modal for the user onboarding. 
@@ -28,36 +26,31 @@ export default function IntroductionModal() {
     
 
     return (
-        <>
-            <IntroductionContainer >
-                <Header>
-                    <Typography type="h3" textAlign="center">
-                        Welcome to Dev Launchers
-                    </Typography>
-                    <div>
-                        <RainbowBar height="6px" />
-                    </div>
-                </Header>
-                <Body>
-                    <Typography type="p">
-                        We'll introduce you to Dev Launchers and set up your profile for team collaboration.
-                    </Typography>
-                    <Typography type="p">
-                        Rest assured, we'll guide you through each step of the process.
-                    </Typography>
-                </Body>
-                <Footer>
-                    <Button buttonType="primary" buttonSize="xl" onClick={onGetStarted}>
-                        
-                        GET STARTED (TAKES 2-5 MINUTES)
-                    </Button>
-
-                    <Button buttonType="alternative" buttonSize="xl" onClick={onSetUpLater}>
-                        SET UP LATER
-                    </Button>
-                </Footer>
-            </IntroductionContainer>
-        </>
-    );
-
-    }
+        <div className={styles.introductionContainer}>
+        <div className={styles.header}>
+            <Typography type="h3" textAlign="center">
+                Welcome to Dev Launchers
+            </Typography>
+            <div>
+                <RainbowBar height="6px" />
+            </div>
+        </div>
+        <div className={styles.body}>
+            <Typography type="p">
+                We'll introduce you to Dev Launchers and set up your profile for team collaboration.
+            </Typography>
+            <Typography type="p">
+                Rest assured, we'll guide you through each step of the process.
+            </Typography>
+        </div>
+        <div className={styles.footer}>
+            <Button buttonType="primary" buttonSize="xl" onClick={onGetStarted}>
+                GET STARTED (TAKES 2-5 MINUTES)
+            </Button>
+            <Button buttonType="alternative" buttonSize="xl" onClick={onSetUpLater}>
+                SET UP LATER
+            </Button>
+        </div>
+    </div>
+  );
+}
