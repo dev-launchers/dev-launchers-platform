326aa44e5487b125ad70415d434cba1c4d61acf8
chungthuang <7826979+chungthuang@users.noreply.github.com>
2024-06-16 11:58:39 -0500

    fix: Add eslint to warn about deprecated component

--- packages/eslint-config-bases/src/bases/typescript.js
+++ packages/eslint-config-bases/src/bases/typescript.js
@@ -28,6 +28,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:import/recommended',
     'plugin:import/typescript',
+    'plugin:eslint-plugin-deprecation/recommended',
   ],
   rules: {
     'spaced-comment': [
