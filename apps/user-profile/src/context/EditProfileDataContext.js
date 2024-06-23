129750787d0f63e253e1a92966a2642f46b72515
Jude Amos <code@judeamos.com>
2024-06-23 17:58:19 +0100

    formatting change

--- apps/user-profile/src/context/EditProfileDataContext.js
+++ apps/user-profile/src/context/EditProfileDataContext.js
@@ -1,27 +1,3 @@
-// import { createContext, useReducer } from "react";
-// import { editProfileReducer, editProfileState } from "../state/reducers/editProfileReducer";
-// import { editProfileActions } from "../state/actions";
-
-// export const EditProfileDataContext = createContext();
-
-// export const EditProfileDataProvider = ({children}) => {
-//     const [editProfileIsOpen, dispatch] = useReducer(editProfileReducer, editProfileState);
-
-//     const openModal = () => {
-//         dispatch({type: editProfileActions.SHOW_EDIT_PROFILE_MODAL});
-//     }
-
-//     const closeModal = () => {
-//         dispatch({type: editProfileActions.HIDE_EDIT_PROFILE_MODAL});
-//     }
-
-//     return (
-//         <EditProfileDataContext.Provider value={{ editProfileIsOpen, openModal, closeModal }}>
-//             {children}
-//         </EditProfileDataContext.Provider>
-//     );
-// };
-
 import axios from 'axios';
 import { useState, useEffect, useReducer } from 'react';
 import constate from 'constate';
@@ -38,4 +14,5 @@ export const EditProfileData = ({ children }) => {
 
 const [EditProfileDataProvider, editProfileDataContext] =
   constate(EditProfileData);
+
 export { EditProfileDataProvider, editProfileDataContext };
--- apps/user-profile/src/state/reducers/editProfileReducer.js
+++ apps/user-profile/src/state/reducers/editProfileReducer.js
@@ -8,11 +8,13 @@ const editProfileReducer = (state, action) => {
   switch (action.type) {
     case editProfileActions.SHOW_EDIT_PROFILE_MODAL: {
       return {
+        ...state,
         showEditProfileModal: true,
       };
     }
     case editProfileActions.HIDE_EDIT_PROFILE_MODAL: {
       return {
+        ...state,
         showEditProfileModal: false,
       };
     }
