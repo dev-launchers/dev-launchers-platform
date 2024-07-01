96479d0af4bac61ed98240940c7f0f3f5f892a17
neelutiwari <tiwarineelu59@gmail.com>
2024-06-30 19:24:29 -0500

    Side nav bar changes

--- apps/user-profile/src/components/modules/UserProfile/SideBar/SideBar.js
+++ apps/user-profile/src/components/modules/UserProfile/SideBar/SideBar.js
@@ -1,11 +1,17 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
+import {
+  useSidebarState,
+  useSidebarDispatch,
+} from './../../../../context/SidebarContext';
+import { useUserDataContext } from '@devlaunchers/components/context/UserDataContext';
 
 const images = require.context(
   './../../../../images/UserProfile',
   false,
   /\.(png|jpe?g|svg)$/
 );
+
 const getImage = (name) => {
   const image = images(`./${name}`);
   console.log(`Image loaded: ${name} -> ${image}`);
@@ -14,47 +20,42 @@ const getImage = (name) => {
 
 function Avatar({ src, alt }) {
   return (
-    <div className="flex justify-center items-center rounded-full overflow-hidden w-16 h-16">
+    <div className="flex justify-center items-center rounded-full overflow-hidden w-10 h-10">
       <img
         loading="lazy"
         src={src}
         alt={alt}
-        className="w-full h-full object-cover"
+        className="w-10 h-10 object-cover"
       />
     </div>
   );
 }
 
-function UserInfo({ name, title }) {
+function UserInfo({ name }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="text-sky-300 text-xl font-normal font-['Oswald'] leading-normal tracking-wide">
+      <div className="text-sky-blue text-md font-normal font-['Oswald'] leading-normal tracking-wide">
         {name}
-      </div>
-      <div className="text-zinc-100 text-sm font-normal font-['Nunito Sans'] leading-tight">
-        {title}
       </div>
     </div>
   );
 }
 
-export function NavItem({ icon, hoverIcon, label, href, isActive, onClick }) {
+export function NavItem({ icon, hoverIcon, label, isActive, onClick }) {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <div className="flex w-full">
       <a
-        href={href}
         onClick={onClick}
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className={`flex items-center w-full py-2 px-6 rounded-full transition-colors duration-200 ${
+        className={`flex items-center w-full py-2 px-6 rounded-full transition-colors duration-200 cursor-pointer ${
           isActive ? 'text-black bg-white' : 'text-white bg-black'
-        } ${isHovered ? 'hover:bg-white hover:text-black' : ''}`}
+        } ${isHovered || isActive ? 'hover:bg-white hover:text-black' : ''}`}
       >
         <img
-          loading="lazy"
-          src={isHovered ? hoverIcon : icon}
+          src={isHovered || isActive ? icon : hoverIcon}
           alt=""
           className="w-5 h-5 mr-3"
         />
@@ -64,86 +65,65 @@ export function NavItem({ icon, hoverIcon, label, href, isActive, onClick }) {
   );
 }
 
-const Sidebar = ({ isPublic, profilePictureUrl, displayName, title }) => {
+const Sidebar = () => {
+  const { userData } = useUserDataContext();
+  const state = useSidebarState();
+  const dispatch = useSidebarDispatch();
   const router = useRouter();
-  const routerPathname = router.pathname;
+  const { tab } = router.query;
+
+  useEffect(() => {
+    if (tab) {
+      dispatch({ type: 'SET_ACTIVE_TAB', payload: tab });
+    }
+  }, [tab]);
+
+  const handleTabClick = (tab) => {
+    dispatch({ type: 'SET_ACTIVE_TAB', payload: tab });
+    router.push(
+      {
+        pathname: router.pathname,
+        query: { tab },
+      },
+      undefined,
+      { shallow: true }
+    );
+  };
 
   const navItems = [
     {
       icon: getImage('overview.png'),
       hoverIcon: getImage('naoverview.png'),
       label: 'Overview',
-      href: '/UserProjects',
+      tab: 'overview',
     },
-    {
-      icon: getImage('projects.png'),
-      hoverIcon: getImage('naprojects.png'),
-      label: 'Projects',
-      href: '/UserProjects',
-    },
-    {
-      icon: getImage('profiles.png'),
-      hoverIcon: getImage('naprofiles.png'),
-      label: 'Profiles',
-      href: '/UserProfiles',
-    },
-    {
-      icon: getImage('ideas.png'),
-      hoverIcon: getImage('naideas.png'),
-      label: 'Ideas',
-      href: '/UserIdeas',
-    },
-    {
-      icon: getImage('opportunities.png'),
-      hoverIcon: getImage('naopportunities.png'),
-      label: 'Opportunities',
-      href: '/UserOpportunities',
-    },
+    // { icon: getImage('projects.png'), hoverIcon: getImage('naprojects.png'), label: 'Projects', tab: 'projects' },
+    // { icon: getImage('profiles.png'), hoverIcon: getImage('naprofiles.png'), label: 'Profiles', tab: 'profiles' },
+    // { icon: getImage('ideas.png'), hoverIcon: getImage('naideas.png'), label: 'Ideas', tab: 'ideas' },
+    // { icon: getImage('opportunities.png'), hoverIcon: getImage('naopportunities.png'), label: 'Opportunities', tab: 'opportunities' },
   ];
 
-  if (isPublic) {
-    return null;
-  }
-
   return (
-    <div className="flex flex-col gap-5 pt-6 text-white border-r-2 bg-black border-black border-solid shadow-sm bg-zinc-900 max-w-[288px]">
-      <div className="flex flex-col gap-5 w-full">
+    <div className="flex flex-col gap-5 pt-6 text-white border-r-2 bg-black border-black h-full border-solid shadow-sm bg-zinc-900 max-w-[288px]">
+      <div className="flex flex-col gap-5 w-full ">
         <div className="flex gap-3 self-start ml-8">
-          <Avatar src={profilePictureUrl} alt="Profile avatar" />
-          <UserInfo name={displayName} title="Software Engineer" />
+          <Avatar src={userData.profilePictureUrl} alt="Profile avatar" />
+          <UserInfo name={userData.name} />
         </div>
         <div className="gap-0 mt-5 w-full border border-solid bg-zinc-700 border-zinc-700 min-h-[1px]" />
       </div>
       <nav className="flex flex-col items-center gap-3 w-full text-base font-light tracking-wider text-white uppercase">
-        {navItems.map((item, index) => (
+        {navItems.map(({ icon, hoverIcon, label, tab }) => (
           <NavItem
-            key={index}
-            icon={item.hoverIcon}
-            hoverIcon={item.icon}
-            label={item.label}
-            href={item.href}
-            isActive={routerPathname === item.href}
-            onClick={(e) => {
-              e.preventDefault();
-              router.push(item.href);
-            }}
+            key={tab}
+            icon={icon}
+            hoverIcon={hoverIcon}
+            label={label}
+            isActive={state.activeTab === tab}
+            onClick={() => handleTabClick(tab)}
           />
         ))}
       </nav>
-      <div className="flex flex-col gap-0 items-center justify-center px-7 py-6 mt-auto w-full text-base font-light tracking-wider text-center uppercase border-t border-solid border-zinc-700 text-zinc-100">
-        <a
-          href="/logout"
-          className="flex gap-2.5 px-6 py-3 shadow-sm rounded-full hover:bg-zinc-800"
-        >
-          <img
-            loading="lazy"
-            src="/icons/logout.svg"
-            alt=""
-            className="w-5 h-5"
-          />
-          <div>Log out</div>
-        </a>
-      </div>
     </div>
   );
 };
--- apps/user-profile/src/components/modules/UserProfile/UserProfile.js
+++ apps/user-profile/src/components/modules/UserProfile/UserProfile.js
@@ -1,167 +1,127 @@
 import React, { useEffect, useState } from 'react';
-import 'react-tabs/style/react-tabs.css';
 import axios from 'axios';
 import { useRouter } from 'next/router';
-
 import Loader from './../../common/Loader';
 import { useUserDataContext } from '@devlaunchers/components/context/UserDataContext';
 import SideBar from './SideBar';
 import Overview from './Overview';
 import EditProfileModal from './EditProfileModal';
-import Modal from './../../../components/common/Modal'
+import Modal from './../../../components/common/Modal';
 import { editProfileDataContext } from '../../../context/EditProfileDataContext';
-// State management component
-/**
- * This component has been broken down into two,
- * 1. State management component (UserProfile) – initialising states, getting data from backeend.
- * 2. The view component (UserProfileView) – rendering the UI elements.
- * @export
- * @param {*} { publicUserData, isPublic }
- * @return {*}
- */
+import { useSidebarState } from '../../../context/SidebarContext';
+
 export default function UserProfile({ publicUserData, isPublic }) {
   const { userData, isAuthenticated } = useUserDataContext();
   const { editProfileState } = editProfileDataContext();
-
+  const state = useSidebarState();
   const [loading, setLoading] = useState(true);
-  const [opportunities, setOpportunities] = React.useState([]);
-  const [myProjects, setMyProjects] = React.useState([]);
-  const [projects, setProjects] = React.useState([]);
-  const [ideas, setIdeas] = React.useState([]);
-  const [people, setPeople] = React.useState([]);
-  const [interests, setInterests] = React.useState([]);
+  const [opportunities, setOpportunities] = useState([]);
+  const [myProjects, setMyProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
+  const [ideas, setIdeas] = useState([]);
+  const [people, setPeople] = useState([]);
+  const [interests, setInterests] = useState([]);
 
-  // If user hasn't set a username, redirect them to the signup form
   const router = useRouter();
-  React.useEffect(() => {
+
+  useEffect(() => {
     if (isAuthenticated && userData.name === '') router.push('/signup');
   }, [isAuthenticated]);
 
-  // Start Projects/Opportunities
-  React.useEffect(() => {
-    getProjectData();
+  useEffect(() => {
+    const fetchData = async () => {
+      await getProjectData();
+      await getIdeaData();
+      await getPeopleData();
+    };
+    fetchData();
   }, []);
-
-  const getProjectData = async () => {
-    await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/projects`)
-      .then(({ data }) => {
-        if (data) {
-          setProjects(data);
-          const tempOpportunities = [];
-          data.map((project) => {
-            project.opportunities.map((opportunity) => {
-              opportunity.project = project;
-              tempOpportunities.push(opportunity);
-            });
-          });
-          setOpportunities(tempOpportunities);
-        }
-      })
-      .catch(() => {
-        console.error('Could not fetch project data');
-      });
-  };
-
-  // React.useEffect(() => {
-  //   const myProjects = [];
-  //   projects.map((project) => {
-  //     [...project.team.leaders, ...project.team.members].map((member) => {
-  //       if (member.id == userData.id) myProjects.push(project);
-  //     });
-  //   });
-  //   setMyProjects(myProjects);
-  // }, [projects, userData]);
-  // End Projects/Opportunities
-
-  // Start Ideas
-  React.useEffect(() => {
-    getIdeaData();
-  }, []);
-  const getIdeaData = async () => {
-    await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/idea-cards`)
-      .then(({ data }) => {
-        if (data) {
-          setIdeas(data);
-        }
-      })
-      .catch(() => {
-        console.error('Could not fetch idea data');
-      });
-  };
-  // End Ideas
-
-  // Start People
-  React.useEffect(() => {
-    getPeopleData().catch(() => {
-      console.error(`Could not fetch People's data`);
-    });
-  }, []);
-  const getPeopleData = async () => {
-    const userCount = (
-      await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/count`)
-    ).data;
-    let randomUserIds = [
-      parseInt(Math.random() * userCount),
-      parseInt(Math.random() * userCount),
-      parseInt(Math.random() * userCount),
-      parseInt(Math.random() * userCount),
-      parseInt(Math.random() * userCount),
-      parseInt(Math.random() * userCount),
-    ];
-
-    let usersData = await Promise.all(
-      randomUserIds.map(
-        async (userId) =>
-          (
-            await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/${userId}`)
-          ).data
-      )
-    );
-
-    setPeople(usersData);
-  };
-  // End People
-
-  // Start Interests
-  React.useEffect(() => {
-    getInterests();
-  }, []);
-
-  const getInterests = async () => {
-    await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/interests`)
-      .then(({ data }) => {
-        data && setInterests(data);
-      })
-      .catch(() => {
-        console.error('Could not fetch interest data');
-      });
-  };
-  // End Interests
 
   useEffect(() => {
     setLoading(userData?.id === -1 || publicUserData?.id === -1);
   }, [publicUserData, userData]);
 
+  const getProjectData = async () => {
+    try {
+      const { data } = await axios(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/projects`
+      );
+      if (data) {
+        setProjects(data);
+        const tempOpportunities = [];
+        data.forEach((project) => {
+          if (project.opportunities) {
+            project.opportunities.forEach((opportunity) => {
+              opportunity.project = project;
+              tempOpportunities.push(opportunity);
+            });
+          }
+        });
+        setOpportunities(tempOpportunities);
+      }
+    } catch (error) {
+      console.error('Could not fetch project data', error);
+    }
+  };
+
+  const getIdeaData = async () => {
+    try {
+      const { data } = await axios(
+        `${process.env.NEXT_PUBLIC_STRAPI_URL}/idea-cards`
+      );
+      if (data) setIdeas(data);
+    } catch (error) {
+      console.error('Could not fetch idea data', error);
+    }
+  };
+
+  const getPeopleData = async () => {
+    try {
+      const userCount = (
+        await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/count`)
+      ).data;
+      let randomUserIds = Array.from({ length: 6 }, () =>
+        parseInt(Math.random() * userCount)
+      );
+      let usersData = await Promise.all(
+        randomUserIds.map(async (userId) => {
+          return (
+            await axios(`${process.env.NEXT_PUBLIC_STRAPI_URL}/users/${userId}`)
+          ).data;
+        })
+      );
+      setPeople(usersData);
+    } catch (error) {
+      console.error(`Could not fetch People's data`, error);
+    }
+  };
+
+  const renderContent = () => {
+    switch (state.activeTab) {
+      case 'overview':
+        return <Overview />;
+      case 'projects':
+        return <UserProjects myProjects={myProjects} />;
+      case 'profiles':
+        return <People people={people} />;
+      case 'ideas':
+        return <RecommendedIdeas ideas={ideas} />;
+      case 'opportunities':
+        return <Opportunities opportunities={opportunities} />;
+      default:
+        return <Overview />;
+    }
+  };
+
   return loading ? (
     <Loader />
   ) : (
-    <div className="flex flex-row bg-[#f9f9f9] h-screen">
+    <div className="flex flex-row bg-[#f9f9f9]">
       <div className="w-72">
-        <SideBar
-          isPublic={isPublic}
-          profilePictureUrl={
-            isPublic
-              ? publicUserData?.profile?.profilePictureUrl
-              : userData.profilePictureUrl
-          }
-          displayName={
-            isPublic ? publicUserData?.profile?.displayName : userData.name
-          }
-          title={isPublic ? publicUserData?.profile?.bio : userData.bio}
-        />
+        <SideBar />
       </div>
-      <div className="px-20 pb-20">
-        <Overview />
+      <div className="px-20 pb-20 w-full">
+        {renderContent()}
         {editProfileState.showEditProfileModal ? <EditProfileModal /> : null}
       </div>
     </div>
