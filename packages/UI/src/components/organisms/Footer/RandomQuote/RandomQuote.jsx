6775af7c376837cf99c1dd5999fffafb8064930f
Enjoy2Live <iMohammedMaqbol@gmail.com>
2024-07-01 18:48:28 +0300

    refactor quotes to not use top level await as cloudflare doesn't support

--- packages/UI/src/components/organisms/Footer/RandomQuote/RandomQuote.jsx
+++ packages/UI/src/components/organisms/Footer/RandomQuote/RandomQuote.jsx
@@ -1,22 +1,55 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Wrapper from './StyledRandomQuote';
 
-// Retrieve all quotes from our content layer
-const quotes =
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  (await import('@devlaunchers/components/src/content/collections/quotes.json'))
-    .data;
+const quotes = JSON.parse(`
+{
+  "_comment": "",
+  "_model": {
+    "author": "<string>",
+    "body": "<string>"
+  },
+
+  "data": [
+    {
+      "author": "Daniel Bell",
+      "body": "Technology, like art, is a soaring exercise of the human imagination."
+    },
+    {
+      "author": "Sukant Ratnakar",
+      "body": "Innovation is the outcome of a habit, not a random act."
+    },
+    {
+      "author": "Arthur C. Clarke",
+      "body": "Any sufficiently advanced technology is indistinguishable from magic."
+    },
+    {
+      "author": "Sean Gerety",
+      "body": "The technology you use impresses no one. The experience you create with it is everything."
+    },
+    {
+      "author": "Matt Mullenweg",
+      "body": "Technology is best when it brings people together."
+    },
+    {
+      "author": "Steve Jobs",
+      "body": "Technology is nothing. What’s important is that you have a faith in people, that they’re basically good and smart, and if you give them tools, they’ll do wonderful things with them."
+    }
+  ]
+}
+`).data;
 
 export default function RandomQuote() {
-  // Select and display a random quote from our quotes collection
-  const [randomNum, setRandomNum] = useState(0);
-  React.useEffect(() => {
+  const [randomNum, setRandomNum] = useState(
+    parseInt(quotes.length * Math.random(), 10)
+  );
+  useEffect(() => {
     setRandomNum(parseInt(quotes.length * Math.random(), 10));
   }, []);
+
   const randomQuote = quotes[randomNum];
   return (
     <Wrapper>
-      &quot;{randomQuote.body}&quot; - {randomQuote.author}
+      &quot;{randomQuote?.body}&quot; - {randomQuote?.author}
     </Wrapper>
   );
 }
