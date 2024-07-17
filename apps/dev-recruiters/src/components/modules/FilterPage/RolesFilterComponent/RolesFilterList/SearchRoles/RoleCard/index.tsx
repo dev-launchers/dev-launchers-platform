1910bbadcb269d8ed3a91fdeb77575a3efe26850
ajaywayase <ajaywayase98@gmail.com>
2024-07-17 12:36:34 -0700

    When roles are filtered, more details modal giving error

--- apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/RolesFilterList/SearchRoles/RoleCard/index.tsx
+++ apps/dev-recruiters/src/components/modules/FilterPage/RolesFilterComponent/RolesFilterList/SearchRoles/RoleCard/index.tsx
@@ -32,6 +32,7 @@ const RoleCard = ({ role, key22, opportunities }: Props) => {
 
   const handleOpenModal = () => {
     setShowModal(true);
+    console.log('opppeeen');
   };
 
   const handleCloseModal = () => {
@@ -60,16 +61,16 @@ const RoleCard = ({ role, key22, opportunities }: Props) => {
   return (
     <CardContainer>
       <CardContent>
-        <Title>{role.title}</Title>
+        <Title>{role?.attributes?.title}</Title>
         <Time style={{ fontSize: '1rem' }}>PRODUCT PLATFORM</Time>
         <Divider />
         <TimeContainer>
           <TimeCommitment>TIME COMMITMENT</TimeCommitment>
-          <Time>{role.commitmentHoursPerWeek} hrs per week</Time>
+          <Time>{role?.attributes?.commitmentHoursPerWeek} hrs per week</Time>
         </TimeContainer>
         <AboutContainer>
           <TimeCommitment>ABOUT THE ROLE</TimeCommitment>
-          <AboutDescription>{role.description}</AboutDescription>
+          <AboutDescription>{role?.attributes?.description}</AboutDescription>
         </AboutContainer>
         <ButtonsContainer onClick={handleOpenModal}>
           More Details
