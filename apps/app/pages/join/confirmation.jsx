04e371d72e9e4fa368d3a4cb1c634847c65359a2
Enjoy2Live <iMohammedMaqbol@gmail.com>
2024-07-21 06:17:14 +0300

    fix incorrect import path

--- apps/app/pages/join/confirmation.jsx
+++ apps/app/pages/join/confirmation.jsx
@@ -1,5 +1,5 @@
 import Page from '@devlaunchers/dev-recruiters/src/pages/confirmation';
-import App from '@devlaunchers/dev-recruiters/_app.js';
+import App from '@devlaunchers/dev-recruiters/src/pages/_app.jsx';
 export { getStaticProps } from '@devlaunchers/dev-recruiters/src/pages/confirmation';
 
 /////////////////////////////////////////
