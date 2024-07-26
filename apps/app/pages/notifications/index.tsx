d17f8f194ddf0b69cb628f3ac9607e9ea0aaf69e
Enjoy2Live <iMohammedMaqbol@gmail.com>
2024-07-26 16:20:33 +0300

    Add notifications route to the Nextjs app

---
+++ apps/app/pages/notifications/index.tsx
@@ -0,0 +1,9 @@
+import NotificationsPage from '@devlaunchers/components/src/components/pages/notification/Notifications';
+
+export default function NotificationsRoute() {
+  return (
+    <div className="py-10">
+      <NotificationsPage />
+    </div>
+  );
+}
