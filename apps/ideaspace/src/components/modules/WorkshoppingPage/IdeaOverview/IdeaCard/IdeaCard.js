c0abb840adfcf1db62fa019f74632b8ae7655c84
remi <remitoutin@gmail.com>
2024-07-29 17:35:50 -0500

    fix(update idea card buttons for workshopping page to FOLLOW, UPVOTE, and SHARE)

--- apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
@@ -1,6 +1,7 @@
 import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import StarBorderOutlinedIcon from '@mui/icons-material/StarBorderOutlined';
 import {
   StyledCard,
   TopView,
@@ -12,44 +13,35 @@ import {
   BottomView,
   Button,
   StyledText,
-} from './StyledIdeaCard'
+} from './StyledIdeaCard';
 
 export const IdeaCard = ({ ideaImage, ideaName, ideaTagLine }) => {
   return (
     <StyledCard>
       <TopView>
         <LeftView>
-          {
-            ideaImage ? <img src={ideaImage}/> : 
-            <StyledDiv>
-            </StyledDiv>
-          }
-          
+          {ideaImage ? <img src={ideaImage} /> : <StyledDiv></StyledDiv>}
         </LeftView>
         <RightView>
-          <IdeaName>
-            {ideaName}
-          </IdeaName>
-          <IdeaTagLine>
-            {ideaTagLine}
-          </IdeaTagLine>
+          <IdeaName>{ideaName}</IdeaName>
+          <IdeaTagLine>{ideaTagLine}</IdeaTagLine>
         </RightView>
       </TopView>
 
-      {/* <BottomView>
+      <BottomView>
         <Button>
           <BookmarkBorderOutlinedIcon />
-          <StyledText>Save</StyledText>
+          <StyledText>FOLLOW</StyledText>
+        </Button>
+        <Button>
+          <StarBorderOutlinedIcon />
+          <StyledText>UPVOTE</StyledText>
         </Button>
         <Button>
           <ShareOutlinedIcon />
-          <StyledText>Share</StyledText>
+          <StyledText>SHARE</StyledText>
         </Button>
-        <Button>
-          <FavoriteBorderOutlinedIcon />
-          <StyledText>Like</StyledText>
-        </Button>
-      </BottomView> */}
+      </BottomView>
     </StyledCard>
   );
 };
