ac8a4d1f9cbd2216734cea26ddb2ee296329f2be
remi <remitoutin@gmail.com>
2024-07-31 17:55:38 -0500

    fix(Create handleUpvoteClick)

--- apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
@@ -16,6 +16,44 @@ import {
 } from './StyledIdeaCard';
 
 export const IdeaCard = ({ ideaImage, ideaName, ideaTagLine }) => {
+  // a function to keep track of the number of upvotes and when the user clicks the upvote button for this idea
+  function handleUpvoteClick(event) {
+    if (liked) {
+      // if there's a like object corresponding to this user and idea, delete it
+
+      // Refresh the page so that the new number of upvotes is displayed
+      setState(true);
+
+      setLiked(false);
+    } else {
+      // create a like object using the Like collection from the strapiv4 repo, storing the user ID, the idea ID, and the "IdeaCard" object type
+      var likeData = {
+        objectId: this.id,
+        objectType: 'IdeaCard',
+        users_permission_user: userData.userId,
+      };
+
+      try {
+        const res = agent.Likes.post(likeData);
+      } catch (error) {
+        console.error(error);
+      }
+
+      event.preventDefault();
+
+      try {
+        props.setHandleTextChange('');
+      } catch (error) {
+        console.error(error);
+      }
+
+      // Refresh the page so that the new number of upvotes is displayed
+      setState(true);
+
+      setLiked(true);
+    }
+  }
+
   return (
     <StyledCard>
       <TopView>
@@ -33,7 +71,7 @@ export const IdeaCard = ({ ideaImage, ideaName, ideaTagLine }) => {
           <BookmarkBorderOutlinedIcon />
           <StyledText>FOLLOW</StyledText>
         </Button>
-        <Button>
+        <Button onClick={handleUpvoteClick}>
           <StarBorderOutlinedIcon />
           <StyledText>UPVOTE</StyledText>
         </Button>
