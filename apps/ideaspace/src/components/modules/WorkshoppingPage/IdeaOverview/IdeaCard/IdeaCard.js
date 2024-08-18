165a1984cdd5aac87f8e26f966a15dbc410d1974
remi <remitoutin@gmail.com>
2024-08-18 11:00:32 -0500

    fix(Workshopping page can be accessed without login)

--- apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
+++ apps/ideaspace/src/components/modules/WorkshoppingPage/IdeaOverview/IdeaCard/IdeaCard.js
@@ -37,7 +37,7 @@ export const IdeaCard = ({ ideaImage, ideaId, ideaName, ideaTagLine }) => {
     setCount(data.length);
     // check if user has already liked idea
     for (let i = 0; i < data.length; i++) {
-      if (data[i].users_permissions_user.data.id == userData.id) {
+      if (data[i].users_permissions_user.data?.id == userData.id) {
         setUpvoted(true);
       }
     }
