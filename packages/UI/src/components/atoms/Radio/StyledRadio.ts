d4cbffcf4e2df6053d71d48ac88cc30ae69345a3
remi <remitoutin@gmail.com>
2024-09-01 10:55:08 -0500

    fix(Spacing between radio buttons)

--- packages/UI/src/components/atoms/Radio/StyledRadio.ts
+++ packages/UI/src/components/atoms/Radio/StyledRadio.ts
@@ -5,7 +5,7 @@ export const Option = styled.label`
   font-family: ${({ theme }) => theme.fonts.normal};
   display: flex;
   align-items: center;
-  gap: 0.5rem;
+  gap: 0.75rem;
   list-style: none;
   padding: 0.5rem 1rem;
   border-radius: 8px;
@@ -16,6 +16,8 @@ export const Option = styled.label`
   p {
     pointer-events: none;
   }
+
+  margin-bottom: 0.25rem;
 `;
 
 export const Input = styled.input`
