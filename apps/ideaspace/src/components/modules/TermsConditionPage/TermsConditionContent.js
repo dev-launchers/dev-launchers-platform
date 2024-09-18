332ae62c94cbd9a1c3390c09fd97cd11dda1e7f5
Ahsaask <109789003+Ahsaask@users.noreply.github.com>
2024-09-17 20:15:37 -0600

    Layout, Styling and content added for terms and condition IdeaSpace

--- apps/ideaspace/src/components/modules/TermsConditionPage/TermsConditionContent.js
+++ apps/ideaspace/src/components/modules/TermsConditionPage/TermsConditionContent.js
@@ -9,11 +9,44 @@ const TermsConditionContent = {
   definitions: {
     heading: 'Definitions',
     paragraphs: [
-      '“Idea” and “Ideas” refers to Content related to a project that was submitted or shared to Dev Launchers Services. ',
-      '“Services” refers to the use of any service or product provided by Dev Launchers.',
-      '“Community” refers to any community features or forums made available by Dev Launchers.',
-      '“Content”  means any content, information, text, data, graphic, messages, sounds, videos, code, scripts, software, services or other materials.',
-      '“Terms” refers to the information contained in this Terms and Conditions.',
+      [
+        { text: '“Idea”', bold: true },
+        { text: ' and ', bold: false },
+        { text: '“Ideas”', bold: true },
+        {
+          text: ' refers to Content related to a project that was submitted or shared to Dev Launchers Services.',
+        },
+      ],
+      [
+        { text: '“Services”', bold: true },
+        {
+          text: ' refers to the use of any service or product provided by Dev Launchers.',
+        },
+      ],
+      [
+        { text: '“Community”', bold: true },
+        {
+          text: ' refers to any community features or forums made available by Dev Launchers.',
+        },
+      ],
+      [
+        { text: '“Content”', bold: true },
+        {
+          text: ' means any content, information, text, data, graphic, messages, sounds, videos, code, scripts, software, services or other materials.',
+        },
+      ],
+      [
+        { text: '“Terms”', bold: true },
+        {
+          text: ' refers to the information contained in this Terms and Conditions.',
+        },
+      ],
+    ],
+  },
+  summary: {
+    heading: 'Summary',
+    paragraphs: [
+      'You understand and agree to be bound to the following terms and conditions when you submit your idea, use and collaborate in Dev Launchers website.',
     ],
   },
   ageEligibility: {
@@ -30,6 +63,66 @@ const TermsConditionContent = {
       'You are free to submit your idea to other organizations without informing us.',
     ],
   },
+  userGenerated: {
+    heading: 'User-Generated Content',
+    ownership: {
+      heading: 'Ownership of Content, Right to Post, and License Grants',
+      pointers: [
+        "You retain ownership of and responsibility for your Content. If you're submitting an idea you don’t own or do not own the rights to, you agree that you are responsible for any Content you submit; that you will only submit Content that you have the right to post; and that you will fully comply with any third-party licenses relating to Content you post.",
+        'You must own the Idea you are submitting. You cannot submit the Idea of another person or party.',
+        'Often, employers will require an employee to sign an employment contract. Please determine whether your employer owns the Idea prior to your submission. Dev Launchers does not wish to receive Ideas that are in violation of an agreement or understanding with another party, such as your current or former employer.',
+      ],
+    },
+    confidentiality: {
+      heading: 'Confidentiality',
+      pointers: [
+        'All ideas submitted to Dev Launchers will be treated as confidential information. We will not disclose or share any information regarding the submitted ideas with third parties without the express consent of the submitter.',
+        'However, we cannot guarantee the confidentiality of any information that is transmitted over the internet.',
+      ],
+    },
+    consentForUse: {
+      heading: 'Consent for Use',
+      pointers: [
+        'We need the legal right to do things like host your Content, publish it, and share it. By submitting an idea to Dev Launchers, you grant us and our legal successors a non-exclusive, royalty-free, irrevocable, worldwide license to store, archive, use, reproduce, distribute, and display your Content for any purpose, including but not limited to development, marketing, and commercialization.',
+        'You also grant us the right to make incidental copies, as necessary to provide the Service, including improving the Service over time.',
+      ],
+    },
+    licenseGrantToOtherUsers: {
+      heading: 'License Grant to Other Users',
+      pointers: [
+        'Any Content you post publicly, including Ideas, comments, and contributions to Ideas of other members, may be viewed by others.',
+      ],
+    },
+    responsibilityForContent: {
+      heading: 'Responsibility for Content',
+      pointers: [
+        'The information you are submitting to Dev Launchers is true and correct.',
+        'You may create or upload Content while using the platform. You are solely responsible for the Content of, and for any harm resulting from, any Content that you post, upload, link to, or otherwise make available via the Service, regardless of the form of that Content.',
+        'We are not responsible for any loss, misuse, or damage of your Content. By submitting an idea, you acknowledge and agree that you are solely responsible for the content of your submission. We reserve the right to disqualify any submitter who violates these terms and conditions.',
+      ],
+    },
+    organizationNotResponsible: {
+      heading: 'Organization Not Responsible',
+      pointers: [
+        'We have no obligation to (but may, in our discretion) monitor or review Content. We have no responsibility for and make no promises about Content you may encounter on the Community, including whether it infringes third-party rights or its reliability, accuracy, usefulness, or safety.',
+        'You may find Content on the Community to be offensive, indecent, or objectionable. However, you agree not to hold us responsible in any way for any Content you encounter.',
+      ],
+    },
+    contentDeletion: {
+      heading: 'Content Deletion',
+      pointers: [
+        'Once Content is submitted, Dev Launchers maintains the right to host said Content in perpetuity barring any other provisions made in these terms, or if we receive a valid legal request for content to be removed.',
+        'In such cases, we will notify the submitter via email and provide them with a reason for the removal. All users maintain the rights and ability to remove their Personally Identifiable Information stored in our systems and attached to any Content submitted to the platform at any time, for any reason.',
+      ],
+    },
+    removalOfContent: {
+      heading: 'Dev Launchers May Remove Content',
+      pointers: [
+        'We reserve the right to remove any Content at any time without notice, for any or no reason, including if any such Content violates these Terms.',
+        'We do not promise to store or make available on the platform any of your Content or any other Content for any length of time.',
+      ],
+    },
+  },
   intellectualPropertyRights: {
     heading: 'Intellectual Property Rights',
     paragraphs: [
@@ -43,7 +136,13 @@ const TermsConditionContent = {
       'Dev Launchers agree to review your Idea when properly submitted via the Idea submission process. We have no obligation to use your Idea.',
       'Following the preliminary evaluation, if we are interested in pursuing your project, you will be notified per our process.',
       'You will not be compensated for Ideas you submitted to Dev Launchers or any projects developed by Dev launchers.',
-      // Add sub-heading object
+    ],
+    selectionProcess:
+      'The submitted ideas will be reviewed by our team, who will evaluate them based on factors such as originality, feasibility, and market potential. The decisions of our team are final, and we reserve the right to reject any ideas submitted for any reason.',
+    subPointers: [
+      'Confidential information of any type',
+      'Product names, slogans or logos',
+      'Business methods or ways of conducting our business',
     ],
   },
   consulting: {
@@ -56,7 +155,10 @@ const TermsConditionContent = {
     heading: 'Indemnity',
     paragraphs: [
       'You will defend (at our request), indemnify and hold harmless us from and against any claim by a third party arising from or related to: ',
-      // Add sub pointers
+    ],
+    subPointers: [
+      '(a) your use or attempted use of Dev Launchers in violation of these Terms, ',
+      '(b) your violation of any law or rights of any third party, or © any Content you submit to Dev Launchers, including any claim of infringement, misappropriation or violation of any intellectual property, privacy or other rights.',
     ],
   },
   changeToTerms: {
--- apps/ideaspace/src/components/modules/TermsConditionPage/TermsTable.js
+++ apps/ideaspace/src/components/modules/TermsConditionPage/TermsTable.js
@@ -3,17 +3,17 @@ import React from 'react';
 const TermsTable = () => {
   return (
     <>
-      <div class="relative overflow-x-auto shadow-md sm:rounded-lg mb-16 ">
-        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
-            Our products
-            <p class="mt-1 text-sm font-normal text-gray-500 dark:text-gray-400">
-              Browse a list of Flowbite products designed to help you work and
-              play, stay organized, get answers, keep in touch, grow your
-              business, and more.
+      <div class="relative overflow-x-auto shadow-md sm:rounded-xl mb-16 ">
+        <table class="w-full text-sm text-left rtl:text-right text-gray-500 ">
+          <caption class="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white border-t ">
+            Summary
+            <p class="mt-1 text-sm font-normal text-gray-800 ">
+              You understand and agree to be bound to the following terms and
+              conditions when you submit your idea, use and collaborate in Dev
+              Launchers website.
             </p>
           </caption>
-          <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <thead class="text-xs text-gray-700 uppercase bg-[#F0EDEE]">
             <tr>
               <th scope="col" class="px-6 py-3">
                 Section
@@ -24,78 +24,78 @@ const TermsTable = () => {
             </tr>
           </thead>
           <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Age Eligibility
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 How old do you have to be to use our platform?
               </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b ">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Non-confidentiality
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 This section warns you about not submitting confidential
                 information.
               </td>
             </tr>
-            <tr class="bg-white dark:bg-gray-800">
+            <tr class="bg-white">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 User-Generated Content
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 You own the content you submit on Dev Launchers. However, you
                 have some responsibilities regarding it, and we reserve some
                 rights about it.
               </td>
             </tr>
 
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Intellectual Property Rights
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 Talks about your understanding that you must seek appropriate
                 legal protection for your IP Rights.
               </td>
             </tr>
 
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Idea evaluation and approval
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 Talks about how Dev launchers IdeaSpace reviews, evaluates and
                 selects ideas and what we aren’t obligated to provide to idea
                 owners.
               </td>
             </tr>
 
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Indemnity
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 You will defend (at our request), indemnify and hold harmless us
                 from and against any claim by a third party related to your use
                 of the terms and conditions or your violation of any law or
@@ -103,14 +103,14 @@ const TermsTable = () => {
               </td>
             </tr>
 
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <tr class="bg-white border-b">
               <th
                 scope="row"
-                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
               >
                 Changes to Terms
               </th>
-              <td class="px-6 py-4">
+              <td class="px-6 py-4 text-gray-800">
                 We may modify this agreement, but we will use reasonable efforts
                 to notify you of the changes.
               </td>
