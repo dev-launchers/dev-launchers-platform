cda2447a0159bd4494d453d4c19351a46e5095d2
Jude Amos <code@judeamos.com>
2024-10-17 23:29:05 +0100

    renaming variables

--- apps/user-profile/src/components/common/BubbleList/BubbleList.js
+++ apps/user-profile/src/components/common/BubbleList/BubbleList.js
@@ -3,12 +3,12 @@ import { Typography } from '@devlaunchers/components/components/atoms';
 function BubbleList({ list, bubbleColorClass = 'bg-white' }) {
   return (
     <div className="flex gap-4 flex-wrap">
-      {list.map((value) => {
-        return value ? (
+      {list.map((item) => {
+        return item ? (
           <div
             className={`flex justify-center items-center h-10 px-4 rounded-3xl shadow-lg ${bubbleColorClass}`}
           >
-            <Typography type="p">{value}</Typography>
+            <Typography type="p">{item}</Typography>
           </div>
         ) : null;
       })}
