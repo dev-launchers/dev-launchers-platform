9cdbb855986bbeea70872d5397cde80d33aa175a
Jude Amos <code@judeamos.com>
2024-10-20 17:28:39 +0100

    removing old comments

--- apps/user-profile/src/components/common/RadioCards/RadioCards.js
+++ apps/user-profile/src/components/common/RadioCards/RadioCards.js
@@ -1,11 +1,5 @@
 import { Typography } from '@devlaunchers/components/components/atoms';
 
-/**
- * @description  This component renders the RoleCard Component.
- * You can change the color of title by passing an theme name from "theme1", "theme2" and "theme3".
- * "theme1" is navy, "theme2" is light blue and "theme3" is orange.
- * @returns
- */
 export default function RadioCards({ cards = config, className }) {
   return (
     <div className={`flex gap-6 ${className}`}>
