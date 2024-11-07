b3d92003a978c576a3d2b60359221cdafd36cef9
Yacine Kahlerras <kahlerasse@gmail.com>
2024-11-08 00:33:22 +0100

    removed comented stories

--- packages/UI/src/components/atoms/NavLink/NavLink.stories.tsx
+++ packages/UI/src/components/atoms/NavLink/NavLink.stories.tsx
@@ -5,13 +5,3 @@ export default {
   title: 'Deprecated/Atoms/NavLink',
   component: NavLink,
 } as Meta<typeof NavLink>;
-
-// const NavLinkTemplate: ComponentStory<typeof NavLink> = (args) => (
-//   <NavLink {...args}>Navigation Link</NavLink>
-// );
-
-// export const Light = NavLinkTemplate.bind({});
-// Light.args = {
-//   href: '/anywhere',
-//   text: 'Learn',
-// };
