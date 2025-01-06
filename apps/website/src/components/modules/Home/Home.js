bf12b5295e4461753879c609c33afb981053cd9d
Anna Isaacks <annaisaacksdesign@gmail.com>
2025-01-05 18:17:01 -0600

    Home center max width update

--- apps/website/src/components/modules/Home/Home.js
+++ apps/website/src/components/modules/Home/Home.js
@@ -10,7 +10,7 @@ import {
 
 const Home = () => {
   return (
-    <main className=" relative min-h-screen bg-black text-white flex flex-col justify-center gap-8 ">
+    <main className=" relative min-h-screen mx-auto items-center bg-black text-white flex flex-col justify-center gap-8 ">
       <Hero />
       <Opportunities />
       <JoinUs />
