2275a245a89cc906cdcff153acd52a521f2e7b0b
Anna Isaacks <annaisaacksdesign@gmail.com>
2025-01-06 12:19:27 -0600

    Updates to the resource page - centered and max width applied

--- apps/website/src/pages/resources.js
+++ apps/website/src/pages/resources.js
@@ -13,7 +13,6 @@ import { useSheetsContext } from '../context/SheetsContext';
 
 import PageBody from '../../src/components/common/PageBody';
 import Section from '../../src/components/common/Section';
-import Sec from '../../src/components/common/Section';
 
 export default function Resources() {
   const { learnPageData, createPageData } = useSheetsContext();
@@ -23,41 +22,43 @@ export default function Resources() {
       <Head>
         <title>Resources</title>
       </Head>
-      <main className="min-h-screen bg-black text-white">
-        <section className="max-w-full bg-black py-32 px-4 md:px-8">
-          <h1
-            className="text-7xl max-w-3xl font-bold text-left leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white"
-            style={{ fontSize: '4.5rem' }}
-          >
-            Resources for Growth
-          </h1>
-          <p className="text-xl text-left max-w-3xl  text-gray-500  font-normal">
-            Embark on a path to expanding your technological prowess. Whether
-            you're just starting or looking to refine specific skills, our
-            curated resources are here to guide you every step of the way. Dive
-            into a comprehensive collection of tools, tutorials, and hands-on
-            projects designed to enhance your understanding and practical
-            knowledge.
-          </p>
+      <main className="flex flex-col justify-center items-center mx-auto min-h-screen bg-black text-white">
+        <section className="max-w-7xl py-32 px-4 md:px-8">
+          <div className="flex flex-col justify-center items-center">
+            <h1
+              className="text-7xl max-w-3xl font-bold text-center leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white"
+              style={{ fontSize: '4.5rem' }}
+            >
+              Resources for Growth
+            </h1>
+            <p className="text-xl text-center max-w-4xl  text-gray-500  font-normal">
+              Embark on a path to expanding your technological prowess. Whether
+              you're just starting or looking to refine specific skills, our
+              curated resources are here to guide you every step of the way.
+              Dive into a comprehensive collection of tools, tutorials, and
+              hands-on projects designed to enhance your understanding and
+              practical knowledge.
+            </p>
+          </div>
         </section>
 
-        <section className="max-w-full bg-black pt-4 pb-10 px-4 md:px-8">
-          <div className="py-16">
-            <h2 className="text-6xl max-w-3xl font-bold text-left leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
+        <section className="max-w-7xl  pt-4 pb-10 px-4 md:px-8">
+          <div className="flex flex-col justify-center items-center py-16">
+            <h2 className="text-6xl max-w-3xl font-bold text-center leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
               Resources by Category
             </h2>
-            <p className="text-lg text-left max-w-4xl  text-gray-500  font-normal">
+            <p className="text-lg text-center max-w-3xl  text-gray-500  font-normal">
               Explore a diverse array of resources organized by category to find
               exactly what you need to progress. From coding tools and design
               software to advanced tutorials, our categories are designed to
               cater to learners at all levels.
             </p>
           </div>
-          <div>
-            <Tabs2 defaultValue="code" className="">
+          <div className="flex flex-col justify-center items-center py-4">
+            <Tabs2 defaultValue="dev" className="w-full">
               <TabsList
                 className="flex flex-row justify-start
-               py-8 px-4  gap-8 bg-gray-900"
+                  py-8 px-4  gap-8 bg-gray-900"
               >
                 <TabsTrigger
                   className=" text-xl focus:bg-purple-500"
@@ -316,24 +317,24 @@ export default function Resources() {
           </div>
         </section>
 
-        <section className="max-w-full py-24 px-4 md:px-8">
-          <div className="py-16">
-            <h2 className="text-6xl max-w-3xl font-bold text-left leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
+        <section className="max-w-7xl  pt-4 pb-10 px-4 md:px-8">
+          <div className="flex flex-col justify-center items-center py-16">
+            <h2 className="text-6xl max-w-3xl font-bold text-center leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
               Learning and Games
             </h2>
-            <p className="text-lg text-left max-w-4xl  text-gray-500  font-normal">
+            <p className="text-lg text-center max-w-3xl  text-gray-500  font-normal">
               Learn Code and Design using these games to build conceptual
               learning to prepare for creating software projects and
               applications
             </p>
           </div>
 
-          <div class="">
+          <div class="flex flex-col justify-center items-center py-4">
             <Tabs2
               defaultValue={Object.keys(learnPageData)[0]}
               className="w-full"
             >
-              <TabsList className="flex flex-row justify-start py-8 px-4  gap-8 bg-gray-900">
+              <TabsList className="flex flex-row justify-center py-8 px-4  gap-8 bg-gray-900">
                 {Object.keys(learnPageData).map((category) => (
                   <TabsTrigger
                     key={category}
@@ -393,12 +394,12 @@ export default function Resources() {
             </Tabs2>
           </div>
         </section>
-        <section className="max-w-full py-24 px-4 md:px-8">
-          <div className="py-16">
-            <h2 className="text-6xl max-w-3xl font-bold text-left leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
+        <section className="max-w-7xl pt-4 pb-10 px-4 md:px-8">
+          <div className="flex flex-col justify-center items-center py-16">
+            <h2 className="text-6xl max-w-3xl font-bold text-center leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
               Create with Templates
             </h2>
-            <p className="text-lg text-left max-w-4xl  text-gray-500  font-normal">
+            <p className="text-lg text-center max-w-3xl  text-gray-500  font-normal">
               Ready to test your skills? These templates are great ways to get
               started and reinforce your learning
             </p>
@@ -409,7 +410,7 @@ export default function Resources() {
               defaultValue={Object.keys(createPageData)[0]}
               className="w-full"
             >
-              <TabsList className="flex flex-row justify-start py-8 px-4  gap-8 bg-gray-900">
+              <TabsList className="flex flex-row justify-center py-8 px-4  gap-8 bg-gray-900">
                 {Object.keys(createPageData).map((category) => (
                   <TabsTrigger
                     key={category}
@@ -469,21 +470,23 @@ export default function Resources() {
             </Tabs2>
           </div>
         </section>
-        <section className="max-w-full py-24 px-4 md:px-8">
-          <h2 className="text-6xl max-w-3xl font-bold text-left leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
-            Create and Collaborate
-          </h2>
-          <p className="text-lg text-left max-w-4xl  text-gray-500  font-normal">
-            Join us in real cross-functional teams to apply your skills to real
-            world applications
-          </p>
-          <div className="flex pt-8">
-            <a
-              className="bg-purple-900 border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:text-purple-900 hover:font-semibold  hover:bg-purple-500 hover:border-purple-300 hover:border-4"
-              href="/join"
-            >
-              Join Today
-            </a>
+        <section className="max-w-7xl pt-4 pb-10 px-4 md:px-8">
+          <div className="flex flex-col justify-center items-center">
+            <h2 className="text-6xl max-w-3xl font-bold text-center leading-relaxed tracking-wide border-none border-0 m-0 p-0 text-white">
+              Create and Collaborate
+            </h2>
+            <p className="text-lg text-center max-w-3xl  text-gray-500  font-normal">
+              Join us in real cross-functional teams to apply your skills to
+              real world applications
+            </p>
+            <div className="flex pt-8">
+              <a
+                className="bg-purple-900 border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:text-purple-900 hover:font-semibold  hover:bg-purple-500 hover:border-purple-300 hover:border-4"
+                href="/join"
+              >
+                Join Today
+              </a>
+            </div>
           </div>
         </section>
       </main>
