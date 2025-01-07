739125cf7a9d6df1f4b5daec846b3c896c3b6a80
Anna Isaacks <annaisaacksdesign@gmail.com>
2025-01-06 20:39:21 -0600

    Updating Tech Stack styling and adding hyperlinks to logos

--- apps/website/src/components/modules/Home/styles.js
+++ apps/website/src/components/modules/Home/styles.js
@@ -35,7 +35,7 @@ export const styles = {
     logoStyle: 'h-16 w-auto flex-shrink-0',
     partnerContainer: 'w-full max-w-7xl mx-auto pt-20 px-4',
     partnerLogos:
-      'flex flex-col md:flex-row items-center justify-around gap-20 mt-8',
+      'flex flex-col md:flex-row items-center justify-between gap-20 mt-8',
     stackLogo:
       'flex items-center justify-center  gap-4 p-4 rounded-lg  transition-colors',
     subHeading:
