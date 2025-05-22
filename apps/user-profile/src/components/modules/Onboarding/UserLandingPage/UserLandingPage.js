d0eb47b25c26d6d1389ed9abab0553370817165c
remi <remitoutin@gmail.com>
2025-05-21 23:13:46 -0500

    refactor(userlandingpage): added link to terms & conditions page
    re #2118

--- apps/user-profile/src/components/modules/Onboarding/UserLandingPage/UserLandingPage.js
+++ apps/user-profile/src/components/modules/Onboarding/UserLandingPage/UserLandingPage.js
@@ -315,7 +315,10 @@ export default function UserLandingPage() {
                   className="w-6 h-6 text-purple-700 accent-purple-700 bg-white border-purple-700 rounded"
                 ></input>
                 <label id="agree-checkbox" className="text-sm text-black">
-                  I have read and agree to the <a>Terms and Conditions</a>
+                  I have read and agree to the{' '}
+                  <a href="https://staging.devlaunchers.org/page/terms-and-conditions">
+                    Terms and Conditions
+                  </a>
                 </label>
               </div>
               <p className="text-base text-red-500"> *</p>
