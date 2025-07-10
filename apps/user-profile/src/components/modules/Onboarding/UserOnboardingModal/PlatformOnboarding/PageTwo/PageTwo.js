9a99371128354f6c972f0fe2fdee1bd71f41ff2c
unknown <dbradham@banyanwater.com>
2025-07-09 20:41:51 -0500

    fix(user profile text align): text alignment on user profile onboarding

--- apps/user-profile/src/components/modules/Onboarding/UserOnboardingModal/PlatformOnboarding/PageTwo/PageTwo.js
+++ apps/user-profile/src/components/modules/Onboarding/UserOnboardingModal/PlatformOnboarding/PageTwo/PageTwo.js
@@ -10,8 +10,10 @@ export default function PageTwo() {
   return (
     <div className="flex flex-col gap-16 justify-center">
       <div className="flex flex-col text-center">
-        <Typography variant="secondary">ABOUT US</Typography>
-        <Typography textweight="light" textalign="center" type="h3">
+        <Typography textAlign="center" variant="secondary">
+          ABOUT US
+        </Typography>
+        <Typography textweight="light" textAlign="center" type="h3">
           What is DevLaunchers?
         </Typography>
       </div>
