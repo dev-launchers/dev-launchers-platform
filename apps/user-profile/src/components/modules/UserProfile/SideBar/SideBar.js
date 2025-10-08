d543f7ed53597491a178d4cc3f6e08ddc64a18f4
Rajkrithika <mail2krithika1@gmail.com>
2025-10-08 18:51:44 -0400

    SideBar integration

--- apps/user-profile/src/components/modules/UserProfile/SideBar/SideBar.js
+++ apps/user-profile/src/components/modules/UserProfile/SideBar/SideBar.js
@@ -2,23 +2,20 @@ import { Typography } from '@devlaunchers/components/components/atoms';
 import { useSidebarDataContext } from './../../../../context/SidebarDataContext';
 import { sidebarActions } from './../../../../state/actions';
 import ProfileImage from '../../../common/ProfileImage';
-
 import { useUserDataContext } from '@devlaunchers/components/context/UserDataContext';
 import UserInfo from './UserInfo';
 import {
   OutlinedOverview,
-  OutlinedProjects,
   OutlinedProfiles,
   OutlinedIdeas,
+  OutlinedProjects,
   OutlinedOpportunities,
 } from './../../../common/Icons';
-
 import OutlinedCat from './../../../common/CatIcon/OutlinedCat';
 
 function SideBar() {
   const { userData } = useUserDataContext();
   const { sidebarState, sidebarDispatch } = useSidebarDataContext();
-
   const { pages } = sidebarState;
 
   const styling = {
@@ -31,54 +28,34 @@ function SideBar() {
     },
   };
 
-  const getActiveStyling = (activePage) => {
-    return {
-      li: `group ${styling.li} ${
-        activePage ? styling.active.background : styling.background
-      }`,
-      iconColor: `${
-        activePage
-          ? 'stroke-black'
-          : 'stroke-grayscale-100 group-hover:stroke-black'
-      }`,
-      typography: `${activePage ? styling.active.text : styling.text}`,
-    };
-  };
+  const getActiveStyling = (activePage) => ({
+    li: `group ${styling.li} ${
+      activePage ? styling.active.background : styling.background
+    }`,
+    iconColor: `${
+      activePage
+        ? 'stroke-black'
+        : 'stroke-grayscale-100 group-hover:stroke-black'
+    }`,
+    typography: `${activePage ? styling.active.text : styling.text}`,
+  });
 
+  // Set active styles for each page
   styling.overview = getActiveStyling(pages.showOverview);
-
   styling.projects = getActiveStyling(pages.showProjects);
-
   styling.profiles = getActiveStyling(pages.showProfiles);
-
   styling.ideas = getActiveStyling(pages.showIdeas);
-
   styling.opportunities = getActiveStyling(pages.showOpportunities);
-
   styling.chatbot = getActiveStyling(pages.showChatbot);
 
-  const onOverviewClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_OVERVIEW_SETTING });
-  };
-  const onProjectsClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_PROJECTS_SETTING });
-  };
-  const onProfilesClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_PROFILES_SETTING });
-  };
-  const onIdeasClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_IDEAS_SETTING });
-  };
-  const onOpportunitiesClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_OPPORTUNITIES_SETTING });
-  };
-
-  const onChatbotClick = () => {
-    sidebarDispatch({ type: sidebarActions.SHOW_CHATBOT_SETTING });
+  // Sidebar click handlers
+  const handleClick = (actionType) => {
+    sidebarDispatch({ type: actionType });
   };
 
   return (
     <div className="flex flex-col gap-7 w-72 text-white border-r-2 bg-grayscale-900 border-grayscale-900 shadow-tra h-full border-solid shadow-sm bg-zinc-900 max-w-[288px]">
+      {/* User Header */}
       <div className="flex gap-4 w-full items-center px-8 py-6 border-b border-[#474747]">
         <ProfileImage
           imgSrc={userData.profilePictureUrl}
@@ -87,8 +64,13 @@ function SideBar() {
         />
         <UserInfo name={userData.name} />
       </div>
+
+      {/* Sidebar Links */}
       <ul className="flex flex-col gap-6 px-4">
-        <li className={styling.overview.li} onClick={onOverviewClick}>
+        <li
+          className={styling.overview.li}
+          onClick={() => handleClick(sidebarActions.SHOW_OVERVIEW_SETTING)}
+        >
           <OutlinedOverview colorClass={styling.overview.iconColor} />
           <Typography
             variant="secondary"
@@ -97,36 +79,68 @@ function SideBar() {
             OVERVIEW
           </Typography>
         </li>
-        {/* <li className={styling.projects.li} onClick={onProjectsClick}>
+
+        <li
+          className={styling.projects.li}
+          onClick={() => handleClick(sidebarActions.SHOW_PROJECTS_SETTING)}
+        >
           <OutlinedProjects colorClass={styling.projects.iconColor} />
-          <Typography type="p" className={styling.projects.typography}>
+          <Typography
+            variant="secondary"
+            className={styling.projects.typography}
+          >
             PROJECTS
           </Typography>
         </li>
-        <li className={styling.profiles.li} onClick={onProfilesClick}>
+
+        <li
+          className={styling.profiles.li}
+          onClick={() => handleClick(sidebarActions.SHOW_PROFILES_SETTING)}
+        >
           <OutlinedProfiles colorClass={styling.profiles.iconColor} />
-          <Typography type="p" className={styling.profiles.typography}>
+          <Typography
+            variant="secondary"
+            className={styling.profiles.typography}
+          >
             PROFILES
           </Typography>
         </li>
-        <li className={styling.ideas.li} onClick={onIdeasClick}>
+
+        <li
+          className={styling.ideas.li}
+          onClick={() => handleClick(sidebarActions.SHOW_IDEAS_SETTING)}
+        >
           <OutlinedIdeas colorClass={styling.ideas.iconColor} />
-          <Typography type="p" className={styling.ideas.typography}>
+          <Typography variant="secondary" className={styling.ideas.typography}>
             IDEAS
           </Typography>
         </li>
-        <li className={styling.opportunities.li} onClick={onOpportunitiesClick}>
+
+        <li
+          className={styling.opportunities.li}
+          onClick={() => handleClick(sidebarActions.SHOW_OPPORTUNITIES_SETTING)}
+        >
           <OutlinedOpportunities colorClass={styling.opportunities.iconColor} />
-          <Typography type="p" className={styling.opportunities.typography}>
+          <Typography
+            variant="secondary"
+            className={styling.opportunities.typography}
+          >
             OPPORTUNITIES
           </Typography>
         </li>
-        <li className={styling.chatbot.li} onClick={onChatbotClick}>
+
+        <li
+          className={styling.chatbot.li}
+          onClick={() => handleClick(sidebarActions.SHOW_CHATBOT_SETTING)}
+        >
           <OutlinedCat colorClass={styling.chatbot.iconColor} />
-          <Typography type="p" className={styling.chatbot.typography}>
-            DL Bot
+          <Typography
+            variant="secondary"
+            className={styling.chatbot.typography}
+          >
+            DL BOT
           </Typography>
-        </li> */}
+        </li>
       </ul>
     </div>
   );
