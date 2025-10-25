cb6ca84bcfc433d114505f245af557def70ef872
chungthuang <7826979+chungthuang@users.noreply.github.com>
2025-10-25 13:02:19 -0500

    fix(support-us): render button as anchor tag, so clicking opens link

--- apps/website/src/components/modules/SupportUs/SupportUs.js
+++ apps/website/src/components/modules/SupportUs/SupportUs.js
@@ -65,6 +65,7 @@ export default function SupportUs() {
           </Typography>
           <div className="flex justify-center pt-8">
             <atoms.Button
+              as="a"
               className="bg-purple-900 border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:text-black hover:font-semibold  hover:bg-purple-500 hover:border-purple-300 hover:border-4"
               href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=E6TSNXLFG3UTY&source=url"
             >
@@ -104,12 +105,14 @@ export default function SupportUs() {
           </Typography>
           <div className="flex flex-wrap justify-center gap-8 pt-8">
             <atoms.Button
+              as="a"
               className="bg-black border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:bg-purple-500 hover:text-black hover:font-semibold hover:border-purple-300 hover:border-4"
               href="mailto:contact@devlaunchers.com"
             >
               Contact Us
             </atoms.Button>
             <atoms.Button
+              as="a"
               className="bg-purple-900 border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:text-purple-900 hover:font-semibold  hover:bg-purple-500 hover:border-purple-300 hover:border-4"
               href="https://forms.gle/VLP2YHDsSXmLTqQ8A"
             >
@@ -151,12 +154,14 @@ export default function SupportUs() {
           </Typography>
           <div className="flex flex-wrap justify-center gap-8 pt-8">
             <atoms.Button
+              as="a"
               className="bg-black border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:bg-purple-500 hover:text-black hover:font-semibold hover:border-purple-300 hover:border-4"
               href="mailto:contact@devlaunchers.com"
             >
               Contact Us
             </atoms.Button>
             <atoms.Button
+              as="a"
               className="bg-purple-900 border-2 border-purple-400 text-base text-white font-normal capitalize py-3 px-6 rounded-xl transition-all duration-200 hover:text-black hover:font-semibold  hover:bg-purple-500 hover:border-purple-300 hover:border-4"
               href="https://forms.gle/SrKn5wWAYUNGYmec9"
             >
