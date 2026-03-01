b096d9d77659cd17a2b477c6ed073c95c45d19df
Yaqin She <yaqinshe3@gmail.com>
2026-03-01 13:06:38 -0800

    Fix homepage "Join Today" button redirect

--- apps/website/src/components/modules/Home/Sections/JoinUs.tsx
+++ apps/website/src/components/modules/Home/Sections/JoinUs.tsx
@@ -28,7 +28,7 @@ const JoinUsSection = () => (
       </atoms.Typography>
     </div>
     <div className={styles.section.buttonContainer}>
-      <a href="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?client_id=164131315429-hcllbtrq77jsc7b3fqviak7148hj8oa9.apps.googleusercontent.com&response_type=code&redirect_uri=https%3A%2F%2Fapiv4.devlaunchers.org%2Fapi%2Fconnect%2Fgoogle%2Fcallback&scope=email%20profile&service=lso&o2v=2&ddm=1&flowName=GeneralOAuthFlow">
+      <a href="https://staging.devlaunchers.org/join">
         <atoms.Button type="primary" color="nebula">
           {sections.join.buttonText}
         </atoms.Button>
